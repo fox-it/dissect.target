93d2aee293e203ae3eadb3bbd45c714a877cc0a2
cecinestpasunepipe <110607403+cecinestpasunepipe@users.noreply.github.com>
2022-08-10 14:03:10 +0200

    DIS-1026 - Add exception handler and logger for Windows as well.

--- tests/test_loaders_local.py
+++ tests/test_loaders_local.py
@@ -8,18 +8,17 @@ from dissect.target.target import TargetLogAdapter
 def test_local_loader_drive_skipping(mock_target):
     mock = mock_open()
     # Does it attempt to open the file and pass a raw container?
-    with patch("builtins.open", mock):
-        with patch.object(mock_target.disks, "add") as mock_method:
-            drive = "/xdev/fake"
-            _add_disk_as_raw_container_to_target(drive, mock_target)
-            assert isinstance(mock_method.call_args[0][0], RawContainer) is True
+    with (patch("builtins.open", mock), patch.object(mock_target.disks, "add") as mock_method):
+        drive = "/xdev/fake"
+        _add_disk_as_raw_container_to_target(drive, mock_target)
+        assert isinstance(mock_method.call_args[0][0], RawContainer) is True
     mock.assert_called_with("/xdev/fake", "rb")
 
     # Does it emit a warning instead of raising an exception?
     mock.side_effect = IOError
-    with patch.object(TargetLogAdapter, "warning") as mock_method:
-        with patch("builtins.open", mock):
-            drive = "/xdev/fake"
-            _add_disk_as_raw_container_to_target(drive, mock_target)
-        mock_method.assert_called_with("Unable to open drive: /xdev/fake, skipped")
+    with (patch.object(TargetLogAdapter, "warning") as mock_method, patch("builtins.open", mock)):
+        drive = "/xdev/fake"
+        _add_disk_as_raw_container_to_target(drive, mock_target)
+        assert mock_method.call_args[0][0] == "Unable to open drive: /xdev/fake, skipped"
+        assert isinstance(mock_method.call_args[1]["exc_info"], OSError) is True
     mock.assert_called_with("/xdev/fake", "rb")
