515321bfb191514f41074f5f1070ccfcc323b48d
cecinestpasunepipe <110607403+cecinestpasunepipe@users.noreply.github.com>
2022-08-12 18:13:34 +0200

    DIS-583 inline test parameters to be more consistent with the rest of the project

--- tests/test_loaders_local.py
+++ tests/test_loaders_local.py
@@ -6,26 +6,6 @@ from dissect.target.containers.raw import RawContainer
 from dissect.target.loaders.local import _add_disk_as_raw_container_to_target, LINUX_DRIVE_REGEX
 from dissect.target.target import TargetLogAdapter
 
-testdata_linux_drive_regex = [
-    ("fd0", True),  # Floppy
-    ("fd1", True),  # Floppy
-    ("fd2", True),  # Floppy
-    ("sda", True),  # SCSI
-    ("sdb", True),  # SCSI
-    ("sdc", True),  # SCSI
-    ("sda1", False),  # Partition
-    ("nvme0n1", True),  # NVMe Disk
-    ("nvme0n1p1", False),  # Partition
-    ("hda", True),  # IDE-Controller
-    ("hdb", True),  # IDE-Controller
-    ("hda1", False),  # Partition
-    ("sr0", False),  # Compact Disc
-    ("scd0", False),  # Compact Disc
-    ("tty11", False),  # Not a disk
-    ("xsdaa", False),  # Fake
-    ("xhdaa", False),  # Fake
-]
-
 
 def test_local_loader_drive_skipping(mock_target):
     mock = mock_open()
@@ -46,6 +26,27 @@ def test_local_loader_drive_skipping(mock_target):
     mock.assert_called_with("/xdev/fake", "rb")
 
 
-@pytest.mark.parametrize("drive_name,expected", testdata_linux_drive_regex)
+@pytest.mark.parametrize(
+    "drive_name,expected",
+    [
+        ("fd0", True),  # Floppy
+        ("fd1", True),  # Floppy
+        ("fd2", True),  # Floppy
+        ("sda", True),  # SCSI
+        ("sdb", True),  # SCSI
+        ("sdc", True),  # SCSI
+        ("sda1", False),  # Partition
+        ("nvme0n1", True),  # NVMe Disk
+        ("nvme0n1p1", False),  # Partition
+        ("hda", True),  # IDE-Controller
+        ("hdb", True),  # IDE-Controller
+        ("hda1", False),  # Partition
+        ("sr0", False),  # Compact Disc
+        ("scd0", False),  # Compact Disc
+        ("tty11", False),  # Not a disk
+        ("xsdaa", False),  # Fake
+        ("xhdaa", False),  # Fake
+    ],
+)
 def test_linux_drive_regex(drive_name, expected):
     assert (LINUX_DRIVE_REGEX.match(drive_name) is not None) == expected
