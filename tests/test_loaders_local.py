684a3cf8d1754198533a9968011f3afde4317914
cecinestpasunepipe <110607403+cecinestpasunepipe@users.noreply.github.com>
2022-08-12 17:55:15 +0200

    DIS-583 use separate parameterized test for linux drive regex test

--- tests/test_loaders_local.py
+++ tests/test_loaders_local.py
@@ -1,8 +1,30 @@
+import pytest
+
 from unittest.mock import patch, mock_open
 
 from dissect.target.containers.raw import RawContainer
 from dissect.target.loaders.local import _add_disk_as_raw_container_to_target, LINUX_DRIVE_REGEX
 from dissect.target.target import TargetLogAdapter
+
+testdata_linux_drive_regex = [
+    ("fd0", True),  # Floppy
+    ("fd1", True),  # Floppy
+    ("fd2", True),  # Floppy
+    ("sda", True),  # SCSI
+    ("sdb", True),  # SCSI
+    ("sdc", True),  # SCSI
+    ("sda1", False),  # Partition
+    ("nvme0n1", True),  # NVMe Disk
+    ("nvme0n1p1", False),  # Partition
+    ("hda", True),  # IDE-Controller
+    ("hdb", True),  # IDE-Controller
+    ("hda1", False),  # Partition
+    ("sr0", False),  # Compact Disc
+    ("scd0", False),  # Compact Disc
+    ("tty11", False),  # Not a disk
+    ("xsdaa", False),  # Fake
+    ("xhdaa", False),  # Fake
+]
 
 
 def test_local_loader_drive_skipping(mock_target):
@@ -23,25 +45,7 @@ def test_local_loader_drive_skipping(mock_target):
         assert isinstance(mock_method.call_args[1]["exc_info"], OSError) is True
     mock.assert_called_with("/xdev/fake", "rb")
 
-    # Can we detect all sorts of Linux devices properly?
-    drive_names = {
-        "fd0": True,  # Floppy
-        "fd1": True,  # Floppy
-        "fd2": True,  # Floppy
-        "sda": True,  # SCSI
-        "sdb": True,  # SCSI
-        "sdc": True,  # SCSI
-        "sda1": False,  # Partition
-        "nvme0n1": True,  # NVMe Disk
-        "nvme0n1p1": False,  # Partition
-        "hda": True,  # IDE-Controller
-        "hdb": True,  # IDE-Controller
-        "hda1": False,  # Partition
-        "sr0": False,  # Compact Disc
-        "scd0": False,  # Compact Disc
-        "tty11": False,  # Not a disk
-        "xsdaa": False,  # Fake
-        "xhdaa": False,  # Fake
-    }
-    for name, valid in drive_names.items():
-        assert (LINUX_DRIVE_REGEX.match(name) is not None) == valid
+
+@pytest.mark.parametrize("drive_name,expected", testdata_linux_drive_regex)
+def test_linux_drive_regex(drive_name, expected):
+    assert (LINUX_DRIVE_REGEX.match(drive_name) is not None) == expected
