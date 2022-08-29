bf6ac7feae77642311370ab579e6bcd26a2e7fd7
Stefan de Reuver <9864602+Horofic@users.noreply.github.com>
2022-08-29 14:16:35 +0200

    Fix local loader map_drives functions. Change local loader tests DIS-1424 (#25)

--- tests/test_loaders_local.py
+++ tests/test_loaders_local.py
@@ -1,4 +1,5 @@
 import pytest
+from pathlib import Path
 
 from unittest.mock import patch, mock_open
 
@@ -11,42 +12,42 @@ def test_local_loader_drive_skipping(mock_target):
     mock = mock_open()
     # Does it attempt to open the file and pass a raw container?
     with (patch("builtins.open", mock), patch.object(mock_target.disks, "add") as mock_method):
-        drive = "/xdev/fake"
+        drive = Path("/xdev/fake")
         _add_disk_as_raw_container_to_target(drive, mock_target)
         assert isinstance(mock_method.call_args[0][0], RawContainer) is True
-    mock.assert_called_with("/xdev/fake", "rb")
+    mock.assert_called_with(Path("/xdev/fake"), "rb")
 
     # Does it emit a warning instead of raising an exception?
     mock.side_effect = IOError
     with (patch.object(TargetLogAdapter, "warning") as mock_method, patch("builtins.open", mock)):
-        drive = "/xdev/fake"
+        drive = Path("/xdev/fake")
         _add_disk_as_raw_container_to_target(drive, mock_target)
         assert mock_method.call_args[0][0] == "Unable to open drive: /xdev/fake, skipped"
         assert isinstance(mock_method.call_args[1]["exc_info"], OSError) is True
-    mock.assert_called_with("/xdev/fake", "rb")
+    mock.assert_called_with(Path("/xdev/fake"), "rb")
 
 
 @pytest.mark.parametrize(
-    "drive_name, expected",
+    "drive_path, expected",
     [
-        ("fd0", True),  # Floppy
-        ("fd1", True),  # Floppy
-        ("fd2", True),  # Floppy
-        ("sda", True),  # SCSI
-        ("sdb", True),  # SCSI
-        ("sdc", True),  # SCSI
-        ("sda1", False),  # Partition
-        ("nvme0n1", True),  # NVMe Disk
-        ("nvme0n1p1", False),  # Partition
-        ("hda", True),  # IDE-Controller
-        ("hdb", True),  # IDE-Controller
-        ("hda1", False),  # Partition
-        ("sr0", False),  # Compact Disc
-        ("scd0", False),  # Compact Disc
-        ("tty11", False),  # Not a disk
-        ("xsdaa", False),  # Fake
-        ("xhdaa", False),  # Fake
+        (Path("/dev/fd0"), True),  # Floppy
+        (Path("/dev/fd1"), True),  # Floppy
+        (Path("/dev/fd2"), True),  # Floppy
+        (Path("/dev/sda"), True),  # SCSI
+        (Path("/dev/sdb"), True),  # SCSI
+        (Path("/dev/sdc"), True),  # SCSI
+        (Path("/dev/sda1"), False),  # Partition
+        (Path("/dev/nvme0n1"), True),  # NVMe Disk
+        (Path("/dev/nvme0n1p1"), False),  # Partition
+        (Path("/dev/hda"), True),  # IDE-Controller
+        (Path("/dev/hdb"), True),  # IDE-Controller
+        (Path("/dev/hda1"), False),  # Partition
+        (Path("/dev/sr0"), False),  # Compact Disc
+        (Path("/dev/scd0"), False),  # Compact Disc
+        (Path("/dev/tty11"), False),  # Not a disk
+        (Path("/dev/xsdaa"), False),  # Fake
+        (Path("/dev/xhdaa"), False),  # Fake
     ],
 )
-def test_linux_drive_regex(drive_name, expected):
-    assert (LINUX_DRIVE_REGEX.match(drive_name) is not None) == expected
+def test_linux_drive_regex(drive_path, expected):
+    assert (LINUX_DRIVE_REGEX.match(drive_path.name) is not None) == expected
