f0c20e727c00ba2759b3beeb5c104f8558668316
Erik Schamper <1254028+Schamper@users.noreply.github.com>
2022-08-31 15:04:33 +0200

    Some bug fixes to adpolicy plugin (#8)
    (DIS-1336)

---
+++ tests/test_plugins_os_windows_adpolicy.py
@@ -0,0 +1,14 @@
+from dissect.target.plugins.os.windows import adpolicy
+
+from ._utils import absolute_path
+
+
+def test_adpolicy_plugin(target_win, fs_win):
+    policy_dir = absolute_path("data/adpolicy/")
+
+    fs_win.map_dir("Windows/sysvol/domain/policies", policy_dir)
+
+    target_win.add_plugin(adpolicy.ADPolicyPlugin)
+
+    records = list(target_win.adpolicy())
+    assert len(records) == 9
