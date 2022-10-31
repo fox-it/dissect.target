a538fad815465c923c03cf5041182c792a518cf8
cecinestpasunepipe <110607403+cecinestpasunepipe@users.noreply.github.com>
2022-10-31 17:11:20 +0100

    Improve coverage (DIS-1430)

--- tests/test_loaders_vbox.py
+++ tests/test_loaders_vbox.py
@@ -7,8 +7,11 @@ from dissect.target.loaders.vbox import VboxLoader
 @patch("dissect.hypervisor.vdi.Vbox")
 @patch("pathlib.Path")
 def test_vbox_loader(vdi, vbox, Path, mock_target) -> None:
+    vbox.return_value = vbox
+    vbox.disks.return_value = [Path("/mock.vdi")]
     vbox_loader = VboxLoader(Path("/mock.vbox"))
     vbox_loader.map(mock_target)
-    expected = [call().disks(), call().disks().__iter__()]
+    expected = [call.disks()]
     del vbox.mock_calls[0]
     assert expected == vbox.mock_calls
+    assert len(mock_target.disks) == 1
