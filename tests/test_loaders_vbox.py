37dec6883b8240f4f82d77a11fc1341003b9b06e
cecinestpasunepipe <110607403+cecinestpasunepipe@users.noreply.github.com>
2022-11-02 09:40:47 +0100

    Adjust test to refactored VBox (DIS-1430)

--- tests/test_loaders_vbox.py
+++ tests/test_loaders_vbox.py
@@ -3,15 +3,14 @@ from unittest.mock import call, patch
 from dissect.target.loaders.vbox import VboxLoader
 
 
-@patch("dissect.hypervisor.vdi")
-@patch("dissect.hypervisor.descriptor.vbox.VBox")
 @patch("pathlib.Path")
-def test_vbox_loader(vdi, vbox, Path, mock_target) -> None:
-    vbox.return_value = vbox
-    vbox.disks.return_value = [Path("/mock.vdi")]
+@patch("dissect.target.loaders.vbox.VBox")
+def test_vbox_loader(VBox, Path, mock_target) -> None:
+    VBox.return_value = VBox
+    VBox.disks.return_value = [Path("/mock.vdi")]
     vbox_loader = VboxLoader(Path("/mock.vbox"))
     vbox_loader.map(mock_target)
     expected = [call.disks()]
-    del vbox.mock_calls[0]
-    assert expected == vbox.mock_calls
+    del VBox.mock_calls[0]
+    assert expected == VBox.mock_calls
     assert len(mock_target.disks) == 1
