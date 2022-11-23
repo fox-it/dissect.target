197e1a2bdcd951b60ad99c87ed2ed58ac2f88bf0
Zawadi Done <hi@zawadidone.nl>
2022-11-23 12:34:37 +0100

    Add Virtuozzo Loader (#85)

---
+++ dissect/target/plugins/child/virtuozzo.py
@@ -0,0 +1,38 @@
+from dissect.target.exceptions import UnsupportedPluginError
+from dissect.target.helpers.record import ChildTargetRecord
+from dissect.target.plugin import ChildTargetPlugin
+
+
+class VirtuozzoTargetPlugin(ChildTargetPlugin):
+    """Child target plugin that yields from Virtuozzo container's root.
+
+    Virtuozzo conatiners are by default registered in the folder ``vz/root/$VEID``,
+    where VEID will be substituted with the actual container UUID.
+
+    /
+      etc/
+      var/
+      vz/
+          root/
+              <container-uuid>/
+              <container-uuid>/
+
+    Sources:
+        - https://docs.virtuozzo.com/virtuozzo_hybrid_server_7_command_line_reference/managing-system/configuration-files.html
+    """  # noqa: E501
+
+    __type__ = "virtuozzo"
+
+    PATH = "/vz/root"
+
+    def check_compatible(self) -> None:
+        if not self.target.fs.path(self.PATH).exists():
+            raise UnsupportedPluginError("No /vz/root folder found")
+
+    def list_children(self):
+        for container in self.target.fs.path(self.PATH).iterdir():
+            yield ChildTargetRecord(
+                type=self.__type__,
+                path=str(container),
+                _target=self.target,
+            )
