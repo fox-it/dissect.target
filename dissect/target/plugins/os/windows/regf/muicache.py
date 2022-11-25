7e57b065517a42cbb7c2bc64d1feaec631d74325
Zawadi Done <hi@zawadidone.nl>
2022-11-25 01:11:08 +0100

    Add additonal MUIcache key locations

--- dissect/target/plugins/os/windows/regf/muicache.py
+++ dissect/target/plugins/os/windows/regf/muicache.py
@@ -1,3 +1,5 @@
+from typing import Generator
+
 from flow.record.fieldtypes import uri
 
 from dissect.target.exceptions import UnsupportedPluginError
@@ -6,6 +8,7 @@ from dissect.target.helpers.descriptor_extensions import (
     RegistryRecordDescriptorExtension,
 )
 from dissect.target.helpers.record import create_extended_descriptor
+from dissect.target.helpers.regutil import RegistryKey
 from dissect.target.plugin import Plugin, export
 
 MuicacheRecord = create_extended_descriptor([RegistryRecordDescriptorExtension, UserRecordDescriptorExtension])(
@@ -23,10 +26,14 @@ class MuicachePlugin(Plugin):
     """Plugin that iterates various MUIcache locations."""
 
     KEYS = [
-        "HKCU\\Software\\Classes\\Local Settings\\Software\\Microsoft\\Windows\\Shell\\MuiCache",
-        # FIXME subkeys, XP, etc
-        # "HKCU\\Software\\Classes\\Local Settings\\MuiCache",
-        # "HKCU\\Software\\Microsoft\\Windows\\ShellNoRoam\\MUICache",
+        "HKCU\\Software\\Classes\\Local Settings\\Software\\Microsoft\\Windows\\Shell\\MuiCache",  # NT >= 6.0
+        "HKCU\\Software\\Classes\\Local Settings\\MuiCache",  # NT >= 6.0
+        "HKCU\\Software\\Microsoft\\Windows\\ShellNoRoam\\MUICache",  # NT < 6.0
+    ]
+
+    FIELD_NAMES = [
+        "FriendlyAppName",
+        "ApplicationCompany",
     ]
 
     def check_compatible(self):
@@ -43,6 +50,7 @@ class MuicachePlugin(Plugin):
 
         Sources:
             - https://www.magnetforensics.com/blog/forensic-analysis-of-muicache-files-in-windows/
+            - https://forensafe.com/blogs/muicache.html
 
         Yields MuicacheRecords with fields:
             hostname (string): The target hostname.
@@ -52,24 +60,43 @@ class MuicachePlugin(Plugin):
             value (string): The value.
             path (uri): The executable path.
         """
-        for key in self.KEYS:
-            for r in self.target.registry.keys(key):
-                user = self.target.registry.get_user(r)
-                for index, entry in enumerate(r.values()):
-                    try:
-                        path, name = entry.name.rsplit(".", 1)
-                        path = uri.from_windows(path)
-                        yield MuicacheRecord(
-                            index=index,
-                            name=name,
-                            value=entry.value,
-                            path=str(path),
-                            _target=self.target,
-                            _key=r,
-                            _user=user,
-                        )
-                    except ValueError:
-                        continue
-                    except Exception:
-                        self.target.log.exception("Exception while parsing muicache")
-                        continue
+        for reg_path in self.KEYS:
+            for key in self.target.registry.keys(reg_path):
+                if len(key.subkeys()):
+                    for subkey in key.subkeys():
+                        for item in subkey.subkeys():
+                            yield from self._get_records(item)
+                else:
+                    yield from self._get_records(key)
+
+    def _get_records(self, key: RegistryKey) -> Generator[MuicacheRecord, None, None]:
+        for index, entry in enumerate(key.values()):
+            user = self.target.registry.get_user(key)
+            try:
+                if entry.name.endswith(tuple(self.FIELD_NAMES)):
+                    path, name = entry.name.rsplit(".", 1)
+                else:
+                    name = None
+                    path = entry.name.rsplit(",-", 1)[0]
+
+                value = entry.value
+                path = uri.from_windows(path)
+
+                # Filter on the type of the registry value
+                if type(value) is bytes:
+                    continue
+
+                yield MuicacheRecord(
+                    index=index,
+                    name=name,
+                    value=value,
+                    path=path,
+                    _target=self.target,
+                    _key=key,
+                    _user=user,
+                )
+            except ValueError:
+                continue
+            except Exception:
+                self.target.log.exception("Exception while parsing muicache")
+                continue
--- tests/test_plugins_os_windows_muicache.py
+++ tests/test_plugins_os_windows_muicache.py
@@ -1,30 +1,52 @@
-from dissect.target.helpers.regutil import VirtualHive, VirtualKey, VirtualValue
+from dissect.target.helpers.regutil import VirtualKey, VirtualValue
 from dissect.target.plugins.os.windows.regf.muicache import MuicachePlugin
 
 
 def test_muicache_plugin(target_win_users, hive_hku):
-    muicache_key = VirtualKey(
+    # NT >= 6.0
+    muicache_shell_key = VirtualKey(
         hive_hku,
         "Software\\Classes\\Local Settings\\Software\\Microsoft\\Windows\\Shell\\MuiCache",
     )
-
-    muicache_key.add_value("LangID", VirtualValue(hive_hku, "LangID", b"\t\x04"))
-    muicache_key.add_value(
+    muicache_shell_key.add_value("LangID", VirtualValue(hive_hku, "LangID", b"\t\x04"))
+    muicache_shell_key.add_value(
         "C:\\Windows\\System32\\fsquirt.exe.FriendlyAppName",
         VirtualValue(hive_hku, "C:\\Windows\\System32\\fsquirt.exe.FriendlyAppName", "fsquir"),
     )
-    muicache_key.add_value(
+    muicache_shell_key.add_value(
         "C:\\Windows\\System32\\fsquirt.test.exe.ApplicationCompany",
         VirtualValue(hive_hku, "C:\\Windows\\System32\\fsquirt.test.exe.ApplicationCompany", "fsquir"),
     )
 
+    # NT >= 6.0 (subkey)
+    muicache_key = VirtualKey(
+        hive_hku,
+        "Software\\Classes\\Local Settings\\MuiCache\\35\\52C64B7E",
+    )
+    muicache_key.add_value(
+        "@C:\\Windows\\system32\\wsecedit.dll,-718",
+        VirtualValue(hive_hku, "@C:\\Windows\\system32\\wsecedit.dll,-718", "Local Security Policy"),
+    )
+
+    # NT < 6.0
+    muicache_shellnoroam_key = VirtualKey(
+        hive_hku,
+        "Software\\Microsoft\\Windows\\ShellNoRoam\\MUICache",
+    )
+    muicache_shellnoroam_key.add_value(
+        "@C:\\WINDOWS\\inf\\unregmp2.exe,-4",
+        VirtualValue(hive_hku, "@C:\\WINDOWS\\inf\\unregmp2.exe,-4", "Windows Media Player"),
+    )
+
+    hive_hku.map_key(muicache_shell_key.path, muicache_shell_key)
     hive_hku.map_key(muicache_key.path, muicache_key)
+    hive_hku.map_key(muicache_shellnoroam_key.path, muicache_shellnoroam_key)
 
     target_win_users.add_plugin(MuicachePlugin)
 
     results = list(target_win_users.muicache())
 
-    assert len(results) == 2
+    assert len(results) == 4
     assert results[0].index == 1
     assert results[0].name == "FriendlyAppName"
     assert results[0].value == "fsquir"
@@ -33,3 +55,11 @@ def test_muicache_plugin(target_win_users, hive_hku):
     assert results[1].name == "ApplicationCompany"
     assert results[1].value == "fsquir"
     assert results[1].path == "C:/Windows/System32/fsquirt.test.exe"
+    assert results[2].index == 0
+    assert results[2].name is None
+    assert results[2].value == "Local Security Policy"
+    assert results[2].path == "@C:/Windows/system32/wsecedit.dll"
+    assert results[3].index == 0
+    assert results[3].name is None
+    assert results[3].value == "Windows Media Player"
+    assert results[3].path == "@C:/WINDOWS/inf/unregmp2.exe"
