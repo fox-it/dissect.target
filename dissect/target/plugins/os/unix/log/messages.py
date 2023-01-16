9bb2eb7c05ae8cbae8f623bb3a35d7209bd43f61
JSCU-CNI <121175071+JSCU-CNI@users.noreply.github.com>
2023-01-16 17:04:42 +0100

    slightly improve unix messages plugin

--- dissect/target/plugins/os/unix/log/messages.py
+++ dissect/target/plugins/os/unix/log/messages.py
@@ -24,10 +24,18 @@ RE_DAEMON = re.compile(r"^[^:]+:\d+:\d+[^\[\]:]+\s([^\[:]+)[\[|:]{1}")
 RE_PID = re.compile(r"\w\[(\d+)\]")
 RE_MSG = re.compile(r"[^:]+:\d+:\d+[^:]+:\s(.*)$")
 
+LOG_LOCATIONS = [
+    "/var/log/messages*",
+    "/var/log/syslog*",
+]
+
 
 class MessagesPlugin(Plugin):
     def check_compatible(self):
-        pass
+        for log_location in LOG_LOCATIONS:
+            if len(list(self.target.fs.glob_ext(log_location))) > 0:
+                return True
+        return False
 
     @export(record=MessagesRecord)
     def messages(self):
@@ -40,60 +48,68 @@ class MessagesPlugin(Plugin):
         Sources:
             - https://geek-university.com/linux/var-log-messages-file/
         """
-        for f in self.target.fs.glob_ext("/var/log/messages*"):
-            # FIXME Year is not part of syslog timestamp, take it from file. does not work yet for tar.
-            # FIXME watch for change of year in file!!!
-            # year = datetime.datetime.fromtimestamp(f.stat.mtime).year
-            year = 2018
-            fh = f.open()
-            if f.name[-2:] == "gz":
-                fh = zlib.decompress(fh.read(), 31)
-                try:
-                    fh = StringIO(fh.decode())
-                except UnicodeDecodeError:
-                    pass
-            if "bz2" in f.name:
-                fh = bz2.decompress(fh)
-                fh = StringIO(fh.decode())
+        for log_location in LOG_LOCATIONS:
+            if self.target.fs.glob_ext(log_location):
+                for f in self.target.fs.glob_ext(log_location):
+                    # FIXME Year is not part of syslog timestamp, take it from file. does not work yet for tar.
+                    # FIXME watch for change of year in file!!!
+                    file_mtime = self.target.fs.get(str(f)).stat().st_mtime
+                    year = datetime.datetime.fromtimestamp(file_mtime).year
 
-            for line in fh:
-                line = line.strip()
-                if not line:
-                    continue
+                    fh = f.open()
+                    if f.name[-2:] == "gz":
+                        fh = zlib.decompress(fh.read(), 31)
+                        try:
+                            fh = StringIO(fh.decode())
+                        except UnicodeDecodeError:
+                            pass
+                    if "bz2" in f.name:
+                        fh = bz2.decompress(fh)
+                        fh = StringIO(fh.decode())
 
-                # FIXME some messages files have long and short hostname in the file
-                try:
-                    # FIXME add timezone?
-                    ts = datetime.datetime.strptime(RE_TS.search(line).groups()[0], "%b %d %H:%M:%S")
-                    ts = ts.replace(year=year)
-                except Exception:
-                    ts = datetime.datetime.now()
+                    for line in fh:
 
-                try:
-                    daemon = RE_DAEMON.search(line).groups()[0]
-                except Exception:
-                    daemon = None
+                        # Line can be a byte object on debian and centos
+                        if isinstance(line, bytes):
+                            line = line.decode("utf-8").strip()
+                        else:
+                            line = line.strip()
 
-                try:
-                    pid = RE_PID.search(line)
-                    if pid is not None:
-                        pid = pid.groups()[0]
-                except Exception:
-                    pid = None
+                        if not line:
+                            continue
 
-                try:
-                    msg = RE_MSG.search(line).groups()[0]
-                    if msg is None:
-                        msg = line
-                except Exception:
-                    msg = line
+                        try:
+                            ts = datetime.datetime.strptime(RE_TS.search(line).groups()[0], "%b %d %H:%M:%S")
+                            ts = ts.replace(year=year)
+                        except Exception:
+                            # Set ts to epoch 1970-01-01 if we could not convert
+                            ts = datetime.datetime.utcfromtimestamp(0)
 
-                yield MessagesRecord(
-                    ts=ts,
-                    filepath=f.name,
-                    daemon=daemon,
-                    pid=pid,
-                    msg=msg,
-                    raw=line,
-                    _target=self.target,
-                )
+                        try:
+                            daemon = RE_DAEMON.search(line).groups()[0]
+                        except Exception:
+                            daemon = None
+
+                        try:
+                            pid = RE_PID.search(line)
+                            if pid is not None:
+                                pid = pid.groups()[0]
+                        except Exception:
+                            pid = None
+
+                        try:
+                            msg = RE_MSG.search(line).groups()[0]
+                            if msg is None:
+                                msg = line
+                        except Exception:
+                            msg = line
+
+                        yield MessagesRecord(
+                            ts=ts,
+                            filepath=f.path,
+                            daemon=daemon,
+                            pid=pid,
+                            msg=msg,
+                            raw=line,
+                            _target=self.target,
+                        )
