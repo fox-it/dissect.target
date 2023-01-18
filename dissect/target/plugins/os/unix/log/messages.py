b3fc057d14fca2744d4c2da9d6da4f21c804e567
JSCU-CNI <121175071+JSCU-CNI@users.noreply.github.com>
2023-01-18 17:45:04 +0100

    add year rollover support for unix messages plugin

--- dissect/target/plugins/os/unix/log/messages.py
+++ dissect/target/plugins/os/unix/log/messages.py
@@ -1,9 +1,7 @@
-import bz2
-import datetime
 import re
-import zlib
-from io import StringIO
+from datetime import datetime
 
+from dissect.target.helpers.fsutil import TargetPath, decompress_and_readlines
 from dissect.target.helpers.record import TargetRecordDescriptor
 from dissect.target.plugin import Plugin, export
 
@@ -11,31 +9,27 @@ MessagesRecord = TargetRecordDescriptor(
     "linux/messages",
     [
         ("datetime", "ts"),
-        ("uri", "filepath"),
         ("string", "daemon"),
         ("varint", "pid"),
-        ("string", "msg"),
+        ("string", "message"),
         ("string", "raw"),
+        ("uri", "source"),
     ],
 )
 
-RE_TS = re.compile(r"(\w+\s\d+\s\d{2}:\d{2}:\d{2})")
+RE_TS = re.compile(r"(\w+\s{1,2}\d+\s\d{2}:\d{2}:\d{2})")
 RE_DAEMON = re.compile(r"^[^:]+:\d+:\d+[^\[\]:]+\s([^\[:]+)[\[|:]{1}")
 RE_PID = re.compile(r"\w\[(\d+)\]")
 RE_MSG = re.compile(r"[^:]+:\d+:\d+[^:]+:\s(.*)$")
 
-LOG_LOCATIONS = [
-    "/var/log/messages*",
-    "/var/log/syslog*",
-]
-
 
 class MessagesPlugin(Plugin):
     def check_compatible(self):
-        for log_location in LOG_LOCATIONS:
-            if len(list(self.target.fs.glob_ext(log_location))) > 0:
-                return True
-        return False
+        return self.target.fs.path("/var/log/syslog").exists() or self.target.fs.path("/var/log/messages").exists()
+
+    @export(record=MessagesRecord)
+    def syslog(self):
+        return self.messages()
 
     @export(record=MessagesRecord)
     def messages(self):
@@ -48,68 +42,65 @@ class MessagesPlugin(Plugin):
         Sources:
             - https://geek-university.com/linux/var-log-messages-file/
         """
-        for log_location in LOG_LOCATIONS:
-            if self.target.fs.glob_ext(log_location):
-                for f in self.target.fs.glob_ext(log_location):
-                    # FIXME Year is not part of syslog timestamp, take it from file. does not work yet for tar.
-                    # FIXME watch for change of year in file!!!
-                    file_mtime = self.target.fs.get(str(f)).stat().st_mtime
-                    year = datetime.datetime.fromtimestamp(file_mtime).year
 
-                    fh = f.open()
-                    if f.name[-2:] == "gz":
-                        fh = zlib.decompress(fh.read(), 31)
-                        try:
-                            fh = StringIO(fh.decode())
-                        except UnicodeDecodeError:
-                            pass
-                    if "bz2" in f.name:
-                        fh = bz2.decompress(fh)
-                        fh = StringIO(fh.decode())
+        log_files: [TargetPath] = list(self.target.fs.path("/var/log/").glob("syslog*")) + list(
+            self.target.fs.path("/var/log/").glob("messages*")
+        )
 
-                    for line in fh:
+        for log_file in log_files:
 
-                        # Line can be a byte object on debian and centos
-                        if isinstance(line, bytes):
-                            line = line.decode("utf-8").strip()
-                        else:
-                            line = line.strip()
+            file_ctime = self.target.fs.get(str(log_file)).stat().st_ctime
+            year_file_created = datetime.fromtimestamp(file_ctime).year
+            last_seen_year = year_file_created
+            last_seen_month = 0
 
-                        if not line:
-                            continue
+            for line in decompress_and_readlines(log_file):
 
-                        try:
-                            ts = datetime.datetime.strptime(RE_TS.search(line).groups()[0], "%b %d %H:%M:%S")
-                            ts = ts.replace(year=year)
-                        except Exception:
-                            # Set ts to epoch 1970-01-01 if we could not convert
-                            ts = datetime.datetime.utcfromtimestamp(0)
+                # Line can be a byte object on debian and centos
+                if isinstance(line, bytes):
+                    line = line.decode("utf-8").strip()
+                else:
+                    line = line.strip()
 
-                        try:
-                            daemon = RE_DAEMON.search(line).groups()[0]
-                        except Exception:
-                            daemon = None
+                if not line or line == "":
+                    continue
 
-                        try:
-                            pid = RE_PID.search(line)
-                            if pid is not None:
-                                pid = pid.groups()[0]
-                        except Exception:
-                            pid = None
+                try:
+                    ts = datetime.strptime(RE_TS.search(line).groups()[0], "%b %d %H:%M:%S")
+                    if last_seen_month > ts.month:
+                        last_seen_year += 1
+                    last_seen_month = ts.month
+                    ts = ts.replace(year=last_seen_year)
+                except Exception as e:
+                    self.target.log.warn(f"Could not convert timestamp line in {log_file}: {e}")
+                    # Set ts to epoch 1970-01-01 if we could not convert
+                    ts = datetime.utcfromtimestamp(0)
 
-                        try:
-                            msg = RE_MSG.search(line).groups()[0]
-                            if msg is None:
-                                msg = line
-                        except Exception:
-                            msg = line
+                try:
+                    daemon = RE_DAEMON.search(line).groups()[0]
+                except Exception:
+                    daemon = None
 
-                        yield MessagesRecord(
-                            ts=ts,
-                            filepath=f.path,
-                            daemon=daemon,
-                            pid=pid,
-                            msg=msg,
-                            raw=line,
-                            _target=self.target,
-                        )
+                try:
+                    pid = RE_PID.search(line)
+                    if pid is not None:
+                        pid = pid.groups()[0]
+                except Exception:
+                    pid = None
+
+                try:
+                    msg = RE_MSG.search(line).groups()[0]
+                    if msg is None:
+                        msg = line
+                except Exception:
+                    msg = line
+
+                yield MessagesRecord(
+                    ts=ts,
+                    daemon=daemon,
+                    pid=pid,
+                    message=msg,
+                    raw=line,
+                    source=str(log_file),
+                    _target=self.target,
+                )
---
+++ tests/test_plugins_os_unix_log_messages.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+from os import stat
+
+from dissect.target.plugins.os.unix.log.messages import MessagesPlugin
+
+from ._utils import absolute_path
+
+
+def test_unix_log_messages_plugin(target_unix, fs_unix):
+
+    data_file = absolute_path("data/unix/logs/messages")
+    fs_unix.map_file("var/log/messages", data_file)
+
+    year = datetime.fromtimestamp(stat(absolute_path("data/unix/logs/messages")).st_ctime).year
+
+    target_unix.add_plugin(MessagesPlugin)
+
+    results = list(target_unix.messages())
+    assert len(results) == 2
+
+    assert results[0].ts == datetime(year, 12, 31, 3, 14, 15)
+    assert results[0].message == "Starting Journal Service..."
+    assert results[0].pid == 1
+    assert results[0].source == "/var/log/messages"
+
+    assert results[1].ts == datetime(year + 1, 1, 1, 13, 21, 34)
+    assert results[1].message == "Stopped target Swap."
+    assert results[1].pid is None
+    assert results[1].source == "/var/log/messages"
