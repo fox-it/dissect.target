fd647a879680ebb207cbfce8bafb904e760765b6
JSCU-CNI <121175071+JSCU-CNI@users.noreply.github.com>
2023-01-23 17:49:26 +0100

    add unix unit test and implement review comments

--- dissect/target/plugins/os/unix/generic.py
+++ dissect/target/plugins/os/unix/generic.py
@@ -3,14 +3,14 @@ from statistics import median
 from typing import Union
 
 from dissect.target.plugin import Plugin, export
-
+from dissect.util import ts
 
 class GenericPlugin(Plugin):
     def check_compatible(self):
         pass
 
     @export(property=True)
-    def activity(self) -> Union[datetime, str]:
+    def activity(self) -> Union[datetime, None]:
         """Return last seen activity based on filesystem timestamps."""
         var_log = self.target.fs.path("/var/log")
         if not var_log.exists():
@@ -22,12 +22,10 @@ class GenericPlugin(Plugin):
                 last_seen = f.stat().st_mtime
 
         if last_seen != 0:
-            return datetime.fromtimestamp(last_seen)
-
-        return "unknown"
+            return ts.from_unix(last_seen)
 
     @export(property=True)
-    def installdate(self) -> Union[datetime, str]:
+    def install_date(self) -> Union[datetime, None]:
         """Return the likely install date of the filesystem."""
 
         files = [
@@ -49,7 +47,7 @@ class GenericPlugin(Plugin):
                 dates.append(p.stat().st_mtime)
 
         if dates:
-            return datetime.utcfromtimestamp(median(dates))
+            return ts.from_unix(median(dates))
 
         # As a fallback if none of the above files are found, we attempt
         # to discover the birth date of the root filesystem.
@@ -57,6 +55,4 @@ class GenericPlugin(Plugin):
         # If the change time and modify time of the root dir are equal,
         # this is likely the creation timestamp of the root fs.
         if self.target.fs.stat("/").st_ctime == self.target.fs.stat("/").st_mtime:
-            return datetime.utcfromtimestamp(self.target.fs.stat("/").st_ctime)
-
-        return "unknown"
+            return ts.from_unix(self.target.fs.stat("/").st_ctime)
