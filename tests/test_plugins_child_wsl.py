3c44f3e0198ef83b0bc7e0bbb1394e14f174cc3b
Schamper <1254028+Schamper@users.noreply.github.com>
2023-02-01 16:15:30 +0100

    Change WSL test

--- tests/test_plugins_child_wsl.py
+++ tests/test_plugins_child_wsl.py
@@ -1,14 +1,12 @@
-from dissect.target.plugins.child.wsl import WSLTargetPlugin
+import io
 
-from ._utils import absolute_path
+from dissect.target.plugins.child.wsl import WSLTargetPlugin
 
 
 def test_plugins_child_wsl(target_win_users, fs_win):
-
-    vhdx_file = absolute_path("data/ext4.vhdx")
-    fs_win.map_file(
+    fs_win.map_file_fh(
         "users/john/appdata/local/Packages/CanonicalGroupLimited.Ubuntu22.04LTS_79rhkp1fndgsc/LocalState/ext4.vhdx",
-        vhdx_file,
+        io.BytesIO(),
     )
 
     target_win_users.add_plugin(WSLTargetPlugin)
@@ -20,12 +18,3 @@ def test_plugins_child_wsl(target_win_users, fs_win):
         children[0].path
         == "C:/Users/John/AppData/Local/Packages/CanonicalGroupLimited.Ubuntu22.04LTS_79rhkp1fndgsc/LocalState/ext4.vhdx"  # noqa E501
     )
-
-    child_targets = list(target_win_users.open_children())
-    assert len(child_targets) == 1
-
-    assert child_targets[0].hostname == "DESKTOP-123567"
-    assert child_targets[0].os == "linux"
-    assert child_targets[0].version == "Ubuntu 22.04.1 LTS (Jammy Jellyfish)"
-    assert len(list(child_targets[0].users())) == 27
-    assert list(child_targets[0].users())[-1].name == "user"
