8a6a42ee026fca2ed09ace705699c867bb43cc48
Zawadi Done <hi@zawadidone.nl>
2023-02-22 13:09:30 +0100

    Add VSS folders to test

--- tests/test_loaders_velociraptor.py
+++ tests/test_loaders_velociraptor.py
@@ -7,7 +7,15 @@ from ._utils import mkdirs
 
 def test_velociraptor_loader_windows_ntfs(mock_target, tmpdir_name):
     root = Path(tmpdir_name)
-    mkdirs(root, ["uploads.json", "uploads/mft/%5C%5C.%5CC%3A/windows/system32"])
+    mkdirs(
+        root,
+        [
+            "uploads.json",
+            "uploads/mft/%5C%5C.%5CC%3A/windows/system32",
+            "uploads/mft/%5C%5C%3F%5CGLOBALROOT%5CDevice%5CHarddiskVolumeShadowCopy1",
+            "uploads/mft/%5C%5C%3F%5CGLOBALROOT%5CDevice%5CHarddiskVolumeShadowCopy2",
+        ],
+    )
 
     assert VelociraptorLoader.detect(root) is True
 
@@ -15,6 +23,7 @@ def test_velociraptor_loader_windows_ntfs(mock_target, tmpdir_name):
     loader.map(mock_target)
 
     assert len(mock_target.filesystems) == 1
+    assert "C." in str(mock_target.filesystems[0].base_path)
 
 
 def test_dir_loader_linux(mock_target, tmpdir_name):
