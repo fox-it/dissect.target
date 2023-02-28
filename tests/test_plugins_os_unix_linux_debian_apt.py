6d1aec642fa16c69950ff364c3c97f8ac820b371
JSCU-CNI <121175071+JSCU-CNI@users.noreply.github.com>
2023-02-28 10:29:44 +0100

    implement review comments

--- tests/test_plugins_os_unix_linux_debian_apt.py
+++ tests/test_plugins_os_unix_linux_debian_apt.py
@@ -1,7 +1,6 @@
-from datetime import timedelta, timezone
+from datetime import datetime, timezone
 
 import pytest
-from flow.record.fieldtypes import datetime as dt
 
 from dissect.target.plugins.os.unix.linux.debian.apt import AptPlugin
 
@@ -17,7 +16,7 @@ from ._utils import absolute_path
     ],
 )
 def test_apt_logs(test_file, target_unix, fs_unix) -> None:
-    tz = timezone(timedelta(hours=0))
+    tz = timezone.utc
     data_file = absolute_path(f"data/plugins/os/unix/linux/debian/apt/{test_file}")
     fs_unix.map_file(f"/var/log/apt/{test_file}", data_file)
     target_unix.add_plugin(AptPlugin)
@@ -28,13 +27,13 @@ def test_apt_logs(test_file, target_unix, fs_unix) -> None:
     for record in results:
         assert record.package_manager == "apt"
 
-    assert results[0].ts == dt(2022, 9, 2, 6, 36, 31, tzinfo=tz)
+    assert results[0].ts == datetime(2022, 9, 2, 6, 36, 31, tzinfo=tz)
     assert results[0].operation == "update"
     assert results[0].package_name == "libcurl4:amd64 (7.68.0-1ubuntu2.12, 7.68.0-1ubuntu2.13)"
     assert results[0].command == "/usr/bin/unattended-upgrade"
     assert results[0].requested_by_user is None
 
-    assert results[-1].ts == dt(2022, 9, 7, 7, 48, 28, tzinfo=tz)
+    assert results[-1].ts == datetime(2022, 9, 7, 7, 48, 28, tzinfo=tz)
     assert results[-1].operation == "update"
     assert results[-1].package_name == "linux-generic:amd64 (5.4.0.125.126, 5.4.0.126.127)"
     assert results[-1].command == "/usr/bin/unattended-upgrade"
--- tests/test_plugins_os_unix_linux_suse_zypper.py
+++ tests/test_plugins_os_unix_linux_suse_zypper.py
@@ -1,7 +1,6 @@
-from datetime import timedelta, timezone
+from datetime import datetime, timezone
 
 import pytest
-from flow.record.fieldtypes import datetime as dt
 
 from dissect.target.plugins.os.unix.linux.suse.zypper import ZypperPlugin
 
@@ -17,7 +16,7 @@ from ._utils import absolute_path
     ],
 )
 def test_zypper_logs(test_file, target_unix, fs_unix) -> None:
-    tz = timezone(timedelta(hours=0))
+    tz = timezone.utc
     data_file = absolute_path(f"data/plugins/os/unix/linux/suse/zypp/{test_file}")
     fs_unix.map_file(f"/var/log/zypp/{test_file}", data_file)
     target_unix.add_plugin(ZypperPlugin)
@@ -28,13 +27,13 @@ def test_zypper_logs(test_file, target_unix, fs_unix) -> None:
     for record in results:
         assert record.package_manager == "zypper"
 
-    assert results[0].ts == dt(2022, 12, 16, 12, 56, 23, tzinfo=tz)
+    assert results[0].ts == datetime(2022, 12, 16, 12, 56, 23, tzinfo=tz)
     assert results[0].operation == "other"
     assert results[0].package_name is None
     assert results[0].command == "zypper install unzip"
     assert results[0].requested_by_user == "root"
 
-    assert results[-1].ts == dt(2022, 12, 16, 13, 2, 44, tzinfo=tz)
+    assert results[-1].ts == datetime(2022, 12, 16, 13, 2, 44, tzinfo=tz)
     assert results[-1].operation == "install"
     assert results[-1].package_name == "yast2-4.5.20-1.1:x86_64"
     assert results[-1].command is None
