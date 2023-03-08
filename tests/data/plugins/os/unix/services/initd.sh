72b6d91911a944095e4ef96695a0273cfb050155
JSCU-CNI <121175071+JSCU-CNI@users.noreply.github.com>
2023-03-08 11:34:52 +0100

    improve unix services plugin

---
+++ tests/data/plugins/os/unix/services/initd.sh
@@ -0,0 +1,16 @@
+#! /bin/sh
+
+### BEGIN INIT INFO
+# Provides:             exampled
+# Required-Start:	    $foo $bar
+# Required-Stop:	    $foo $bar
+# Default-Start:	    2 3 4 5
+# Default-Stop:		    
+# Short-Description:	Example initd service
+### END INIT INFO
+
+set -e
+
+echo "Example initd service"
+
+exit 0
