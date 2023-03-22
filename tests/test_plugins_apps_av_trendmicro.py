cd4e15d95e7cc2152b222f5b5c8b056634f9ddd9
cecinestpasunepipe <110607403+cecinestpasunepipe@users.noreply.github.com>
2023-03-22 15:58:25 +0100

    Implement feedback (DIS-1524)

--- tests/test_plugins_apps_av_trendmicro.py
+++ tests/test_plugins_apps_av_trendmicro.py
@@ -1,6 +1,10 @@
 from flow.record.fieldtypes import datetime as dt
 
-from dissect.target.plugins.apps.av.trendmicro import TrendMicroPlugin
+from dissect.target.plugins.apps.av.trendmicro import (
+    TrendMicroPlugin,
+    TrendMicroWFFirewallRecord,
+    TrendMicroWFLogRecord,
+)
 
 from ._utils import absolute_path
 
@@ -11,6 +15,7 @@ def test_trendmicro_plugin_worryfree_firewall(target_win, fs_win):
     target_win.add_plugin(TrendMicroPlugin)
     records = list(target_win.trendmicro.wffirewall())
     assert len(records) == 1
+    assert isinstance(records[0], type(TrendMicroWFFirewallRecord()))
     assert records[0].ts == dt("2023-03-14T09:42:55Z")
     assert str(records[0].local_ip) == "127.0.0.1"
     assert str(records[0].remote_ip) == "255.255.255.255"
@@ -26,6 +31,7 @@ def test_trendmicro_plugin_worryfree_log(target_win, fs_win):
     target_win.add_plugin(TrendMicroPlugin)
     records = list(target_win.trendmicro.wflogs())
     assert len(records) == 1
+    assert isinstance(records[0], type(TrendMicroWFLogRecord()))
     assert records[0].ts == dt("2023-03-10T15:06:19Z")
     assert records[0].threat == "Eicar_test_file"
     assert str(records[0].path) == "C:\\Users\\admin\\Desktop\\test\\eicarcom2.zip"
