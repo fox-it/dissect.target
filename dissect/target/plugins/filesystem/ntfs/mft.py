9a11c3b7137366552d41c3c76bd705c6a731d29d
Miauwkeru <Miauwkeru@users.noreply.github.com>
2023-05-08 07:37:48 +0000

    Add a help message to mft --compact
    (DIS-1891)

--- dissect/target/plugins/filesystem/ntfs/mft.py
+++ dissect/target/plugins/filesystem/ntfs/mft.py
@@ -115,7 +115,7 @@ class MftPlugin(Plugin):
             FilesystemFilenameCompactRecord,
         ]
     )
-    @arg("--compact", action="store_true")
+    @arg("--compact", action="store_true", help="Compacts the mft entry timestamps into a single record.")
     def mft(self, compact: bool = False):
         """Return the MFT records of all NTFS filesystems.
 
