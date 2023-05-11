3c753b311b92671dc2054a7abb4ff5e9445f19dc
Miauwkeru <Miauwkeru@users.noreply.github.com>
2023-05-11 12:12:08 +0000

    Check if plugin was attached to target

--- dissect/target/tools/utils.py
+++ dissect/target/tools/utils.py
@@ -3,7 +3,7 @@ import inspect
 import json
 from datetime import datetime
 from pathlib import Path
-from typing import Any, Callable, Dict, List, Optional, Tuple, Type
+from typing import Any, Callable, Dict, List, Optional, Tuple, Type, Union, Iterator
 
 from dissect.target import Target
 from dissect.target.exceptions import UnsupportedPluginError
@@ -149,8 +149,41 @@ def execute_function_on_target(
 
     cli_params = cli_params or []
 
-    plugin_class = func.class_object
+    target_attr = get_target_attribute(target, func)
+    plugin_method, parser = plugin_function_with_argparser(target_attr)
 
+    if parser:
+        parsed_params, cli_params = parser.parse_known_args(cli_params)
+        method_kwargs = vars(parsed_params)
+        value = plugin_method(**method_kwargs)
+    else:
+        value = target_attr
+
+    output_type = getattr(plugin_method, "__output__", "default") if plugin_method else "default"
+    return (output_type, value, cli_params)
+
+
+def get_target_attribute(target: Target, func: PluginFunction) -> Union[Plugin, Callable]:
+    """Retrieves the function attribute from the target.
+
+    If the function does not exist yet, it will attempt to load it into the target.
+
+    Args:
+        target: The platform we wish to run the function on.
+        func: The function to run on the target.
+
+    Returns:
+        The function, either plugin or a callable to execute.
+
+    Raises:
+        UnsupportedPluginError: When the function was incompatible with the target.
+    """
+    if target.has_function(func.method_name):
+        # If the function is already attached, use the one inside the target.
+        _, target_attr = target.get_function(func.method_name)
+        return target_attr
+
+    plugin_class = func.class_object
     if issubclass(plugin_class, OSPlugin):
         # OS plugin does not need to be added
         plugin_class = target._os_plugin
@@ -162,7 +195,14 @@ def execute_function_on_target(
                 f"Unsupported function `{func.method_name}` for target with plugin {func.class_object}", cause=e
             )
 
-    plugin_obj, target_attr = plugin_factory(target, plugin_class, func.method_name)
+    _, target_attr = plugin_factory(target, plugin_class, func.method_name)
+    return target_attr
+
+
+def plugin_function_with_argparser(
+    target_attr: Union[Plugin, Callable]
+) -> tuple[Optional[Iterator], Optional[argparse.ArgumentParser]]:
+    """Resolves which plugin function to execute, and creates the argument parser for said plugin."""
     plugin_method = None
     parser = None
 
@@ -178,16 +218,7 @@ def execute_function_on_target(
     elif callable(target_attr):
         plugin_method = target_attr
         parser = generate_argparse_for_bound_method(target_attr)
-
-    if parser:
-        parsed_params, cli_params = parser.parse_known_args(cli_params)
-        method_kwargs = vars(parsed_params)
-        value = plugin_method(**method_kwargs)
-    else:
-        value = target_attr
-
-    output_type = getattr(plugin_method, "__output__", "default") if plugin_method else "default"
-    return (output_type, value, cli_params)
+    return plugin_method, parser
 
 
 def persist_execution_report(output_dir: Path, report_data: Dict, timestamp: datetime) -> Path:
