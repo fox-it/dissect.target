f7db22fec676dc309ab2f050f01b69e21f280993
Zawadi <zawadi.done@huntandhackett.com>
2023-07-21 10:15:19 +0200

    Use `target_unix_users`

--- tests/test_plugins_apps_webhosting_cpanel.py
+++ tests/test_plugins_apps_webhosting_cpanel.py
@@ -5,22 +5,19 @@ from dissect.target.plugins.apps.webhosting.cpanel import CPanelPlugin
 from ._utils import absolute_path
 
 
-def test_cpanel_plugin(target_unix, fs_unix):
+def test_cpanel_plugin(target_unix_users, fs_unix):
     data_file = absolute_path("data/plugins/apps/webhosting/cpanel/lastlogin")
-    fs_unix.map_file("/home/test/.lastlogin", data_file)
+    fs_unix.map_file("/home/user/.lastlogin", data_file)
 
     fs_unix.makedirs("/usr/local/cpanel/logs")
 
-    passwd_file = absolute_path("data/unix/configs/passwd")
-    fs_unix.map_file("/etc/passwd", passwd_file)
+    target_unix_users.add_plugin(CPanelPlugin)
 
-    target_unix.add_plugin(CPanelPlugin)
-
-    results = list(target_unix.cpanel.lastlogin())
+    results = list(target_unix_users.cpanel.lastlogin())
 
     record = results[0]
 
     assert len(results) == 4
     assert record.ts == datetime(2023, 6, 27, 13, 22, 13, tzinfo=timezone.utc)
-    assert record.user == "test"
+    assert record.user == "user"
     assert record.remote_ip == "8.8.8.8"
