0653c6aed6bdb1c8ccb0746c9ab4914418422734
Schamper <1254028+Schamper@users.noreply.github.com>
2023-07-25 12:20:57 +0200

    Add support for Linux MD RAID

---
+++ dissect/target/volumes/md.py
@@ -0,0 +1,47 @@
+from collections import defaultdict
+from typing import BinaryIO, Iterator, Union
+
+from dissect.volume import md
+from dissect.volume.md.md import find_super_block
+
+from dissect.target.volume import LogicalVolumeSystem, Volume
+
+
+class MdVolumeSystem(LogicalVolumeSystem):
+    def __init__(self, fh: Union[BinaryIO, list[BinaryIO]], *args, **kwargs):
+        self.md = md.MD(fh)
+        super().__init__(fh, *args, **kwargs)
+
+    @classmethod
+    def open_all(cls, volumes: list[BinaryIO]) -> Iterator[LogicalVolumeSystem]:
+        devices = defaultdict(list)
+
+        for vol in volumes:
+            if not cls.detect_volume(vol):
+                continue
+
+            device = md.Device(vol)
+            devices[device.set_uuid].append(device)
+
+        for devs in devices.values():
+            try:
+                yield cls(devs)
+            except Exception:
+                continue
+
+    @staticmethod
+    def _detect(fh: BinaryIO) -> bool:
+        vols = [fh] if not isinstance(fh, list) else fh
+        for vol in vols:
+            if MdVolumeSystem.detect_volume(vol):
+                return True
+        return False
+
+    @staticmethod
+    def _detect_volume(fh: BinaryIO) -> bool:
+        offset, _, _ = find_super_block(fh)
+        return offset is not None
+
+    def _volumes(self) -> Iterator[Volume]:
+        fh = self.md.open()
+        yield Volume(fh, 1, None, fh.size, None, self.md.name, self.md.uuid, raw=self.md, vs=self)
