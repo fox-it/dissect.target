91b863e7cd7d771f53a6ef65fe1f1f8150cc999c
DevJoost <joostgerritsen16@gmail.com>
2023-08-18 08:59:28 +0200

    Add lsmod and modules plugin

---
+++ dissect/target/plugins/os/unix/modules.py
@@ -0,0 +1,71 @@
+from dataclasses import dataclass
+from typing import Iterator, Optional
+
+from dissect.target.helpers.record import TargetRecordDescriptor
+from dissect.target.plugin import Plugin, export, internal
+from dissect.target.target import Target
+
+ModuleRecord = TargetRecordDescriptor(
+    "linux/module",
+    [
+        ("string", "name"),
+        ("varint", "size"),
+        ("varint", "refcount"),
+        ("stringlist", "modules_referred"),
+        ("uri", "source"),
+    ],
+)
+
+
+@dataclass
+class Module:
+    path: Optional[str] = None
+    name: Optional[str] = None
+    size: Optional[int] = None
+    refcnt: Optional[int] = None
+    modules_referred: Optional[list[str]] = None
+
+
+class ModulePlugin(Plugin):
+    def __init__(self, target: Target):
+        super().__init__(target)
+        self._module_paths = list(self.target.fs.glob("/sys/module/*"))
+
+    def check_compatible(self) -> bool:
+        return len(self._module_paths) > 0
+
+    @internal
+    def iterate_modules(self):
+        for module_path in self._module_paths:
+            if self.target.fs.path(module_path + "/initstate").exists():
+                module_folder = self.target.fs.path(module_path)
+                module = Module(module_path, module_folder.name)
+                module.size = int(module_folder.joinpath("coresize").read_text())
+                module.refcnt = int(module_folder.joinpath("refcnt").read_text())
+                if module_folder.joinpath(
+                    "holders"
+                ).exists():  # Normally the holders folder should exist, however Acquire doesn't acquire a folder if its empty
+                    module.modules_referred = [item.name for item in module_folder.joinpath("holders").iterdir()]
+                else:
+                    module.modules_referred = []
+                yield module
+
+    @export(record=ModuleRecord)
+    def modules(self) -> Iterator[ModuleRecord]:
+        """Return information about active kernel modules."""
+        for module in self.iterate_modules():
+            yield ModuleRecord(
+                name=module.name,
+                size=module.size,
+                refcount=module.refcnt,
+                modules_referred=module.modules_referred,
+                source=module.path,
+            )
+
+    @export(property=True)
+    def lsmod(self):
+        """Return information about active kernel modules in lsmod format"""
+        lsmod_output = f"\n{'Module ':<28} {'Size':<7}  Used by\n"
+        for module in self.iterate_modules():
+            lsmod_output += f"{module.name:<28} {module.size:<7}  {module.refcnt} {','.join(module.modules_referred)}\n"
+        return lsmod_output
---
+++ tests/test_plugins_os_unix_modules.py
@@ -0,0 +1,20 @@
+from dissect.target.plugins.os.unix.modules import ModulePlugin
+
+from ._utils import absolute_path
+
+
+def test_modules_plugin(target_unix, fs_unix):
+    test_folder = absolute_path("data/unix/module")
+    fs_unix.map_dir("/sys/module", test_folder)
+
+    target_unix.add_plugin(ModulePlugin)
+    results = list(target_unix.modules())
+    assert len(results) == 2
+    assert results[0].name == "modulea"
+    assert results[0].size == 1
+    assert results[0].refcount == 3
+    assert results[0].modules_referred == ["holdera"]
+    assert results[1].name == "moduleb"
+    assert results[1].size == 2
+    assert results[1].refcount == 4
+    assert results[1].modules_referred == ["holdera", "holderb"]
