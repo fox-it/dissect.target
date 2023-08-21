a9fb66ede99926a55b86ad694081a2c210cd8950
Schamper <1254028+Schamper@users.noreply.github.com>
2023-08-21 18:29:04 +0200

    Reuse more code

--- dissect/target/loaders/velociraptor.py
+++ dissect/target/loaders/velociraptor.py
@@ -3,17 +3,13 @@ from __future__ import annotations
 import logging
 import zipfile
 from pathlib import Path
-from typing import Optional, Union
+from typing import TYPE_CHECKING, Optional, Union
 
-from dissect.target import filesystem, target
-from dissect.target.filesystems.dir import DirectoryFilesystem
-from dissect.target.filesystems.zip import (
-    ZipFilesystemDirectoryEntry,
-    ZipFilesystemEntry,
-)
-from dissect.target.helpers import fsutil, loaderutil
 from dissect.target.loaders.dir import DirLoader, find_dirs, map_dirs
 from dissect.target.plugin import OperatingSystem
+
+if TYPE_CHECKING:
+    from dissect.target import Target
 
 log = logging.getLogger(__name__)
 
@@ -22,13 +18,13 @@ UNIX_ACCESSORS = ["file", "auto"]
 WINDOWS_ACCESSORS = ["mft", "ntfs", "lazy_ntfs", "ntfs_vss", "auto"]
 
 
-def find_fs_directories(path: Path, is_zip: bool) -> tuple[Optional[OperatingSystem], Optional[list[Path]]]:
+def find_fs_directories(path: Path) -> tuple[Optional[OperatingSystem], Optional[list[Path]]]:
     # As of Velociraptor version 0.7.0 the structure of the Velociraptor Offline Collector varies by operating system.
     # Generic.Collectors.File (Unix) uses the accessors file and auto.
     # Generic.Collectors.File (Windows) and Windows.KapeFiles.Targets (Windows) uses the accessors
     # mft, ntfs, lazy_ntfs, ntfs_vss and auto.
 
-    fs_root = zipfile.Path(path, at=FILESYSTEMS_ROOT) if is_zip else path.joinpath(FILESYSTEMS_ROOT)
+    fs_root = path.joinpath(FILESYSTEMS_ROOT)
 
     # Unix
     for accessor in UNIX_ACCESSORS:
@@ -64,15 +60,14 @@ class VelociraptorLoader(DirLoader):
     def __init__(self, path: Union[Path, str], **kwargs):
         super().__init__(path)
 
-        self.is_zip = str(path).lower().endswith(".zip")
-
-        if self.is_zip:
+        if path.suffix == ".zip":
             log.warning(
-                f"Zip file {path!r} is compressed, which will slightly affect performance. "
-                "Consider uncompressing the archive before passing the unzipped folder to Dissect."
+                f"Velociraptor target {path!r} is compressed, which will slightly affect performance. "
+                "Consider uncompressing the archive and passing the uncompressed folder to Dissect."
             )
-
-            self.zip = zipfile.ZipFile(path)
+            self.root = zipfile.Path(path)
+        else:
+            self.root = path
 
     @staticmethod
     def detect(path: Path) -> bool:
@@ -84,79 +79,25 @@ class VelociraptorLoader(DirLoader):
         #   results/
         #   uploads.json
         #   [...] other files related to the collection
-        if (
-            str(path).lower().endswith(".zip")
-            and zipfile.Path(path, at="uploads/").exists()
-            and zipfile.Path(path, at="uploads.json").exists()
-        ):
-            _, dirs = find_fs_directories(path, True)
+        if path.suffix == ".zip":
+            path = zipfile.Path(path)
+
+        if path.joinpath(FILESYSTEMS_ROOT).exists() and path.joinpath("uploads.json").exists():
+            _, dirs = find_fs_directories(path)
             return bool(dirs)
-        else:
-            if path.joinpath(FILESYSTEMS_ROOT).exists() and path.joinpath("uploads.json").exists():
-                _, dirs = find_fs_directories(path, False)
-                return bool(dirs)
-            return False
 
-    def map(self, target: target.Target) -> None:
-        os_type, dirs = find_fs_directories(self.path, self.is_zip)
+        return False
+
+    def map(self, target: Target) -> None:
+        os_type, dirs = find_fs_directories(self.root)
         if os_type == OperatingSystem.WINDOWS:
-            # Velociraptor doesn't have the correct filenames the paths $J and $Secure:$SDS".
-            if not self.is_zip:
-                for path in dirs:
-                    dfs = DirectoryFilesystem(path, alt_separator="\\", case_sensitive=False)
-                    target.filesystems.add(dfs)
-
-                    loaderutil.add_virtual_ntfs_filesystem(
-                        target,
-                        dfs,
-                        usnjrnl_path="$Extend/$UsnJrnl%3A$J",
-                        sds_path="$Secure%3A$SDS",
-                    )
-
-                    parts = str(path).split("/")
-                    root = str(self.path).split("/")
-                    vol_name = parts[len(root) + 2].strip("%3A")[-1].lower() + ":"
-                    if vol_name == "c:":
-                        target.fs.mount("c:", dfs)
-                        vol_name == "sysvol"
-
-                    target.fs.mount(vol_name, dfs)
-            else:
-                volumes = {}
-
-                for zipinfo in self.zip.infolist():
-                    filename = zipinfo.filename
-                    if not any([filename.startswith(dir.at) for dir in dirs]):
-                        continue
-
-                    parts = filename.split("/")
-                    volume_name = parts[2].strip("%3A")[-1].lower()
-                    if volume_name == "c":
-                        volume_name = "sysvol"
-
-                    if volume_name not in volumes:
-                        vol = filesystem.VirtualFilesystem(case_sensitive=False)
-                        vol.zip = self.zip
-                        volumes[volume_name] = vol
-                        target.filesystems.add(vol)
-
-                    volume = volumes[volume_name]
-                    mname = "/".join(parts[3:])
-
-                    entry_cls = ZipFilesystemDirectoryEntry if zipinfo.is_dir() else ZipFilesystemEntry
-                    entry = entry_cls(volume, fsutil.normpath(mname), zipinfo)
-                    volume.map_file_entry(entry.path, entry)
-
-                for vol_name, vol in volumes.items():
-                    loaderutil.add_virtual_ntfs_filesystem(
-                        target,
-                        vol,
-                        usnjrnl_path="$Extend/$UsnJrnl%3A$J",
-                        sds_path="$Secure%3A$SDS",
-                    )
-
-                    target.fs.mount(vol_name, vol)
-                    if vol_name == "sysvol":
-                        target.fs.mount("c:", vol)
+            # Velociraptor doesn't have the correct filenames for the paths "$J" and "$Secure:$SDS"
+            map_dirs(
+                target,
+                dirs,
+                os_type,
+                usnjrnl_path="$Extend/$UsnJrnl%3A$J",
+                sds_path="$Secure%3A$SDS",
+            )
         else:
             map_dirs(target, dirs, os_type)
