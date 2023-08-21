d4801ffa7472bf5ecc3cbcedd9ad8006672366c1
DevJoost <joostgerritsen16@gmail.com>
2023-08-21 14:20:21 +0200

    Fixing test

--- tests/test_plugins_os_unix_modules.py
+++ tests/test_plugins_os_unix_modules.py
@@ -13,8 +13,8 @@ def test_modules_plugin(target_unix, fs_unix):
     assert results[0].name == "modulea"
     assert results[0].size == 1
     assert results[0].refcount == 3
-    assert results[0].modules_referred == ["holdera"]
+    assert results[0].used_by == ["holdera"]
     assert results[1].name == "moduleb"
     assert results[1].size == 2
     assert results[1].refcount == 4
-    assert results[1].modules_referred == ["holdera", "holderb"]
+    assert results[1].used_by == ["holdera", "holderb"]
