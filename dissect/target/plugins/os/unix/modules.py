acca0472bc53bd9bc7f2a84b32729b180431949e
DevJoost <joostgerritsen16@gmail.com>
2023-08-21 11:23:58 +0200

    Implement changes suggested in review

--- dissect/target/plugins/os/unix/modules.py
+++ dissect/target/plugins/os/unix/modules.py
@@ -6,66 +6,65 @@ from dissect.target.plugin import Plugin, export, internal
 from dissect.target.target import Target
 
 ModuleRecord = TargetRecordDescriptor(
-    "linux/module",
+    "unix/module",
     [
         ("string", "name"),
         ("varint", "size"),
         ("varint", "refcount"),
-        ("stringlist", "modules_referred"),
-        ("uri", "source"),
+        ("string[]", "used_by"),
+        ("path", "source"),
     ],
 )
 
 
 @dataclass
 class Module:
-    path: Optional[str] = None
-    name: Optional[str] = None
-    size: Optional[int] = None
-    refcnt: Optional[int] = None
-    modules_referred: Optional[list[str]] = None
+    path: str
+    name: str
+    size: int
+    refcnt: int
+    used_by: list[str]
 
 
 class ModulePlugin(Plugin):
     def __init__(self, target: Target):
         super().__init__(target)
-        self._module_paths = list(self.target.fs.glob("/sys/module/*"))
+        self._module_paths = list(self.target.fs.path("/sys/module").iterdir())
 
     def check_compatible(self) -> bool:
         return len(self._module_paths) > 0
 
     @internal
-    def iterate_modules(self) -> Iterator[Module]:
+    def _iterate_modules(self) -> Iterator[Module]:
         for module_path in self._module_paths:
-            if self.target.fs.path(module_path + "/initstate").exists():
-                module_folder = self.target.fs.path(module_path)
-                module = Module(module_path, module_folder.name)
-                module.size = int(module_folder.joinpath("coresize").read_text())
-                module.refcnt = int(module_folder.joinpath("refcnt").read_text())
-                if module_folder.joinpath(
-                    "holders"
-                ).exists():  # Normally the holders folder should exist, however Acquire doesn't acquire a folder if its empty
-                    module.modules_referred = [item.name for item in module_folder.joinpath("holders").iterdir()]
-                else:
-                    module.modules_referred = []
-                yield module
+            if module_path.joinpath("initstate").exists():
+                # Normally the holders folder should exist, however Acquire currently doesn't collect a folder if its empty
+                holders = []
+                if (holders_path := module_path.joinpath("holders")).exists():
+                    holders = [item.name for item in holders_path.iterdir()]
+                yield Module(
+                    module_path,
+                    module_path.name,
+                    int(module_path.joinpath("coresize").read_text()),
+                    int(module_path.joinpath("refcnt").read_text()),
+                    holders,
+                )
 
     @export(record=ModuleRecord)
     def modules(self) -> Iterator[ModuleRecord]:
         """Return information about active kernel modules."""
-        for module in self.iterate_modules():
+        for module in self._iterate_modules():
             yield ModuleRecord(
                 name=module.name,
                 size=module.size,
                 refcount=module.refcnt,
-                modules_referred=module.modules_referred,
+                used_by=module.used_by,
                 source=module.path,
             )
 
     @export(output="yield")
-    def lsmod(self):
+    def lsmod(self) -> Iterator[str]:
         """Return information about active kernel modules in lsmod format"""
-        lsmod_output = f"{'Module ':<28} {'Size':<7}  Used by\n"
-        for module in self.iterate_modules():
-            lsmod_output += f"{module.name:<28} {module.size:<7}  {module.refcnt} {','.join(module.modules_referred)}\n"
-        yield lsmod_output
+        yield f"{'Module ':<28} {'Size':<7}  Used by"
+        for module in self._iterate_modules():
+            yield f"{module.name:<28} {module.size:<7}  {module.refcnt} {','.join(module.used_by)}"
