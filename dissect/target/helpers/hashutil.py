6e10501b2db8b26732972fd2cd6da4924b8c2711
pyrco <105293448+pyrco@users.noreply.github.com>
2023-11-07 10:39:11 +0100

    Fix the hashutil functions to properly deal with path record field types (#432)
    (DIS-2526)

--- dissect/target/helpers/hashutil.py
+++ dissect/target/helpers/hashutil.py
@@ -1,14 +1,16 @@
 from __future__ import annotations
 
 import hashlib
-from typing import TYPE_CHECKING
+import warnings
+from typing import TYPE_CHECKING, BinaryIO, Union
 
-from flow.record import GroupedRecord, Record, RecordDescriptor
-from flow.record.fieldtypes import path, posix_path, windows_path
+from flow.record import GroupedRecord, Record, RecordDescriptor, fieldtypes
 
 from dissect.target.exceptions import FileNotFoundError, IsADirectoryError
 
 if TYPE_CHECKING:
+    from hashlib._hashlib import HASH
+
     from dissect.target.target import Target
 
 BUFFER_SIZE = 32768
@@ -22,7 +24,7 @@ HashRecord = RecordDescriptor(
 )
 
 
-def _hash(fh, ctx):
+def _hash(fh: BinaryIO, ctx: Union[HASH, list[HASH]]) -> tuple[str]:
     if not isinstance(ctx, list):
         ctx = [ctx]
 
@@ -35,23 +37,23 @@ def _hash(fh, ctx):
     return tuple(c.hexdigest() for c in ctx)
 
 
-def md5(fh):
+def md5(fh: BinaryIO) -> tuple[str]:
     return _hash(fh, hashlib.md5)[0]
 
 
-def sha1(fh):
+def sha1(fh: BinaryIO) -> tuple[str]:
     return _hash(fh, hashlib.sha1)[0]
 
 
-def sha256(fh):
+def sha256(fh: BinaryIO) -> tuple[str]:
     return _hash(fh, hashlib.sha256)[0]
 
 
-def common(fh):
+def common(fh: BinaryIO) -> tuple[str]:
     return _hash(fh, [hashlib.md5, hashlib.sha1, hashlib.sha256])
 
 
-def custom(fh, algos):
+def custom(fh: BinaryIO, algos: list[Union[str, HASH]]) -> tuple[str]:
     if isinstance(algos[0], str):
         ctx = [getattr(hashlib, h) for h in algos]
     else:
@@ -60,34 +62,63 @@ def custom(fh, algos):
     return _hash(fh, ctx)
 
 
-def hash_uri_records(target, record: Record) -> Record:
+def hash_uri_records(target: Target, record: Record) -> Record:
     """Hash uri paths inside the record."""
-    uri_fields = (field for field in record._field_types.items() if issubclass(field[1], path))
+    warnings.warn(
+        (
+            "The hash_uri_records() function is deprecated, and will be removed in dissect.target 3.15. "
+            "Use hash_path_records() instead"
+        ),
+        DeprecationWarning,
+    )
+    return hash_path_records(target, record)
 
-    hashed_uri_records = []
-    for name, _ in uri_fields:
-        try:
-            uri_record = hash_uri(target, getattr(record, name))
-            hashed_uri_records.append(uri_record)
-        except (FileNotFoundError, IsADirectoryError):
-            pass
 
-    if not hashed_uri_records:
-        return record
+def hash_path_records(target: Target, record: Record) -> Record:
+    """Hash files from path fields inside the record."""
+    hashed_paths = []
 
     if target.os == "windows":
-        path_type = windows_path
+        path_type = fieldtypes.windows_path
     else:
-        path_type = posix_path
+        path_type = fieldtypes.posix_path
 
-    hashed_uri_records = list(zip(*hashed_uri_records))
-    hashed_holder = HashRecord(paths=list(map(path_type, hashed_uri_records[0])), digests=hashed_uri_records[1])
+    for field_name, field_type in record._field_types.items():
+        if not issubclass(field_type, fieldtypes.path):
+            continue
 
-    return GroupedRecord(record._desc.name, [record, hashed_holder])
+        path = getattr(record, field_name, None)
+        if path is None:
+            continue
+
+        try:
+            resolved_path = target.resolve(str(path))
+            path_hash = target.fs.hash(resolved_path)
+        except (FileNotFoundError, IsADirectoryError):
+            pass
+        else:
+            resolved_path = path_type(resolved_path)
+            hashed_paths.append((resolved_path, path_hash))
+
+    if not hashed_paths:
+        return record
+
+    paths, digests = zip(*hashed_paths)
+    hash_record = HashRecord(paths=paths, digests=digests)
+
+    return GroupedRecord(record._desc.name, [record, hash_record])
 
 
-def hash_uri(target: Target, path: str) -> Record:
+def hash_uri(target: Target, path: str) -> tuple[str, str]:
     """Hash the target path."""
+    warnings.warn(
+        (
+            "The hash_uri() function is deprecated, and will be removed in dissect.target 3.15."
+            "Use target.fs.hash() instead"
+        ),
+        DeprecationWarning,
+    )
+
     if path is None:
         raise FileNotFoundError()
 
