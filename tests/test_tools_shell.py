4340a71d32d62d0f090b63ed2b15bf634f54e677
JSCU-CNI <121175071+JSCU-CNI@users.noreply.github.com>
2023-11-13 11:10:46 +0100

    move unicode files to uncompressed tar file

--- tests/test_tools_shell.py
+++ tests/test_tools_shell.py
@@ -114,12 +114,12 @@ def test_target_cli_save(target_win, tmp_path, folders, files, save, expected):
 )
 def test_target_cli_unicode_argparse(capsys, monkeypatch, provided_input: str, expected_output: str) -> None:
     with monkeypatch.context() as m:
-        m.setattr("sys.argv", ["target-shell", "tests/data/unicode.tgz"])
-        m.setattr("sys.stdin", StringIO(f"ls charsets/{provided_input}"))
+        m.setattr("sys.argv", ["target-shell", "tests/data/tools/shell/unicode.tar"])
+        m.setattr("sys.stdin", StringIO(f"ls unicode/charsets/{provided_input}"))
 
         target_shell()
         out, err = capsys.readouterr()
-        out = out.replace("unicode.tgz />", "").strip()
+        out = out.replace("unicode.tar />", "").strip()
 
         assert out == expected_output
         assert "unrecognized arguments" not in err
