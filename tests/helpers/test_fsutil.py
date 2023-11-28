1b42460f0824eebe1f52381a704a8f9a6d77f23d
pyrco <105293448+pyrco@users.noreply.github.com>
2023-11-28 14:43:47 +0100

    Fix permission issues when tests are run on windows
    On Windows, files can not be opened when already open. This gives issues
    when using temporary files. By not deleting a temporaryfile after it is
    closed and closing it after it is created, this is circumvented.
    
    By moving all these files into pytest's tmp_path, the tmp_path mechanism
    will take care of removing these temporary files.

--- tests/helpers/test_fsutil.py
+++ tests/helpers/test_fsutil.py
@@ -374,9 +374,9 @@ def test_fs_attrs_no_os_listxattr():
 
 
 def test_target_path_checks_dirfs(tmp_path, target_win):
-    with tempfile.NamedTemporaryFile(dir=tmp_path) as tf:
+    with tempfile.NamedTemporaryFile(dir=tmp_path, delete=False) as tf:
         tf.write(b"dummy")
-        tf.flush()
+        tf.close()
         tmpfile_name = os.path.basename(tf.name)
 
         fs = DirectoryFilesystem(path=tmp_path)
@@ -388,9 +388,9 @@ def test_target_path_checks_dirfs(tmp_path, target_win):
 
 
 def test_target_path_checks_mapped_dir(tmp_path, target_win):
-    with tempfile.NamedTemporaryFile(dir=tmp_path) as tf:
+    with tempfile.NamedTemporaryFile(dir=tmp_path, delete=False) as tf:
         tf.write(b"dummy")
-        tf.flush()
+        tf.close()
         tmpfile_name = os.path.basename(tf.name)
 
         target_win.filesystems.entries[0].map_dir("test-dir", tmp_path)
@@ -408,9 +408,9 @@ def test_target_path_checks_virtual():
 
 
 def test_target_path_backslash_normalisation(target_win, fs_win, tmp_path):
-    with tempfile.NamedTemporaryFile(dir=tmp_path) as tf:
+    with tempfile.NamedTemporaryFile(dir=tmp_path, delete=False) as tf:
         tf.write(b"dummy")
-        tf.flush()
+        tf.close()
 
         fs_win.map_dir("windows/system32/", tmp_path)
         fs_win.map_file("windows/system32/somefile.txt", tf.name)
