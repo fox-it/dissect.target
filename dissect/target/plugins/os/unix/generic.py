2f1fcb3fcf514601a0abc3bb358c447d220187f8
JSCU-CNI <121175071+JSCU-CNI@users.noreply.github.com>
2023-12-06 14:02:03 +0100

    fix activity plugin for unix
    When a target system contains (broken) symlinks in /var/log,
    the activity plugin could break.

--- dissect/target/plugins/os/unix/generic.py
+++ dissect/target/plugins/os/unix/generic.py
@@ -20,6 +20,8 @@ class GenericPlugin(Plugin):
 
         last_seen = 0
         for f in var_log.iterdir():
+            if f.is_symlink() or not f.exists():
+                continue
             if f.stat().st_mtime > last_seen:
                 last_seen = f.stat().st_mtime
 
