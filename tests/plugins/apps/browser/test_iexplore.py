2902f84ad94add809b7383b1ac0e58a2ce993ccf
Schamper <1254028+Schamper@users.noreply.github.com>
2024-01-16 16:51:35 +0100

    Cleanup iexplore test

--- tests/plugins/apps/browser/test_iexplore.py
+++ tests/plugins/apps/browser/test_iexplore.py
@@ -1,5 +1,4 @@
-import gzip
-import tempfile
+from typing import Iterator
 
 import pytest
 
@@ -9,47 +8,31 @@ from dissect.target.plugins.apps.browser import iexplore
 from tests._utils import absolute_path
 
 
-def test_iexplore_history(
-    target_win: Target, fs_win: VirtualFilesystem, tmp_path: str, target_win_users: Target
-) -> None:
-    setup_iexplore(target_win, fs_win, tmp_path, target_win_users)
+@pytest.fixture
+def target_iexplore(target_win_users: Target, fs_win: VirtualFilesystem) -> Iterator[Target]:
+    base_path = "Users\\John\\AppData\\Local\\Microsoft\\Windows\\WebCache"
+    files = [
+        ("WebCacheV01.dat", "_data/plugins/apps/browser/iexplore/WebCacheV01.dat.gz", "gzip"),
+    ]
 
-    records = list(target_win.iexplore.history())
+    for filename, test_path, compression in files:
+        fs_win.map_file("\\".join([base_path, filename]), absolute_path(test_path), compression=compression)
+
+    target_win_users.add_plugin(iexplore.InternetExplorerPlugin)
+
+    yield target_win_users
+
+
+def test_iexplore_history(target_iexplore: Target) -> None:
+    records = list(target_iexplore.iexplore.history())
     assert len(records) == 41
 
-    records = list(target_win.browser.history())
+    records = list(target_iexplore.browser.history())
     assert len(records) == 41
 
 
-def test_iexplore_downloads(
-    target_win: Target, fs_win: VirtualFilesystem, tmp_path: str, target_win_users: Target
-) -> None:
-    setup_iexplore(target_win, fs_win, tmp_path, target_win_users)
-
-    records = list(target_win.iexplore.downloads())
+def test_iexplore_downloads(target_iexplore: Target) -> None:
+    records = list(target_iexplore.iexplore.downloads())
     assert len(records) == 1
     assert records[0].path.as_posix() == "C:\\Users\\John\\Downloads\\archlinux-2023.02.01-x86_64.iso"
     assert records[0].url == "https://mirror.cj2.nl/archlinux/iso/2023.02.01/archlinux-2023.02.01-x86_64.iso"
-
-
-@pytest.fixture
-def setup_iexplore(target_win: Target, fs_win: VirtualFilesystem, tmp_path: str, target_win_users: Target) -> None:
-    cache_archive = absolute_path("_data/plugins/apps/browser/iexplore/WebCacheV01.dat.gz")
-
-    with tempfile.NamedTemporaryFile(dir=tmp_path, delete=False) as tf:
-        with gzip.GzipFile(cache_archive, "rb") as f:
-            tf.write(f.read())
-        tf.flush()
-        tf.close()
-
-        user = target_win_users.user_details.find(username="John")
-        webcache_dir = user.home_path.joinpath("AppData/Local/Microsoft/Windows/WebCache/")
-        webcache_file = webcache_dir.joinpath(iexplore.InternetExplorerPlugin.CACHE_FILENAME)
-
-        webcache_dir = str(webcache_dir)[3:]  # drop C:/
-        webcache_file = str(webcache_file)[3:]  # drop C:/
-
-        fs_win.map_dir(webcache_dir, tmp_path)
-        fs_win.map_file(webcache_file, tf.name)
-
-        target_win.add_plugin(iexplore.InternetExplorerPlugin)
