51d2ebd18252c501ff02939dee167502cac95d25
Stefan de Reuver <9864602+Horofic@users.noreply.github.com>
2024-02-07 15:20:36 +0100

    Update test_gnulocate.py

--- tests/plugins/os/unix/locate/test_gnulocate.py
+++ tests/plugins/os/unix/locate/test_gnulocate.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 from dissect.target.filesystem import VirtualFilesystem
 from dissect.target.plugins.os.unix.locate.gnulocate import (
     GNULocatePlugin,
@@ -16,8 +18,8 @@ def test_gnulocate(target_unix: Target, fs_unix: VirtualFilesystem) -> None:
     assert len(records) == 3575
     assert isinstance(records[0], type(GNULocateRecord()))
 
-    assert records[0].path.as_posix() == "/"
-    assert records[1].path.as_posix() == "/.dockerenv"
+    assert Path(records[0].path).as_posix() == "/"
+    assert Path(records[1].path).as_posix() == "/.dockerenv"
 
     # test namespace plugin
     records = list(target_unix.locate.locate())
@@ -25,5 +27,5 @@ def test_gnulocate(target_unix: Target, fs_unix: VirtualFilesystem) -> None:
     assert len(records) == 3575
     assert isinstance(records[0], type(GNULocateRecord()))
 
-    assert records[0].path.as_posix() == "/"
-    assert records[1].path.as_posix() == "/.dockerenv"
+    assert Path(records[0].path).as_posix() == "/"
+    assert Path(records[1].path).as_posix() == "/.dockerenv"
