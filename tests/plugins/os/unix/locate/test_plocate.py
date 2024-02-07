483a0bf984c1e86868b82986e658f2e6ddc0f8fe
Stefan de Reuver <9864602+Horofic@users.noreply.github.com>
2024-02-07 15:22:29 +0100

    Update test_plocate.py

--- tests/plugins/os/unix/locate/test_plocate.py
+++ tests/plugins/os/unix/locate/test_plocate.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 from dissect.target.filesystem import VirtualFilesystem
 from dissect.target.plugins.os.unix.locate.plocate import PLocatePlugin, PLocateRecord
 from dissect.target.target import Target
@@ -13,5 +15,5 @@ def test_plocate(target_unix: Target, fs_unix: VirtualFilesystem) -> None:
     assert len(records) == 3481
     assert isinstance(records[0], type(PLocateRecord()))
 
-    assert records[0].path.as_posix() == "/.dockerenv"
-    assert records[1].path.as_posix() == "/bin"
+    assert Path(records[0].path).as_posix() == "/.dockerenv"
+    assert Path(records[1].path).as_posix() == "/bin"
