6e18a741bd596718ab7c7cd3455e8da725d77ac4
Stefan de Reuver <9864602+Horofic@users.noreply.github.com>
2024-02-07 13:40:56 +0100

    Add .as_posix() to test_mlocate.py

--- tests/plugins/os/unix/locate/test_mlocate.py
+++ tests/plugins/os/unix/locate/test_mlocate.py
@@ -15,25 +15,25 @@ def test_mlocate(target_unix: Target, fs_unix: VirtualFilesystem) -> None:
 
     root_directory = records[0]
     assert isinstance(root_directory, type(MLocateRecord()))
-    assert root_directory.parent == "/"
-    assert root_directory.path == "/.dockerenv"
+    assert root_directory.parent.as_posix() == "/"
+    assert root_directory.path.as_posix() == "/.dockerenv"
     assert root_directory.ts == datetime(1970, 1, 1, 0, 0, 0, tzinfo=timezone.utc)
 
     entry = records[1]
     assert isinstance(entry, type(MLocateRecord()))
-    assert entry.parent == "/"
-    assert entry.path == "/bin"
+    assert entry.parent.as_posix() == "/"
+    assert entry.path.as_posix() == "/bin"
     assert entry.type == "file"  # symlink
 
     entry = records[2]
     assert isinstance(entry, type(MLocateRecord()))
-    assert entry.parent == "/"
-    assert entry.path == "/boot"
+    assert entry.parent.as_posix() == "/"
+    assert entry.path.as_posix() == "/boot"
     assert entry.type == "directory"
 
     entry = records[1337]
     assert isinstance(entry, type(MLocateRecord()))
-    assert entry.parent == "/usr/lib/x86_64-linux-gnu/perl-base/unicore/lib/Bc"
-    assert entry.path == "/usr/lib/x86_64-linux-gnu/perl-base/unicore/lib/Bc/WS.pl"
+    assert entry.parent.as_posix() == "/usr/lib/x86_64-linux-gnu/perl-base/unicore/lib/Bc"
+    assert entry.path.as_posix() == "/usr/lib/x86_64-linux-gnu/perl-base/unicore/lib/Bc/WS.pl"
     assert entry.type == "file"
     assert entry.ts == datetime(2024, 1, 15, 12, 16, 7, tzinfo=timezone.utc)
