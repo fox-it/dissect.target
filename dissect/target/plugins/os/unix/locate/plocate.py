73cabc96eddd70a32c4e71c01a4aac275748827b
Stefan de Reuver <9864602+Horofic@users.noreply.github.com>
2024-02-19 15:49:08 +0100

    Apply bug workaround in plocate plugin to all PyPy versions (#546)

--- dissect/target/plugins/os/unix/locate/plocate.py
+++ dissect/target/plugins/os/unix/locate/plocate.py
@@ -1,7 +1,6 @@
 from __future__ import annotations
 
 import platform
-import sys
 from io import BytesIO
 from typing import BinaryIO, Iterable
 
@@ -114,12 +113,12 @@ class PLocateFile:
         self.buf = RangeStream(self.fh, self.fh.tell(), self.compressed_length_bytes)
 
     def __iter__(self) -> Iterable[PLocateFile]:
-        # NOTE: This is a workaround for a PyPy 3.9 bug
+        # NOTE: This is a workaround for a PyPy bug
         # We don't know what breaks, but PyPy + zstandard = unhappy times
         # You just get random garbage data back instead of the decompressed data
         # This weird dance of using a decompressobj and unused data is the only way that seems to work
         # It's more expensive on memory, but at least it doesn't break
-        if platform.python_implementation() == "PyPy" and sys.version_info < (3, 10):
+        if platform.python_implementation() == "PyPy":
             obj = self.ctx.decompressobj()
             buf = self.buf.read()
 
