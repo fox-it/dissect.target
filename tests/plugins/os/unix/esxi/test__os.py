4464b451267202dd170f7d70b96626d37bcf85af
Miauwkeru <Miauwkeru@users.noreply.github.com>
2024-02-20 07:35:45 +0000

    Add return typehint to tests

--- tests/plugins/os/unix/esxi/test__os.py
+++ tests/plugins/os/unix/esxi/test__os.py
@@ -8,7 +8,7 @@ from dissect.target.plugins.os.unix.esxi._os import (
 )
 
 
-def test__create_tar_fs_no_envelope(target_linux: Target, fs_unix: VirtualFilesystem):
+def test__create_tar_fs_no_envelope(target_linux: Target, fs_unix: VirtualFilesystem) -> None:
     with (
         patch("dissect.target.plugins.os.unix.esxi._os.HAS_ENVELOPE", False),
         patch("dissect.target.plugins.os.unix.esxi._os.tar") as mocked_tar,
@@ -21,7 +21,7 @@ def test__create_tar_fs_no_envelope(target_linux: Target, fs_unix: VirtualFilesy
         mocked_tar.TarFilesystem.assert_called()
 
 
-def test__create_tar_fs_envelope(target_linux: Target, fs_unix: VirtualFilesystem):
+def test__create_tar_fs_envelope(target_linux: Target, fs_unix: VirtualFilesystem) -> None:
     with (
         patch("dissect.target.plugins.os.unix.esxi._os.HAS_ENVELOPE", True),
         patch("dissect.target.plugins.os.unix.esxi._os.tar") as mocked_tar,
@@ -33,7 +33,7 @@ def test__create_tar_fs_envelope(target_linux: Target, fs_unix: VirtualFilesyste
         mocked_tar.TarFilesystem.assert_called()
 
 
-def test__create_tar_fs_failed_envelope(target_linux: Target, fs_unix: VirtualFilesystem):
+def test__create_tar_fs_failed_envelope(target_linux: Target, fs_unix: VirtualFilesystem) -> None:
     with (
         patch("dissect.target.plugins.os.unix.esxi._os.HAS_ENVELOPE", True),
         patch("dissect.target.plugins.os.unix.esxi._os.tar") as mocked_tar,
@@ -47,13 +47,13 @@ def test__create_tar_fs_failed_envelope(target_linux: Target, fs_unix: VirtualFi
         mocked_tar.TarFilesystem.assert_called()
 
 
-def test__decrypt_crypto_not_local(target_linux: Target, fs_unix: VirtualFilesystem):
+def test__decrypt_crypto_not_local(target_linux: Target, fs_unix: VirtualFilesystem) -> None:
     target_linux._name = "not_local"
     with patch("dissect.target.plugins.os.unix.esxi._os.HAS_ENVELOPE", False):
         assert _create_local_fs(target_linux, fs_unix.path(""), fs_unix.path("")) is None
 
 
-def test__decrypt_crypto_local(fs_unix: VirtualFilesystem):
+def test__decrypt_crypto_local(fs_unix: VirtualFilesystem) -> None:
     with patch("dissect.target.plugins.os.unix.esxi._os.subprocess.run") as mocked_run:
         mocked_run.return_value.stdout = b"data"
 
