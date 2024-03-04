19c80aded030f2af4f70938644d2c6538b59face
JSCU-CNI <121175071+JSCU-CNI@users.noreply.github.com>
2024-03-04 12:30:27 +0100

    improve target registry tool

--- dissect/target/tools/reg.py
+++ dissect/target/tools/reg.py
@@ -6,7 +6,12 @@ import argparse
 import logging
 
 from dissect.target import Target
-from dissect.target.exceptions import RegistryError, TargetError
+from dissect.target.exceptions import (
+    RegistryError,
+    RegistryKeyNotFoundError,
+    TargetError,
+)
+from dissect.target.helpers.regutil import RegistryKey
 from dissect.target.tools.utils import (
     catch_sigpipe,
     configure_generic_arguments,
@@ -29,7 +34,8 @@ def main():
     parser.add_argument("targets", metavar="TARGETS", nargs="+", help="Targets to load")
     parser.add_argument("-k", "--key", required=True, help="key to query")
     parser.add_argument("-kv", "--value", help="value to query")
-    parser.add_argument("-d", "--depth", type=int, const=0, nargs="?", default=1)
+    parser.add_argument("-d", "--depth", type=int, const=0, nargs="?", default=1, help="Max depth of sub keys to print")
+    parser.add_argument("-l", "--length", type=int, default=100, help="Max length of key value to print")
 
     configure_generic_arguments(parser)
     args = parser.parse_args()
@@ -38,20 +44,31 @@ def main():
 
     try:
         for target in Target.open_all(args.targets):
+            if not target.has_function("registry"):
+                log.error("Target %s has no Windows Registry", target)
+                continue
+
             try:
-                if args.value:
-                    for key in target.registry.keys(args.key):
-                        try:
-                            print(key.value(args.value))
-                        except RegistryError:
-                            continue
-                else:
+                keys = list(target.registry.keys(args.key))
+
+                if not keys:
+                    log.error("Key %s does not exist on target %s", args.key, target)
+                    continue
+
+                print(target)
+
+                for key in keys:
                     try:
-                        print(target)
-                        for key in target.registry.keys(args.key):
-                            recursor(key, args.depth, 0)
+                        if args.value:
+                            print(key.value(args.value))
+                        else:
+                            recursor(key, args.depth, 0, args.length)
                     except RegistryError:
                         log.exception("Failed to find registry value")
+
+            except RegistryKeyNotFoundError:
+                log.error("Key %s does not exist on target %s", args.key, target)
+
             except Exception:
                 log.exception("Failed to iterate key")
     except TargetError as e:
@@ -60,12 +77,19 @@ def main():
         parser.exit(1)
 
 
-def recursor(key, depth, indent):
-    print(" " * indent + f"+ {key.name!r} ({key.ts})")
+def recursor(key: RegistryKey, depth: int, indent: int, max_length: int = 100) -> None:
+    class_name = ""
+    if hasattr(key, "class_name") and key.class_name:
+        class_name = f" ({key.class_name})"
+
+    print(" " * indent + f"+ {key.name!r} ({key.ts})" + class_name)
 
     for r in key.values():
         try:
-            print(" " * indent + f"  - {r.name!r} {repr(r.value)[:100]}")
+            value = repr(r.value)
+            if len(value) > max_length:
+                value = value[:max_length] + "..."
+            print(" " * indent + f"  - {r.name!r} {value}")
         except NotImplementedError:
             continue
 
@@ -73,7 +97,7 @@ def recursor(key, depth, indent):
         return
 
     for subkey in key.subkeys():
-        recursor(subkey, depth - 1, indent + 2)
+        recursor(subkey, depth - 1, indent + 2, max_length)
 
 
 if __name__ == "__main__":
