51f46b5039bdea2005260246d48a19decbf8d034
Schamper <1254028+Schamper@users.noreply.github.com>
2024-03-05 01:22:17 +0100

    Add all test files to git LFS

--- tests/_data/plugins/os/unix/services/initd.sh
+++ tests/_data/plugins/os/unix/services/initd.sh
@@ -1,16 +1,3 @@
-#! /bin/sh
-
-### BEGIN INIT INFO
-# Provides:             exampled
-# Required-Start:	    $foo $bar
-# Required-Stop:	    $foo $bar
-# Default-Start:	    2 3 4 5
-# Default-Stop:		    
-# Short-Description:	Example initd service
-### END INIT INFO
-
-set -e
-
-echo "Example initd service"
-
-exit 0
+version https://git-lfs.github.com/spec/v1
+oid sha256:b5ff82257079d24ced71bccbb8116913349799690f6b632fd4ee3797abd92386
+size 285
