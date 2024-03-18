eae7c4fa22d9397a3f4e7c0329ac51a70491ac91
Minoru Kobayashi <unknownbit@gmail.com>
2024-03-18 14:18:03 +0900

    Update shell.py

--- dissect/target/tools/shell.py
+++ dissect/target/tools/shell.py
@@ -512,9 +512,9 @@ class TargetCli(TargetCmd):
     @arg("-l", action="store_true")
     @arg("-a", "--all", action="store_true")  # ignored but included for proper argument parsing
     @arg("-h", "--human-readable", action="store_true")
-    @arg("-R", action="store_true", dest="recursive", help="Recursively list subdirectories encountered.")
-    @arg("-c", action="store_true", dest="use_ctime", help="Show time when file status was last changed.")
-    @arg("-u", action="store_true", dest="use_atime", help="Show time of last access.")
+    @arg("-R", "--recursive", action="store_true", help="recursively list subdirectories encountered")
+    @arg("-c", action="store_true", dest="use_ctime", help="show time when file status was last changed")
+    @arg("-u", action="store_true", dest="use_atime", help="show time of last access")
     def cmd_ls(self, args: argparse.Namespace, stdout: TextIO) -> Optional[bool]:
         """list directory contents"""
 
@@ -522,35 +522,16 @@ class TargetCli(TargetCmd):
         dir_list = []
 
         if args.use_ctime and args.use_atime:
-            print("Cannot specify -c and -u at the same time.")
+            print("can't specify -c and -u at the same time")
             return
 
         if not path or not path.exists():
             return
 
-        if path.is_dir():
-            contents = self.scandir(path, color=True)
-        elif path.is_file():
-            contents = [(path, path.name)]
+        self._print_ls(args, path, 0, stdout)
 
-        if not args.l:
-            for target_path, name in contents:
-                print(name, file=stdout)
-                if args.recursive and target_path.is_dir():
-                    dir_list.append(target_path)
-        else:
-            if len(contents) > 1:
-                print(f"total {len(contents)}", file=stdout)
-            for target_path, name in contents:
-                self.print_extensive_file_stat(args=args, stdout=stdout, target_path=target_path, name=name)
-                if args.recursive and target_path.is_dir():
-                    dir_list.append(target_path)
-
-        if args.recursive and len(dir_list):
-            for dir_name in dir_list:
-                self._recursive_ls(args, dir_name, stdout)
-
-    def _recursive_ls(self, args: argparse.Namespace, dir_name: fsutil.TargetPath, stdout: TextIO) -> Optional[bool]:
+    # def _print_ls(self, args: argparse.Namespace, dir_name: fsutil.TargetPath, stdout: TextIO) -> Optional[bool]:
+    def _print_ls(self, args: argparse.Namespace, dir_name: fsutil.TargetPath, dir_depth: int, stdout: TextIO) -> Optional[bool]:
         path = self.resolve_path(dir_name)
         dir_list = []
 
@@ -559,7 +540,9 @@ class TargetCli(TargetCmd):
         elif path.is_file():
             contents = [(path, path.name)]
 
-        print(f"\n{str(path)}:")
+        if dir_depth > 0:
+            print(f"\n{str(path)}:", file=stdout)
+
         if not args.l:
             for target_path, name in contents:
                 print(name, file=stdout)
@@ -575,7 +558,7 @@ class TargetCli(TargetCmd):
 
         if args.recursive and len(dir_list):
             for dir_name in dir_list:
-                self._recursive_ls(args, dir_name, stdout)
+                self._print_ls(args, dir_name, dir_depth+1, stdout)
 
     def print_extensive_file_stat(self, args: argparse.Namespace, stdout: TextIO, target_path: fsutil.TargetPath, name: str) -> None:
         """Print the file status."""
@@ -583,12 +566,12 @@ class TargetCli(TargetCmd):
             entry = target_path.get()
             stat = entry.lstat()
             symlink = f" -> {entry.readlink()}" if entry.is_symlink() else ""
+            show_time = stat.st_mtime
             if args.use_ctime:
-                utc_time = datetime.datetime.utcfromtimestamp(stat.st_ctime).isoformat()
+                show_time = stat.st_ctime
             elif args.use_atime:
-                utc_time = datetime.datetime.utcfromtimestamp(stat.st_atime).isoformat()
-            else:
-                utc_time = datetime.datetime.utcfromtimestamp(stat.st_mtime).isoformat()
+                show_time = stat.st_atime
+            utc_time = datetime.datetime.utcfromtimestamp(show_time).isoformat()
 
             print(
                 f"{stat_modestr(stat)} {stat.st_uid:4d} {stat.st_gid:4d} {stat.st_size:6d} {utc_time} {name}{symlink}",
