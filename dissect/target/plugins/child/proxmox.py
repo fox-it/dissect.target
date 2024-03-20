264e58857ccbebf0d1e0141e58940182498ea587
otnxSl <171164394+otnxSl@users.noreply.github.com>
2024-03-20 15:22:02 +0100

    Made child plugin for loading vm

---
+++ dissect/target/plugins/child/proxmox.py
@@ -0,0 +1,21 @@
+from dissect.target.exceptions import UnsupportedPluginError
+from dissect.target.helpers.record import ChildTargetRecord
+from dissect.target.plugin import ChildTargetPlugin
+
+
+class ProxmoxChildTargetPlugin(ChildTargetPlugin):
+    """Child target plugin that yields from the VM listing."""
+
+    __type__ = "proxmox"
+
+    def check_compatible(self) -> None:
+        if self.target.os != "proxmox":
+            raise UnsupportedPluginError("Not an promox operating system")
+
+    def list_children(self):
+        for vm in self.target.vm_list():
+            yield ChildTargetRecord(
+                type=self.__type__,
+                path=vm.path,
+                _target=self.target,
+            )
