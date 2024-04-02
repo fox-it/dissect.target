afc96549310819e51c5cff9aa9a926b7ca07b83f
Erik Schamper <1254028+Schamper@users.noreply.github.com>
2024-04-02 11:29:57 +0000

    Change

--- dissect/target/filesystem.py
+++ dissect/target/filesystem.py
@@ -1475,7 +1475,7 @@ class LayerFilesystem(Filesystem):
 class EntryList(list):
     """Wrapper list for filesystem entries.
 
-    Expose a getattr on a list of items. Useful to access internal objects from specific filesystem implementations.
+    Exposes a ``__getattr__`` on a list of items. Useful to access internal objects from specific filesystem implementations.
     For example, access the underlying NTFS object from a list of virtual and NTFS entries.
     """
 
