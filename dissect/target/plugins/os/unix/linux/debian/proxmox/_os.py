f4a676663ab8b5792d1713e5d8eb80235c2cd863
otnxSl <171164394+otnxSl@users.noreply.github.com>
2024-05-15 16:48:11 +0200

    Re-implemented Breadth-first search logic into pmxcfs creation

--- dissect/target/plugins/os/unix/linux/debian/proxmox/_os.py
+++ dissect/target/plugins/os/unix/linux/debian/proxmox/_os.py
@@ -22,7 +22,7 @@ log = logging.getLogger(__name__)
 PROXMOX_PACKAGE_NAME="proxmox-ve"
 FILETREE_TABLE_NAME="tree"
 PMXCFS_DATABASE_PATH="/var/lib/pve-cluster/config.db"
-# Change to /etc/pve/nodes/pve/qemu-server once pmxcfs func has been reworked to properly map fs
+# TODO: Change to /etc/pve/nodes/pve/qemu-server once pmxcfs func has been reworked to properly map fs
 VM_CONFIG_PATH="/etc/pve/qemu-server"
 
 
@@ -42,18 +42,15 @@ class ProxmoxPlugin(LinuxPlugin):
     @classmethod
     def detect(cls, target: Target) -> Optional[Filesystem]:
         for fs in target.filesystems:
-            # [REMINDER FOR REPORT] both filesystems are checked for redundancy (in case fs is damaged/missing stuff)
             if (fs.exists("/etc/pve") or fs.exists("/var/lib/pve")):
                 return fs
         return None
 
     @classmethod
     def create(cls, target: Target, sysvol: Filesystem) -> ProxmoxPlugin:
-        # [PERSONAL TO REMOVE] Modifies target / executescode before initializing the class
         obj = super().create(target, sysvol)
         pmxcfs = _create_pmxcfs(sysvol.path(PMXCFS_DATABASE_PATH).open("rb"))
         target.fs.mount("/etc/pve", pmxcfs)
-        # ipdb.set_trace()
 
         return obj
 
@@ -72,50 +69,56 @@ class ProxmoxPlugin(LinuxPlugin):
 
     @export(record=VirtualMachineRecord)
     def vm_list(self) -> Iterator[VirtualMachineRecord]:
-        import ipdb
         configs = self.target.fs.path(VM_CONFIG_PATH)
         for config in configs.iterdir():
             yield VirtualMachineRecord(
                 id=pathlib.Path(config).stem,
                 config_path=config,
             )
-            # ipdb.set_trace()
-            # parsed_config = _parse_vm_configuration(config)
-            # for option in parsed_config:
-            #     config_value = parsed_config[option].decode()
-            #     vm_disk = _get_vm_disk_name(config_value)
-            #     if _is_disk_device(option.decode()) and vm_disk is not None:
-            #         vm_id = pathlib.Path(config).stem
-            #         yield VirtualMachineRecord(
-            #             id=vm_id,
-            #             name=parsed_config[b'name'].decode(),
-            #             storage_id=_get_storage_ID(config_value),
-            #             disk=vm_disk, # TODO: Maybe remove
-            #             config_path=config,
-            #         )
 
 def _create_pmxcfs(fh) -> VirtualFilesystem:
     db = sqlite3.SQLite3(fh)
     filetree_table = db.table(FILETREE_TABLE_NAME)
-    # columns = filetree_table.columns  # For implementing fs with propper stat data later
     rows = filetree_table.rows()
-
     fs_entries = []
     for row in rows:
         fs_entries.append(row)
-    fs_entries.sort(key=lambda entry: (entry.parent, entry.inode), reverse=True)
+    fs_entries.sort(key=lambda entry: (entry.parent, entry.inode))
 
     vfs = VirtualFilesystem()
-    for entry in fs_entries: # might add dir mapping if deemed necessary 
-        if entry.type == 8: # Type 8 file | Type 4 dir
-            path = entry.name
-            parent = entry.parent
-            content = entry.data
-
-            for file in fs_entries:
-                if file.inode == parent and file.inode != 0:
-                    path = f"{file.name}/{path}"
-                else:
-                    vfs.map_file_fh(f"/{path}", BytesIO(content or b""))
+    for entry in fs_entries:
+        content = entry.data
+        if entry.parent == 0: # Root entries do not require parent check
+            vfs.map_file_fh(f"/{entry.name}",  BytesIO(content or b""))
+            continue
+        else:
+            entry_chain = _get_entry_parent_chain(fs_entries, entry)
+            path = _create_fs_path(entry_chain)
+            vfs.map_file_fh(f"/{path}", BytesIO(content or b""))
 
     return  vfs
+
+def _get_entry_parent_chain(fs_entries: list, entry: sqlite3[Row]) -> list[sqlite3[Row]]:
+    """Looks through the list of inodes (fs_entries) and retrieves parent inode of each node until root is found."""
+
+    inode_chain = [entry]
+    target = entry
+    for entry in fs_entries:
+        if target.inode != 0:
+            inode_chain.append(entry)
+            target = entry
+        else:
+            return inode_chain
+
+def _create_fs_path(entry_chain: list[sqlite3[Row]]) -> str:
+    """Creates a full path out of a sorted list of file entries"""
+    
+    entry_chain.sort(key=lambda entry: (entry.parent))
+    entry_names = []
+    for entry in entry_chain:
+        if entry.inode != 0:
+            entry_names.append(entry.name)
+
+    path = "/".join(entry_names)
+
+    return path
