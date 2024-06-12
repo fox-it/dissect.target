3c6b10e65e336f332fc0c37cf5a89c9d4f6557f3
otnxSl <171164394+otnxSl@users.noreply.github.com>
2024-06-12 13:08:29 +0200

    Implemented file & directory fs mounting with metadata

--- dissect/target/plugins/os/unix/linux/debian/proxmox/_os.py
+++ dissect/target/plugins/os/unix/linux/debian/proxmox/_os.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import os
 import re
+import stat
 import pathlib
 import logging
 from io import BytesIO
@@ -9,7 +10,8 @@ from typing import Optional
 
 from dissect.sql import sqlite3
 
-from dissect.target.filesystem import Filesystem, VirtualFilesystem
+from dissect.target.filesystem import Filesystem, VirtualFilesystem, VirtualFile, VirtualDirectory
+from dissect.target.helpers import fsutil
 from dissect.target.plugins.os.unix._os import OperatingSystem, export
 from dissect.target.plugins.os.unix.linux._os import LinuxPlugin
 from dissect.target.helpers.record import TargetRecordDescriptor
@@ -81,21 +83,32 @@ def _create_pmxcfs(fh) -> VirtualFilesystem:
     db = sqlite3.SQLite3(fh)
     filetree_table = db.table(FILETREE_TABLE_NAME)
     rows = filetree_table.rows()
-    fs_entries = []
+    fs_entries = {}
     for row in rows:
-        fs_entries.append(row)
-    fs_entries.sort(key=lambda entry: (entry.parent, entry.inode))
+        fs_entries[row.inode] = row
+    # fs_entries.sort(key=lambda entry: (entry.parent, entry.inode))
 
     vfs = VirtualFilesystem()
-    for entry in fs_entries:
-        content = entry.data
+    for entry in fs_entries.values():
         if entry.parent == 0: # Root entries do not require parent check
-            vfs.map_file_fh(f"/{entry.name}",  BytesIO(content or b""))
-            continue
+            path = entry.name
         else:
-            entry_chain = _get_entry_parent_chain(fs_entries, entry)
-            path = _create_fs_path(entry_chain)
-            vfs.map_file_fh(f"{path}", BytesIO(content or b"")) # TODO: revert root (/) on string once pmxcfs has been propperly implemented
+            # import ipdb; ipdb.set_trace()
+            parts = []
+            current = entry
+            while current.parent != 0:
+                parts.append(current.name)
+                current = fs_entries[current.parent]
+
+            path = "/".join(parts[::-1])
+        if entry.type == 4:
+            fsentry = ProxmoxConfigDirectoryEntry(vfs, path, entry)
+        elif entry.type == 8:
+            fsentry = ProxmoxConfigFileEntry(vfs, path, entry)
+        else:
+            raise ValueError(f"Unknown pmxcfs file type: {entry.type}")
+
+        vfs.map_file_entry(path, fsentry)
 
     return  vfs
 
@@ -123,3 +136,60 @@ def _create_fs_path(entry_chain: list[sqlite3[Row]]) -> str:
     path = "/".join(entry_names)
 
     return path
+
+
+class ProxmoxConfigFileEntry(VirtualFile):
+    def open(self) -> BinaryIO:
+        """Returns file handle (file-like object)."""
+        # if self.entry is not a directory, but a file
+        return BytesIO(self.entry.data or b"")
+
+    def stat(self, follow_symlinks: bool = True) -> fsutil.stat_result:
+        """Return the stat information of this entry."""
+        return self.lstat()
+
+    def lstat(self) -> fsutil.stat_result:
+        """Return the stat information of the given path, without resolving links."""
+        # ['mode', 'addr', 'dev', 'nlink', 'uid', 'gid', 'size', 'atime', 'mtime', 'ctime']
+        return fsutil.stat_result(
+            [
+                stat.S_IFREG | 0o777,
+                self.entry.inode,
+                id(self.fs),
+                1,
+                0,
+                0,
+                len(self.entry.data) if self.entry.data else 0,
+                0,
+                self.entry.mtime,
+                0,
+            ]
+        )
+
+
+class ProxmoxConfigDirectoryEntry(VirtualDirectory):
+    def __init__(self, fs: VirtualFilesystem, path: str, entry: sqlite3.Row):
+        super().__init__(fs, path)
+        self.entry = entry
+
+    def stat(self, follow_symlinks: bool = True) -> fsutil.stat_result:
+        """Return the stat information of this entry."""
+        return self.lstat()
+
+    def lstat(self) -> fsutil.stat_result:
+        """Return the stat information of the given path, without resolving links."""
+        # ['mode', 'addr', 'dev', 'nlink', 'uid', 'gid', 'size', 'atime', 'mtime', 'ctime']
+        return fsutil.stat_result(
+            [
+                stat.S_IFDIR | 0o777,
+                self.entry.inode,
+                id(self.fs),
+                1,
+                0,
+                0,
+                0,
+                0,
+                self.entry.mtime,
+                0,
+            ]
+        )
