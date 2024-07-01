2cc6e104d64a0576018451b45e7b4b0b74801edb
cecinestpasunepipe <110607403+cecinestpasunepipe@users.noreply.github.com>
2024-07-01 10:51:34 +0200

    Improvements

--- dissect/target/loaders/mqtt.py
+++ dissect/target/loaders/mqtt.py
@@ -445,6 +445,7 @@ class MQTTLoader(Loader):
         if cls.broker is None:
             if (uri := kwargs.get("parsed_path")) is None:
                 raise LoaderError("No URI connection details have been passed.")
+
             options = dict(urllib.parse.parse_qsl(uri.query, keep_blank_values=True))
             if options.get("password"):
                 options["password"] = getpass()
