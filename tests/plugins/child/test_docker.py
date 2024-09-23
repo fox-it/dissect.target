cce36c54795fe95cacc621bd03f0e30c612a8aed
JSCU-CNI <121175071+JSCU-CNI@users.noreply.github.com>
2024-09-23 16:06:08 +0200

    correctly fix tests

--- tests/plugins/child/test_docker.py
+++ tests/plugins/child/test_docker.py
@@ -10,7 +10,7 @@ def test_plugins_child_docker(target_linux_docker: Target) -> None:
     assert children[0].type == "docker"
 
     assert [c.path for c in children] == [
-        "/var/lib/docker/image/overlay2/layerdb/mounts/01b646bc043eb4ad72f3a64b4ffd9be2cbeb399e0a07497d749d724460ccad3a",
-        "/var/lib/docker/image/overlay2/layerdb/mounts/589135d12011921ac6ce69753569da5f206f4bc792a9133727ddae860997ee66",
-        "/var/lib/docker/image/overlay2/layerdb/mounts/f988f88e221d97930a665712cf16ab520f7e2c5af395660c145df93aebedf071",
-    ]  # noqa: E501
+        "/var/lib/docker/image/overlay2/layerdb/mounts/01b646bc043eb4ad72f3a64b4ffd9be2cbeb399e0a07497d749d724460ccad3a",  # noqa: E501
+        "/var/lib/docker/image/overlay2/layerdb/mounts/589135d12011921ac6ce69753569da5f206f4bc792a9133727ddae860997ee66",  # noqa: E501
+        "/var/lib/docker/image/overlay2/layerdb/mounts/f988f88e221d97930a665712cf16ab520f7e2c5af395660c145df93aebedf071",  # noqa: E501
+    ]
