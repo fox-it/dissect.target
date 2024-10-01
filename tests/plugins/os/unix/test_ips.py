31ff1b8f22d526b321770893f6519b49d06742c3
Miauwkeru <Miauwkeru@users.noreply.github.com>
2024-10-01 09:12:46 +0000

    Move network_manager.py to os/unix

--- tests/plugins/os/unix/test_ips.py
+++ tests/plugins/os/unix/test_ips.py
@@ -6,8 +6,8 @@ import pytest
 
 from dissect.target import Target
 from dissect.target.filesystem import VirtualFilesystem
-from dissect.target.helpers.network_managers import NetworkManager
 from dissect.target.plugins.os.unix.linux._os import LinuxPlugin
+from dissect.target.plugins.os.unix.network_managers import NetworkManager
 from dissect.target.tools.query import main as target_query
 from tests._utils import absolute_path
 
