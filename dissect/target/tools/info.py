de5ccb98c440b230027beae65b41713913e43178
JSCU-CNI <121175071+JSCU-CNI@users.noreply.github.com>
2024-10-01 14:43:39 +0200

    deduplicate ips output

--- dissect/target/tools/info.py
+++ dissect/target/tools/info.py
@@ -137,7 +137,7 @@ def print_target_info(target: Target) -> None:
             continue
 
         if isinstance(value, list):
-            value = ", ".join(value)
+            value = ", ".join([str(v) for v in value])
 
         if isinstance(value, datetime):
             value = value.isoformat(timespec="microseconds")
