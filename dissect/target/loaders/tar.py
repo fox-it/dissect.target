08b50274b90ec7d80415321b462dfab213320c65
Matthijs Vos <matthijs.vos@northwave.nl>
2024-11-18 16:31:15 +0100

    Move acquire logic into dedicated AcquireLoader

--- dissect/target/loaders/tar.py
+++ dissect/target/loaders/tar.py
@@ -15,7 +15,6 @@ from dissect.target.loader import Loader
 
 log = logging.getLogger(__name__)
 
-
 ANON_FS_RE = re.compile(r"^fs[0-9]+$")
 
 
@@ -38,7 +37,14 @@ class TarLoader(Loader):
 
     @staticmethod
     def detect(path: Path) -> bool:
-        return path.name.lower().endswith((".tar", ".tar.gz", ".tgz"))
+        if not path.name.lower().endswith((".tar", ".tar.gz", ".tgz")):
+            return False
+
+        # Check that this is not an acquire collect, that is handled by AcquireLoader
+        tar = tarfile.open(fileobj=path.open("rb"))
+        acquire_members = [m for m in tar.getmembers() if m.name.startswith(("/fs/", "fs/", "/sysvol/", "sysvol/"))]
+
+        return len(acquire_members) == 0
 
     def is_compressed(self, path: Path | str) -> bool:
         return str(path).lower().endswith((".tar.gz", ".tgz"))
@@ -50,52 +56,14 @@ class TarLoader(Loader):
             if member.name == ".":
                 continue
 
-            if not member.name.startswith(("/fs/", "fs/", "/sysvol/", "sysvol/")):
-                # Not an acquire tar
-                if "/" not in volumes:
-                    vol = filesystem.VirtualFilesystem(case_sensitive=True)
-                    vol.tar = self.tar
-                    volumes["/"] = vol
-                    target.filesystems.add(vol)
+            if "/" not in volumes:
+                vol = filesystem.VirtualFilesystem(case_sensitive=True)
+                vol.tar = self.tar
+                volumes["/"] = vol
+                target.filesystems.add(vol)
 
-                volume = volumes["/"]
-                mname = member.name
-            else:
-                if member.name.startswith(("/fs/", "fs/")):
-                    # Current acquire
-                    parts = member.name.replace("fs/", "").split("/")
-                    if parts[0] == "":
-                        parts.pop(0)
-                else:
-                    # Legacy acquire
-                    parts = member.name.lstrip("/").split("/")
-                volume_name = parts[0].lower()
-
-                # NOTE: older versions of acquire would write to "sysvol" instead of a driver letter
-                # Figuring out the sysvol from the drive letters is easier than the drive letter from "sysvol",
-                # so this was swapped in acquire 3.12. Now we map all volumes to a drive letter and let the
-                # Windows OS plugin figure out which is the sysvol
-                # For backwards compatibility we're forced to keep this check, and assume that "c:" is our sysvol
-                if volume_name == "sysvol":
-                    volume_name = "c:"
-
-                if volume_name == "$fs$":
-                    if len(parts) == 1:
-                        # The fs/$fs$ entry is ignored, only the directories below it are processed.
-                        continue
-                    fs_name = parts[1]
-                    if ANON_FS_RE.match(fs_name):
-                        parts.pop(0)
-                        volume_name = f"{volume_name}/{fs_name}"
-
-                if volume_name not in volumes:
-                    vol = filesystem.VirtualFilesystem(case_sensitive=False)
-                    vol.tar = self.tar
-                    volumes[volume_name] = vol
-                    target.filesystems.add(vol)
-
-                volume = volumes[volume_name]
-                mname = "/".join(parts[1:])
+            volume = volumes["/"]
+            mname = member.name
 
             entry_cls = TarFilesystemDirectoryEntry if member.isdir() else TarFilesystemEntry
             entry = entry_cls(volume, fsutil.normpath(mname), member)
@@ -107,7 +75,6 @@ class TarLoader(Loader):
                 vol,
                 usnjrnl_path=[
                     "$Extend/$Usnjrnl:$J",
-                    "$Extend/$Usnjrnl:J",  # Old versions of acquire used $Usnjrnl:J
                 ],
             )
 
