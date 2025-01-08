32f16e819735b781564b5d298ec7c0f0210feadf
qmadev <190383216+qmadev@users.noreply.github.com>
2025-01-08 21:53:42 +0100

    Processed suggested changes from fox-it

--- dissect/target/plugins/os/windows/regf/cam.py
+++ dissect/target/plugins/os/windows/regf/cam.py
@@ -1,6 +1,7 @@
 from typing import Iterator
 
 from dissect.util.ts import wintimestamp
+from flow.record.fieldtypes import windows_path
 
 from dissect.target.exceptions import UnsupportedPluginError
 from dissect.target.helpers.descriptor_extensions import (
@@ -8,21 +9,20 @@ from dissect.target.helpers.descriptor_extensions import (
     UserRecordDescriptorExtension,
 )
 from dissect.target.helpers.record import create_extended_descriptor
-from dissect.target.helpers.regutil import (
-    RegfKey,
-    RegistryKeyNotFoundError,
-    RegistryValueNotFoundError,
-)
+from dissect.target.helpers.regutil import RegistryKey, RegistryValueNotFoundError
 from dissect.target.plugin import Plugin, export
 from dissect.target.target import Target
 
 CamRecord = create_extended_descriptor([RegistryRecordDescriptorExtension, UserRecordDescriptorExtension])(
     "windows/registry/cam",
     [
+        ("datetime", "ts"),
         ("string", "device"),
         ("string", "app_name"),
+        ("path", "path"),
         ("datetime", "last_started"),
         ("datetime", "last_stopped"),
+        ("varint", "duration"),
     ],
 )
 
@@ -30,47 +30,34 @@ CamRecord = create_extended_descriptor([RegistryRecordDescriptorExtension, UserR
 class CamPlugin(Plugin):
     """Plugin that iterates various Capability Access Manager registry key locations."""
 
-    CONSENT_STORE = "{}\\Software\\Microsoft\\Windows\\CurrentVersion\\CapabilityAccessManager\\ConsentStore"
-    KEYS = []
+    CONSENT_STORES = [
+        "HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\CapabilityAccessManager\\ConsentStore",
+        "HKLM\\Software\\Microsoft\\Windows\\CurrentVersion\\CapabilityAccessManager\\ConsentStore",
+    ]
 
     def __init__(self, target: Target):
         super().__init__(target)
-        for key in target.registry.keys("HKU\\"):
-            if not key.subkeys():
-                continue
+        self.app_regf_keys = self._find_apps()
 
-            for subkey in key.subkeys():
-                hku_base = f"HKU\\{subkey.name}"
+    def _find_apps(self) -> list[RegistryKey]:
+        apps = []
+        for store in self.target.registry.keys(self.CONSENT_STORES):
+            for key in store.subkeys():
+                apps.append(key)
 
-                try:
-                    for k in target.registry.key(self.CONSENT_STORE.format(hku_base)).subkeys():
-                        full_key_path = f"{hku_base}\\{k.path}"
-                        self.KEYS.append(full_key_path)
-                except RegistryKeyNotFoundError:
-                    pass
-
-                try:
-                    for k in target.registry.key(self.CONSENT_STORE.format("HKLM")).subkeys():
-                        full_key_path = (
-                            f"HKLM\\Software\\{k.path}"  # For some reason "Software" disappears so added it here
-                        )
-                        self.KEYS.append(full_key_path)
-                except RegistryKeyNotFoundError:
-                    pass
+        return apps
 
     def check_compatible(self) -> None:
-        if not len(list(self.KEYS)):
+        if not self.app_regf_keys:
             raise UnsupportedPluginError("No Capability Access Manager keys found")
 
-    def yield_apps(self) -> Iterator[RegfKey]:
-        for base_key in self.KEYS:
-            for key in self.target.registry.keys(base_key):
-                if application_keys := key.subkeys():
-                    for app in application_keys:
-                        if "NonPackaged" in app.name:  # NonPackaged registry key has more apps, so yield those apps
-                            yield from app.subkeys()
-
-                        yield app
+    def yield_apps(self) -> Iterator[RegistryKey]:
+        for app in self.app_regf_keys:
+            for key in app.subkeys():
+                if key.name == "NonPackaged":  # NonPackaged registry key has more apps, so yield those apps
+                    yield from key.subkeys()
+                else:
+                    yield key
 
     @export(record=CamRecord)
     def cam(self) -> Iterator[CamRecord]:
@@ -84,41 +71,47 @@ class CamPlugin(Plugin):
             - https://docs.velociraptor.app/exchange/artifacts/pages/windows.registry.capabilityaccessmanager/
             - https://svch0st.medium.com/can-you-track-processes-accessing-the-camera-and-microphone-7e6885b37072
 
-        Yields Cam records with the following fields:
+        Yields ``CamRecord`` with the following fields:
 
         .. code-block:: text
 
             hostname (string): The target hostname.
             domain (string): The target domain.
+            ts (datetime): The modification timestamp of the registry key.
+            device (string): Name of the device privacy permission where asked for.
             app_name (string): The name of the application.
+            path (path): The possible path to the application.
             last_started (datetime): When the application last started using the device.
             last_stopped (datetime): When the application last stopped using the device.
+            duration (datetime): How long the application used the device (seconds).
         """
 
         for key in self.yield_apps():
             last_started = None
             last_stopped = None
-            app_name = key.name.rsplit("\\", 1)[0]
-            app_name = app_name.replace("#", "\\")
-            device = key.path.split("\\")[-2]
+            duration = None
 
             try:
                 last_started = wintimestamp(key.value("LastUsedTimeStart").value)
             except RegistryValueNotFoundError:
                 self.target.log.warning("No LastUsedTimeStart for application: %s", key.name)
-                pass
 
             try:
                 last_stopped = wintimestamp(key.value("LastUsedTimeStop").value)
             except RegistryValueNotFoundError:
                 self.target.log.warning("No LastUsedTimeStop for application: %s", key.name)
-                pass
+
+            if last_started and last_stopped:
+                duration = (last_stopped - last_started).seconds
 
             yield CamRecord(
-                device=device,
-                app_name=app_name,
+                ts=key.ts,
+                device=key.path.split("\\")[-2],
+                app_name=key.name,
+                path=windows_path(key.name.replace("#", "\\")) if "#" in key.name else None,
                 last_started=last_started,
                 last_stopped=last_stopped,
+                duration=duration,
                 _target=self.target,
                 _key=key,
                 _user=self.target.registry.get_user(key),
