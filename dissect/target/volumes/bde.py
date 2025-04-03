c0cc32be9741920a3c749bc297524cfeeb022462
JSCU-CNI <121175071+JSCU-CNI@users.noreply.github.com>
2025-04-03 13:41:45 +0200

    Add `unlock_with_raw_key` to BitlockerVolumeSystem

--- dissect/target/volumes/bde.py
+++ dissect/target/volumes/bde.py
@@ -86,6 +86,13 @@ class BitlockerVolumeSystem(EncryptedVolumeSystem):
                 if not is_wildcard:
                     log.exception("Failed to unlock BDE volume with BEK file %s", bek_file)
 
+    def unlock_with_raw_key(self, raw_key: bytes, is_wildcard: bool = False) -> None:
+        try:
+            self.bde.unlock_with_raw_key(raw_key)
+        except ValueError as e:
+            if not is_wildcard:
+                log.exception("Failed to unlock BDE volume with raw FVEK key (%r): %s", raw_key, e)
+
     def unlock_volume(self) -> AlignedStream:
         if self.bde.has_clear_key():
             self.bde.unlock_with_clear_key()
@@ -101,6 +108,8 @@ class BitlockerVolumeSystem(EncryptedVolumeSystem):
                 elif key.key_type == KeyType.FILE:
                     bek_file = pathlib.Path(key.value)
                     self.unlock_with_bek_file(bek_file, key.is_wildcard)
+                elif key.key_type == KeyType.RAW:
+                    self.unlock_with_raw_key(key.value, key.is_wildcard)
 
                 if self.bde.unlocked:
                     log.info("Volume %s with identifiers %s unlocked with %s", self.fh, identifiers, key)
