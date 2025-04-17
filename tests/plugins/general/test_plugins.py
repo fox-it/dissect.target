a69a32fadbb95d072d7259ec50d2f0732cac8ed3
Erik Schamper <1254028+Schamper@users.noreply.github.com>
2025-04-17 11:52:39 +0200

    Fix multiple namespace plugins in same file (#1102)

--- tests/plugins/general/test_plugins.py
+++ tests/plugins/general/test_plugins.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 from unittest.mock import MagicMock, patch
 
 from dissect.target import plugin
+from dissect.target.plugins.general.loaders import LoaderListPlugin
 from dissect.target.plugins.general.plugins import (
     PluginListPlugin,
     _categorize_functions,
@@ -11,12 +12,12 @@ from dissect.target.plugins.general.plugins import (
 
 
 def test_plugin_description() -> None:
-    description = list(_generate_plugin_tree_overview(PluginListPlugin, False))
+    description = list(_generate_plugin_tree_overview({PluginListPlugin}, False))
     assert description == ["plugins - Print all available plugins. (output: no output)"]
 
 
 def test_plugin_description_compacting() -> None:
-    module = {"hello": {"world": PluginListPlugin}}
+    module = {"hello": {"world": {PluginListPlugin}}}
 
     description = list(_generate_plugin_tree_overview(module, False))
     assert description == [
@@ -27,7 +28,7 @@ def test_plugin_description_compacting() -> None:
 
 
 def test_plugin_description_in_dict_multiple() -> None:
-    module = {"hello": {"world": {"data": PluginListPlugin, "data2": PluginListPlugin}}}
+    module = {"hello": {"world": {"data": {PluginListPlugin}, "data2": {PluginListPlugin}}}}
 
     description = list(_generate_plugin_tree_overview(module, False))
     assert description == [
@@ -37,6 +38,17 @@ def test_plugin_description_in_dict_multiple() -> None:
         "      plugins - Print all available plugins. (output: no output)",
         "    data2:",
         "      plugins - Print all available plugins. (output: no output)",
+    ]
+
+
+def test_plugin_description_multiple_in_path() -> None:
+    module = {"hello": {"world": {PluginListPlugin, LoaderListPlugin}}}
+    description = list(_generate_plugin_tree_overview(module, False))
+    assert description == [
+        "hello:",
+        "  world:",
+        "    loaders - List the available loaders. (output: no output)",
+        "    plugins - Print all available plugins. (output: no output)",
     ]
 
 
@@ -58,4 +70,34 @@ def test_categorize_plugins(mocked_plugins: MagicMock, mocked_load: MagicMock) -
             qualname="DataClass",
         ),
     ]
-    assert _categorize_functions() == {"something": mocked_load.return_value}
+    assert _categorize_functions() == {"something": {mocked_load.return_value}}
+
+    mocked_plugins.return_value = [
+        plugin.FunctionDescriptor(
+            name="data",
+            namespace=None,
+            path="something.data",
+            exported=True,
+            internal=False,
+            findable=True,
+            alias=False,
+            output=None,
+            method_name="data",
+            module="other.root.something.data",
+            qualname="DataClass1",
+        ),
+        plugin.FunctionDescriptor(
+            name="data",
+            namespace=None,
+            path="something.data",
+            exported=True,
+            internal=False,
+            findable=True,
+            alias=False,
+            output=None,
+            method_name="data",
+            module="other.root.something.data",
+            qualname="DataClass2",
+        ),
+    ]
+    assert _categorize_functions() == {"something": {mocked_load.return_value}}
