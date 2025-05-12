f3227722c4274ae7ef822778124f9bcbfcd13fd5
JSCU-CNI <121175071+JSCU-CNI@users.noreply.github.com>
2025-05-12 17:25:21 +0200

    improve _filter_compatible for OSPlugin

--- tests/tools/test_query.py
+++ tests/tools/test_query.py
@@ -405,7 +405,7 @@ def test_list_json_target_filter(capsys: pytest.CaptureFixture, monkeypatch: pyt
             }
         ],
         "alias": False,
-        "path": "os.unix._os.users",
+        "path": "os.unix.linux._os.users",
     }
 
 
