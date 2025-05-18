1f212f1f04e8b727dcec011fe1842b36580dd9c8
Matthijs Vos <matthijs.vos@northwave.nl>
2025-05-18 17:40:17 +0200

    Rework loading acquire collects via subloaders

--- dissect/target/loaders/acquire.py
+++ dissect/target/loaders/acquire.py
@@ -1,13 +1,14 @@
 from __future__ import annotations
 
 import logging
-import zipfile
+import tarfile as tf
+import zipfile as zf
 from pathlib import Path
 
 from dissect.target.filesystems.tar import TarFilesystem
-from dissect.target.filesystems.zip import ZipFilesystem
-from dissect.target.loader import Loader
 from dissect.target.loaders.dir import find_and_map_dirs
+from dissect.target.loaders.tar import TarSubLoader
+from dissect.target.loaders.zip import ZipSubLoader
 from dissect.target.target import Target
 
 log = logging.getLogger(__name__)
@@ -16,37 +17,37 @@ FILESYSTEMS_ROOT = "fs"
 FILESYSTEMS_LEGACY_ROOT = "sysvol"
 
 
-def _get_root(path: Path) -> Path | None:
-    if path.is_file():
-        fh = path.open("rb")
-        if TarFilesystem._detect(fh):
-            return TarFilesystem(fh).path()
-
-        if ZipFilesystem._detect(fh):
-            return zipfile.Path(path.open("rb"))
-
-    return None
-
-
-class AcquireLoader(Loader):
-    def __init__(self, path: Path, **kwargs):
-        super().__init__(path)
-
-        self.root = _get_root(path)
+class AcquireTarSubLoader(TarSubLoader):
+    """Tar implementation for parsing Acquire collects"""
 
     @staticmethod
-    def detect(path: Path) -> bool:
-        root = _get_root(path)
-
-        if not root:
-            return False
-
-        return root.joinpath(FILESYSTEMS_ROOT).exists() or root.joinpath(FILESYSTEMS_LEGACY_ROOT).exists()
+    def detect(tarfile: tf.TarFile) -> bool:
+        for member in tarfile.getmembers():
+            if not member.name.startswith((f"/{FILESYSTEMS_ROOT}/", f"{FILESYSTEMS_ROOT}/",
+                                           f"/{FILESYSTEMS_LEGACY_ROOT}/", f"{FILESYSTEMS_LEGACY_ROOT}/")):
+                return True
+        return False
 
     def map(self, target: Target) -> None:
-        # Handle both root dir 'fs' and 'sysvol' (legacy)
-        fs_root = self.root
-        if fs_root.joinpath(FILESYSTEMS_ROOT).exists():
-            fs_root = fs_root.joinpath(FILESYSTEMS_ROOT)
+        tar_fs = TarFilesystem(tarfile=self.tar).path()
+        map_acquire(target, tar_fs)
 
-        find_and_map_dirs(target, fs_root)
+
+class AcquireZipSubLoader(ZipSubLoader):
+    """Zip implementation for parsing Acquire collects"""
+
+    @staticmethod
+    def detect(zipfile: zf.Path) -> bool:
+        return zipfile.joinpath(FILESYSTEMS_ROOT).exists() or zipfile.joinpath(FILESYSTEMS_LEGACY_ROOT).exists()
+
+    def map(self, target: Target) -> None:
+        map_acquire(target, self.zip)
+
+
+def map_acquire(target: Target, path: Path):
+    """
+    Map Acquire filestructure
+    """
+    if path.joinpath(FILESYSTEMS_ROOT).exists():
+        path = path.joinpath(FILESYSTEMS_ROOT)
+    find_and_map_dirs(target, path)
