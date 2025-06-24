9e613a64b5f3b1e0daa66ab53c779bdc701fb511
Lennart Haagsma <6630974+lhaagsma@users.noreply.github.com>
2025-06-24 17:05:41 +0200

    Test child.name for parallels

--- tests/plugins/child/test_parallels.py
+++ tests/plugins/child/test_parallels.py
@@ -13,21 +13,13 @@ if TYPE_CHECKING:
 def test_parallels_child_detection(target_macos_users: Target, fs_macos: VirtualFilesystem) -> None:
     """Test if we correctly find Parallels child VMs on MacOS targets."""
 
+    config = absolute_path("_data/plugins/child/parallels/config.pvs")
+    fs_macos.map_file("Users/dissect/Parallels/Windows 11.pvm/config.pvs", config)
+    fs_macos.map_file("Users/dissect/Documents/Parallels/Windows 10.pvm/config.pvs", config)
+    fs_macos.map_file("Users/Shared/Parallels/Windows 7.pvm/config.pvs", config)
     fs_macos.map_file(
-        "Users/dissect/Parallels/Windows 11.pvm/config.pvs",
-        absolute_path("_data/plugins/child/parallels/config.pvs"),
-    )
-    fs_macos.map_file(
-        "Users/dissect/Documents/Parallels/Windows 10.pvm/config.pvs",
-        absolute_path("_data/plugins/child/parallels/config.pvs"),
-    )
-    fs_macos.map_file(
-        "Users/dissect/Library/Group Containers/svn.com.parallels.desktop.appstore/Shared/Parallels/My VM.pvm/config.pvs",
-        absolute_path("_data/plugins/child/parallels/config.pvs"),
-    )
-    fs_macos.map_file(
-        "Users/Shared/Parallels/Windows 7.pvm/config.pvs",
-        absolute_path("_data/plugins/child/parallels/config.pvs"),
+        "Users/dissect/Library/Group Containers/svn.com.parallels.desktop.appstore/Shared/Parallels/My VM.pvm/config.pvs",  # noqa: E501
+        config,
     )
 
     target_macos_users.add_plugin(ParallelsChildTargetPlugin)
