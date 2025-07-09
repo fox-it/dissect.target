507046a67aca9850fa7197e3406e708fbdc54cc7
Matthijs Vos <matthijs.vos@northwave.nl>
2025-07-09 11:53:52 +0200

    Fix linting

--- tests/tools/test_fs.py
+++ tests/tools/test_fs.py
@@ -10,7 +10,6 @@ from dissect.target.filesystem import VirtualFile, VirtualFilesystem
 from dissect.target.tools.fs import _extract_path, cp
 from dissect.target.tools.fs import main as target_fs
 
-
 if TYPE_CHECKING:
     from pathlib import Path
 
@@ -28,11 +27,10 @@ def vfs(files: list[str]) -> VirtualFilesystem:
 
 @pytest.mark.parametrize(
     ("path", "expected_files"),
-    [("root/.bash_history", 1), ("this/path/doesntexist", 0), ("**/*_history", 4)
-     ],
+    [("root/.bash_history", 1), ("this/path/doesntexist", 0), ("**/*_history", 4)],
 )
 def test_target_fs(
-        capsys: pytest.CaptureFixture, monkeypatch: pytest.MonkeyPatch, path: str, expected_files: int
+    capsys: pytest.CaptureFixture, monkeypatch: pytest.MonkeyPatch, path: str, expected_files: int
 ) -> None:
     with monkeypatch.context() as m:
         m.setattr("sys.argv", ["target-fs", "tests/_data/tools/info/image.tar", "cp", path])
@@ -41,10 +39,11 @@ def test_target_fs(
         stdout, stderr = capsys.readouterr()
 
         if expected_files > 0:
-            lines = [l for l in stdout.split("\n") if l != ""]
+            lines = [line for line in stdout.split("\n") if line != ""]
             assert len(lines) == expected_files
         else:
             assert stdout == "[!] Path doesn't exist\n"
+
 
 @pytest.mark.parametrize("files", [["file"]])
 def test_extract_file(vfs: VirtualFilesystem, tmp_path: Path) -> None:
