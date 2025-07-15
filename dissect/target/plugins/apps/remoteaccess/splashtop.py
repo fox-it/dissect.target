359c2970434e7de1ac5f8a0ee59a8a5b29afa437
user <Bob Karreman>
2025-07-15 22:30:38 +0200

    Small improvements and review adjustments

--- dissect/target/plugins/apps/remoteaccess/splashtop.py
+++ dissect/target/plugins/apps/remoteaccess/splashtop.py
@@ -40,17 +40,20 @@ class SplashtopPlugin(RemoteAccessPlugin):
         "remoteaccess/splashtop/filetransfer", GENERIC_FILE_TRANSFER_RECORD_FIELDS
     )
 
-    LOG_PATHS = ("sysvol/Program Files (x86)/Splashtop/Splashtop Remote/Server/log/SPLog.txt",)
+    LOG_PATHS = (
+        "sysvol/Program Files (x86)/Splashtop/Splashtop Remote/Server/log/SPLog.txt",  # General agent log including connections and filetransfers
+        # "sysvol/ProgramData/Splashtop/Temp/log/FTCLog.txt", # File transfer log which is included in the SPLog
+    )
 
     def __init__(self, target: Target):
         super().__init__(target)
 
-        self.log_files: set[tuple[TargetPath, UserDetails | None]] = set()
+        self.log_files: set[TargetPath] = set()
 
         for log_path in self.LOG_PATHS:
             log_file = self.target.fs.path(log_path)
             if log_file:
-                self.log_files.add((log_file, None))
+                self.log_files.add(log_file)
 
     def check_compatible(self) -> None:
         if not self.log_files:
@@ -68,7 +71,7 @@ class SplashtopPlugin(RemoteAccessPlugin):
         """
         target_tz = self.target.datetime.tzinfo
 
-        for log_file, user in self.log_files:
+        for log_file in self.log_files:
             for ts, line in year_rollover_helper(log_file, RE_TS, "%b%d %H:%M:%S.%f", target_tz):
                 if line := line.strip():
                     try:
@@ -83,10 +86,9 @@ class SplashtopPlugin(RemoteAccessPlugin):
                             message=message,
                             source=log_file,
                             _target=self.target,
-                            _user=user,
                         )
                     except ValueError as e:
-                        self.target.log.warning("Could not parse log line in file %s: '%s'", log_file, line)
+                        self.target.log.warning("Could not parse log line in file %s: '%r'", log_file, line)
                         self.target.log.debug("", exc_info=e)
 
     @export(record=RemoteAccessFileTransferRecord)
@@ -96,18 +98,24 @@ class SplashtopPlugin(RemoteAccessPlugin):
         Splashtop is a remote desktop application and can be used by adversaries to get (persistent) access to a machine.
         File transfers as logged in the generic logfile (``SPLog.txt``) show what files are downloaded to a system.
         """
+        methods = ("OnUploadRequest", "OnUploadFileCPRequest", "OnDownloadRequest")
         for log_record in self.logs():
             try:
                 # Example log entry:
                 # SM_03280[FTCnnel] OnUploadFileCPRequest 1, 1 =>{"fileID":"353841253","fileName":"NOTE.txt","fileSize":"34","remotesessionFTC":1,"request":"uploadFile"}
-                if "OnUploadFileCPRequest" in log_record.message:
-                    upload_json = json.loads(log_record.message.split("=>")[1])
 
-                    kwargs = log_record._asdict()
-                    kwargs["filename"] = upload_json["fileName"]
-                    yield self.RemoteAccessFileTransferRecord(**kwargs)
+                if any(method in log_record.message for method in methods):
+                    json_data = json.loads(log_record.message.split("=>")[1])
+
+                    yield self.RemoteAccessFileTransferRecord(
+                        ts=log_record.ts,
+                        message=log_record.message,
+                        source=log_record.source,
+                        filename=json_data["fileName"],
+                        _target=self.target,
+                    )
             except ValueError as e:
                 self.target.log.warning(
-                    "Could not parse file transfer from message in file %s: '%s'", log_record.source, log_record.message
+                    "Could not parse file transfer from message in file %s: '%r'", log_record.source, log_record.message
                 )
                 self.target.log.debug("", exc_info=e)
