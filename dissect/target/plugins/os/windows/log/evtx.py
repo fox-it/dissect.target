38916e532029ebe013d485c781c921178b9cbdf5
Gyatso <218940752+respondersGY@users.noreply.github.com>
2025-08-15 15:59:15 +0200

    Adjust log messages

--- dissect/target/plugins/os/windows/log/evtx.py
+++ dissect/target/plugins/os/windows/log/evtx.py
@@ -87,11 +87,11 @@ class EvtxPlugin(WindowsEventlogsMixin, Plugin):
                     try:
                         yield self._build_record(event, entry)
                     except Exception as e:  # noqa: PERF203
-                        self.target.log.warning("Unable to parse event log file %s: %s", entry, e)
+                        self.target.log.warning("Unable to parse event log event %s: %s", event, e)
                         self.target.log.debug("", exc_info=e)
-            except EOFError:
-                # Skip empty or corrupt EVTX files
-                continue
+            except EOFError as e:
+                self.target.log.warning("Unable to parse event log file %s: %s", entry, e)
+                self.target.log.debug("", exc_info=e)
 
     @export(record=DynamicDescriptor(["datetime"]))
     def scraped_evtx(self) -> Iterator[DynamicDescriptor]:
