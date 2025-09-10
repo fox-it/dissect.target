24940dbce0b4ebb85e32a49f7c894369c79e7294
loaflover <34983803+loaflover@users.noreply.github.com>
2025-09-11 00:01:25 +0300

    fixed result format as requested by @Schamper

--- dissect/target/plugins/filesystem/ntfs/utils.py
+++ dissect/target/plugins/filesystem/ntfs/utils.py
@@ -60,9 +60,13 @@ def get_drive_letter(target: Target, filesystem: NtfsFilesystem) -> str:
 
 
 def get_volume_identifier(fs: NtfsFilesystem) -> str | None:
-    """Return the filesystem GUID if available, otherwise the filesystem serial as a UUID."""
+    """Return the filesystem GUID if available, otherwise the filesystem serial"""
     try:
-        return f"{UUID(bytes_le=fs.volume.guid) if fs.volume.guid else UUID(int=fs.ntfs.serial)}"
+        if fs.volume.guid:
+            return str(UUID(bytes_le=fs.volume.guid))
+        elif fs.ntfs.serial:
+            return str(fs.ntfs.serial)
+        return None
     except (AttributeError, TypeError, ValueError):
         # AttributeError is raised when volume is None
         # TypeError is raised when guid is None
