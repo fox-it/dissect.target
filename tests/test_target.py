0fbd1bf6c14c25bf53564ee5a14202e9c3be8446
JSCU-CNI <121175071+JSCU-CNI@users.noreply.github.com>
2025-09-10 13:47:28 +0200

    fix tests on windows

--- tests/test_target.py
+++ tests/test_target.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 import io
 import logging
 import platform
+import sys
 import urllib.parse
 from collections import defaultdict
 from pathlib import Path
@@ -753,17 +754,19 @@ def test_expected_path(path: str | Path, expected: Path) -> None:
 def test_exception_invalid_path() -> None:
     """Test if we throw small and neat error messages and not long stack traces when giving invalid path(s)."""
 
-    with pytest.raises(
-        TargetError,
-        match="Failed to initiate RawLoader for target /path/to/invalid.img: Provided target path does not exist",
-    ):
-        Target.open("/path/to/invalid.img")
+    path = "\\path\\to\\invalid.img" if sys.platform == "windows" else "/path/to/invalid.img"
 
     with pytest.raises(
         TargetError,
-        match="Failed to find loader for /path/to/invalid.img: path does not exist",
+        match=f"Failed to initiate RawLoader for target {path}: Provided target path does not exist",
     ):
-        next(Target.open_all("/path/to/invalid.img"))
+        Target.open(path)
+
+    with pytest.raises(
+        TargetError,
+        match=f"Failed to find loader for {path}: path does not exist",
+    ):
+        next(Target.open_all(path))
 
     with pytest.raises(
         TargetError,
