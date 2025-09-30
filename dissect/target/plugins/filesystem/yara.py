3376c662d0848bcbc52d64cddad709788a5d1bf9
Matthijs Vos <matthijs.vos@northwave.nl>
2025-09-30 11:54:56 +0200

    Add timestamp to yara plugin

--- dissect/target/plugins/filesystem/yara.py
+++ dissect/target/plugins/filesystem/yara.py
@@ -29,6 +29,7 @@ log = logging.getLogger(__name__)
 YaraMatchRecord = TargetRecordDescriptor(
     "filesystem/yara/match",
     [
+        ("datetime", "ts"),
         ("path", "path"),
         ("string", "rule"),
         ("string[]", "matches"),
@@ -99,6 +100,7 @@ class YaraPlugin(Plugin):
                             string_matches.extend(f"{string}={instance}" for instance in string.instances)
 
                         yield YaraMatchRecord(
+                            ts=file.stat().st_mtime,
                             path=self.target.fs.path(file.path),
                             rule=match.rule,
                             matches=string_matches,
