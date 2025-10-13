d459743ba0088b1e1b66e01a4930aea03af637f1
JSCU-CNI <121175071+JSCU-CNI@users.noreply.github.com>
2025-10-13 13:11:53 +0200

    replace parse_vm_inventory with parse_inventory_file in child plugin

--- dissect/target/plugins/child/vmware_workstation.py
+++ dissect/target/plugins/child/vmware_workstation.py
@@ -5,33 +5,12 @@ from typing import TYPE_CHECKING
 from dissect.target.exceptions import UnsupportedPluginError
 from dissect.target.helpers.record import ChildTargetRecord
 from dissect.target.plugin import ChildTargetPlugin
-from dissect.target.plugins.apps.virtualization.vmware_workstation import find_vm_inventory
+from dissect.target.plugins.apps.virtualization.vmware_workstation import find_vm_inventory, parse_inventory_file
 
 if TYPE_CHECKING:
     from collections.abc import Iterator
-    from pathlib import Path
 
     from dissect.target.target import Target
-
-
-def parse_vm_inventory(path: Path) -> dict[str, dict[str, str]]:
-    config = {}
-
-    with path.open("rt") as fh:
-        for line in fh:
-            if not (line := line.strip()) or line.startswith("."):
-                continue
-
-            full_key, value = line.split("=", 1)
-            vm, key = full_key.strip().split(".", 1)
-
-            # Only process vmlist entries, not index entries
-            if "vmlist" not in vm:
-                continue
-
-            config.setdefault(vm, {})[key] = value.strip().strip('"')
-
-    return config
 
 
 class VmwareWorkstationChildTargetPlugin(ChildTargetPlugin):
@@ -49,7 +28,7 @@ class VmwareWorkstationChildTargetPlugin(ChildTargetPlugin):
 
     def list_children(self) -> Iterator[ChildTargetRecord]:
         for inv in self.inventories:
-            inventory = parse_vm_inventory(inv)
+            inventory = parse_inventory_file(inv)
 
             for config in inventory.values():
                 yield ChildTargetRecord(
