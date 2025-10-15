74db8faf5ff0a6c85183f12f7b61c5e404756ab5
JSCU-CNI <121175071+JSCU-CNI@users.noreply.github.com>
2025-10-15 16:31:22 +0200

    better exception handling for openssh key parsing

--- dissect/target/plugins/apps/ssh/openssh.py
+++ dissect/target/plugins/apps/ssh/openssh.py
@@ -22,6 +22,7 @@ if TYPE_CHECKING:
     from pathlib import Path
 
     from dissect.target.helpers.fsutil import TargetPath
+    from dissect.target.plugins.general.users import UserDetails
     from dissect.target.target import Target
 
 
@@ -55,16 +56,16 @@ class OpenSSHPlugin(SSHPlugin):
         if not ssh_user_dirs and not self.sshd_directory.exists():
             raise UnsupportedPluginError("No OpenSSH directories found")
 
-    def ssh_directory_globs(self, glob_user: str, glob_sshd: str) -> list[tuple[str, TargetPath]]:
+    def ssh_directory_globs(self, glob_user: str, glob_sshd: str) -> Iterator[tuple[UserDetails | None, TargetPath]]:
         for user_details in self.target.user_details.all_with_home():
-            yield from product([user_details.user], user_details.home_path.glob(f".ssh/{glob_user}"))
+            yield from product([user_details], user_details.home_path.glob(f".ssh/{glob_user}"))
 
         yield from product([None], self.sshd_directory.glob(glob_sshd))
 
     @export(record=AuthorizedKeysRecord)
     def authorized_keys(self) -> Iterator[AuthorizedKeysRecord]:
         """Yields the content of the authorized_keys files on a target for each user."""
-        for user, authorized_keys_file in self.ssh_directory_globs("authorized_keys*", "administrator_authorized_keys"):
+        for user_details, authorized_keys_file in self.ssh_directory_globs("authorized_keys*", "administrator_authorized_keys"):
             for line in authorized_keys_file.open("rt"):
                 line = line.strip()
                 if not line or line.startswith("#"):
@@ -82,14 +83,14 @@ class OpenSSHPlugin(SSHPlugin):
                     options=options,
                     path=authorized_keys_file,
                     _target=self.target,
-                    _user=user,
+                    _user=user_details.user if user_details else None,
                 )
 
     @export(record=KnownHostRecord)
     def known_hosts(self) -> Iterator[KnownHostRecord]:
         """Yields the content of the known_hosts files on a target for each user."""
 
-        for user, known_hosts_file in self.ssh_directory_globs("known_hosts*", "ssh_known_hosts"):
+        for user_details, known_hosts_file in self.ssh_directory_globs("known_hosts*", "ssh_known_hosts"):
             for line in known_hosts_file.open("rt"):
                 line = line.strip()
                 if not line or line.startswith("#"):
@@ -110,14 +111,14 @@ class OpenSSHPlugin(SSHPlugin):
                         marker=marker,
                         path=known_hosts_file,
                         _target=self.target,
-                        _user=user,
+                        _user=user_details.user if user_details else None,
                     )
 
     @export(record=PrivateKeyRecord)
     def private_keys(self) -> Iterator[PrivateKeyRecord]:
         """Yields OpenSSH private keys on a target for each user."""
 
-        for user, file_path in self.ssh_directory_globs("*", "*"):
+        for user_details, file_path in self.ssh_directory_globs("*", "*"):
             if not file_path.is_file():
                 continue
 
@@ -128,7 +129,7 @@ class OpenSSHPlugin(SSHPlugin):
             try:
                 private_key = SSHPrivateKey(buffer)
             except ValueError as e:
-                self.target.log.warning("Failed to parse SSH private key: %s", file_path)
+                self.target.log.warning("Failed to parse SSH private key %s: %s", file_path, e)
                 self.target.log.debug("", exc_info=e)
                 continue
 
@@ -141,19 +142,24 @@ class OpenSSHPlugin(SSHPlugin):
                 encrypted=private_key.is_encrypted,
                 path=file_path,
                 _target=self.target,
-                _user=user,
+                _user=user_details.user if user_details else None,
             )
 
     @export(record=PublicKeyRecord)
     def public_keys(self) -> Iterator[PublicKeyRecord]:
         """Yields all OpenSSH public keys from all user home directories and the OpenSSH daemon directory."""
 
-        for user, file_path in self.ssh_directory_globs("*.pub", "*.pub"):
+        for user_details, file_path in self.ssh_directory_globs("*.pub", "*.pub"):
             if not file_path.is_file():
                 continue
 
-            key_type, public_key, comment = parse_ssh_public_key_file(file_path)
-            fingerprints = calculate_fingerprints(base64.b64decode(public_key))
+            try:
+                key_type, public_key, comment = parse_ssh_public_key_file(file_path)
+                fingerprints = calculate_fingerprints(base64.b64decode(public_key))
+            except Exception as e:
+                self.target.log.warning("Failed to parse SSH public key %s: %s", file_path, e)
+                self.target.log.debug("", exc_info=e)
+                continue
 
             yield PublicKeyRecord(
                 mtime_ts=file_path.stat().st_mtime,
@@ -163,16 +169,16 @@ class OpenSSHPlugin(SSHPlugin):
                 fingerprint=fingerprints,
                 path=file_path,
                 _target=self.target,
-                _user=user,
+                _user=user_details.user if user_details else None,
             )
 
 
-def parse_ssh_public_key_file(path: Path) -> tuple[str, str, str]:
+def parse_ssh_public_key_file(path: Path) -> tuple[str | None, str, str]:
     _, key_type, public_key, comment = parse_ssh_key(path.read_text().strip())
     return key_type, public_key, comment
 
 
-def parse_ssh_key(key_string: str) -> tuple[str, str, str, str]:
+def parse_ssh_key(key_string: str) -> tuple[str | None, str, str, str]:
     parts = re.findall(r'(?:[^\s,"]|"(?:\\.|[^"])*")+', key_string)
 
     options = None
@@ -188,7 +194,7 @@ def parse_ssh_key(key_string: str) -> tuple[str, str, str, str]:
     return options, keytype, public_key, comment
 
 
-def parse_known_host(known_host_string: str) -> tuple[str, list, str, str, str]:
+def parse_known_host(known_host_string: str) -> tuple[str | None, list, str, str, str]:
     parts = known_host_string.split()
 
     marker = None
