3e4e6f9aec9e1720137e3d7c2e3bb99c111d2f0e
wbi <william.billaud@orange.com>
2025-10-30 17:46:59 +0100

    Add esxi hostd plugins (6&7)

---
+++ dissect/target/plugins/os/unix/esxi/log/hostd.py
@@ -0,0 +1,121 @@
+from __future__ import annotations
+
+import re
+from datetime import datetime
+from re import Pattern
+from typing import TYPE_CHECKING
+
+from dissect.target.exceptions import UnsupportedPluginError
+from dissect.target.helpers.fsutil import open_decompress
+from dissect.target.helpers.record import TargetRecordDescriptor
+from dissect.target.plugin import Plugin, export
+
+if TYPE_CHECKING:
+    from collections.abc import Iterator
+    from pathlib import Path
+
+    from dissect.target.target import Target
+
+HostdRecord = TargetRecordDescriptor(
+    "esxi/log/hostd",
+    [
+        ("datetime", "ts"),
+        ("string", "log_level"),
+        ("string", "application"),
+        ("string", "pid"),
+        ("string", "op_id"),
+        ("string", "user"),
+        ("string", "event_metadata"),
+        ("string", "message"),
+        ("path", "source"),
+    ],
+)
+
+
+class HostdPlugin(Plugin):
+    """Unix audit log plugin."""
+
+    RE_HOSTD: Pattern = re.compile(
+        r"""
+        (?P<ts>\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z) # ts, including milliseconds
+        \s
+        ((?P<log_level>\w+)\s)? # info, warning, of In(166), Wa(164) in esxi8+
+        ((?P<application>(\w+|-))\[(?P<pid>(\d+))\]|-)\s
+        ((\[(?P<metadata>([^\]]+))\])\s)?
+        (?P<message>.*)""",
+        re.VERBOSE,
+    )
+
+    def __init__(self, target: Target):
+        super().__init__(target)
+        self.log_paths = self.get_log_paths()
+
+    def check_compatible(self) -> None:
+        if not len(self.log_paths):
+            raise UnsupportedPluginError("No hostd path found")
+
+    def get_log_paths(self) -> list[Path]:
+        log_paths = []
+
+        log_paths.extend(self.target.fs.path("/var/log").glob("hostd.*"))
+        log_paths.extend(self.target.fs.path("/var/run/log").glob("hostd.*"))
+        log_paths.extend(self.target.fs.path("/var/lib/vmware/osdata").glob("hostd.*"))
+        if osdata_fs := self.target.osdata_fs():
+            log_paths.extend(osdata_fs.glob("log/hostd.*"))
+        return log_paths
+
+    @export(record=HostdRecord)
+    def hostd(self) -> Iterator[HostdRecord]:
+        """Return CentOS and RedHat audit information stored in /var/log/audit*.
+
+        The audit log file on a Linux machine stores security-relevant information.
+        Based on pre-configured rules. Log messages consist of space delimited key=value pairs.
+
+        References:
+            - https://knowledge.broadcom.com/external/article/306962/location-of-esxi-log-files.html
+        """
+        for path in self.log_paths:
+            try:
+                path = path.resolve(strict=True)
+                current_record = None
+                for line in open_decompress(path, "rt"):
+                    if not line:
+                        continue
+                    line = line.strip("\n")
+                    # For multiline event, line start with --> Before Esxi8
+                    # For Esxi8+, --> is after the Date loglevel application[pid] block
+                    if line.startswith("-->"):
+                        line = "\n" + line[3:]
+                        current_record.message = current_record.message + line
+                    else:
+                        if current_record:
+                            yield current_record
+                        if match := self.RE_HOSTD.match(line):
+                            log = match.groupdict()
+                            if metadata := log.get("metadata"):
+                                user = re.search(r"user=(\S+)", metadata)
+                                op_id = re.search(r"opID=(\S+)", metadata)
+                            else:
+                                user = None
+                                op_id = None
+                            current_record = HostdRecord(
+                                _target=self.target,
+                                message=log.get("message"),
+                                log_level=log.get("log_level", None),
+                                application=log.get("application", None),
+                                pid=log.get("pid", None),
+                                user=None if user is None else user.groups()[0],
+                                op_id=None if op_id is None else op_id.groups()[0],
+                                event_metadata=log.get("metadata", ""),
+                                ts=datetime.strptime(log["ts"], "%Y-%m-%dT%H:%M:%S.%f%z"),
+                                source=path,
+                            )
+                        else:
+                            self.target.log.warning("log file contains unrecognized format in %s", path)
+                            self.target.log.warning("log file contains unrecognized format in %s : %s", path, line)
+                            continue
+                if current_record:
+                    yield current_record
+            except Exception as e:
+                self.target.log.warning("An error occurred parsing hostd log file %s: %s", path, str(e), exc_info=e)
+                self.target.log.debug("", exc_info=e)
---
+++ tests/plugins/os/unix/esxi/log/test_hostd.py
@@ -0,0 +1,99 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from flow.record.fieldtypes import datetime as dt
+
+from dissect.target.plugins.os.unix.esxi.log.hostd import HostdPlugin
+from tests._utils import absolute_path
+
+if TYPE_CHECKING:
+    from dissect.target.filesystem import VirtualFilesystem
+    from dissect.target.target import Target
+
+
+def test_esxi_6_hostd_log(target_esxi: Target, fs_esxi: VirtualFilesystem) -> None:
+    """Test with log from an ESXi6"""
+    data_file = absolute_path("_data/plugins/os/unix/esxi/log/hostd/esxi6/hostd.1.gz")
+    fs_esxi.map_file("/var/log/hostd.1.gz", data_file)
+
+    target_esxi.add_plugin(HostdPlugin)
+
+    results = list(target_esxi.hostd())
+    assert len(results) == 2759
+    # line without application
+    assert results[0].ts == dt("2025-08-22T07:35:20.895Z")
+    assert results[0].application is None
+    assert (
+        results[0].message == "time the service was last started, Section for VMware ESX, "
+        "pid=2098599, version=6.7.0, build=14320388, option=Release"
+    )
+
+    # Multiline result
+    assert results[1466].ts == dt("2025-08-22T07:35:25.968Z")
+    assert results[1466].application == "hostd"
+    assert results[1466].log_level == "info"
+    assert results[1466].pid == "2098599"
+    assert results[1466].source == "/var/log/hostd.1.gz"
+    assert (
+        results[1466].message
+        == "Loaded vSAN management connection configuration.\n port: 80\n path: /vsanperf\n sdkTunnelPath: /vsanperf\n "
+        "retryDelaySec: 2\n maxPooledConnections: 2\n maxOpenConnections: 5\n privateKey: /etc/vmware/ssl/rui.key\n "
+        "certificate: /etc/vmware/ssl/rui.crt"
+    )
+
+    # Line with user/opID in metadata
+    assert results[1749].ts == dt("2025-08-22T07:35:28.264Z")
+    assert results[1749].user == "dcui"
+    assert results[1749].op_id == "vsan-bdc1-4715"
+    assert (
+        results[1749].message
+        == "Event 5 : User dcui@127.0.0.1 logged out (login time: Friday, 22 August, 2025 07:35:28 AM,"
+        " number of API invocations: 0, user agent: VMware-client/6.5.0)"
+    )
+
+
+def test_esxi_7_hostd_log(target_esxi: Target, fs_esxi: VirtualFilesystem) -> None:
+    """Test with log from an ESXi 7"""
+    data_file = absolute_path("_data/plugins/os/unix/esxi/log/hostd/esxi7/hostd.0.gz")
+    fs_esxi.map_file("/var/log/hostd.0.gz", data_file)
+
+    target_esxi.add_plugin(HostdPlugin)
+
+    results = list(target_esxi.hostd())
+    assert len(results) == 170
+    # line without application
+    assert results[0].ts == dt("2025-09-09T23:09:51.835Z")
+    assert results[0].application is None
+    assert (
+        results[0].message == "last log rotation time, 2022-11-09T09:15:36.354Z - time the service was last started "
+        "2022-11-09T09:15:36.339Z, Section for VMware ESX, pid=2099676, "
+        "version=7.0.3, build=20328353, option=Release"
+    )
+
+    # Line with opId and user
+    assert results[12].ts == dt("2025-09-09T23:10:06.616Z")
+    assert results[12].log_level == "info"
+    assert results[12].user == "vpxuser:VSPHERE.LOCAL\\vpxd-extension-b37a7345-e529-49c4-83a8-da77a48b1ce4"
+    assert results[12].op_id == "sps-Main-112793-72-459406-de-2b-3426"
+    assert (
+        results[12].message
+        == "Task Completed : haTask--vim.vslm.host.CatalogSyncManager.queryCatalogChange-118109847 Status success"
+    )
+
+    # multiline
+    assert results[29].ts == dt("2025-09-09T23:10:51.079Z")
+    assert results[29].application == "hostd"
+    assert results[29].log_level == "info"
+    assert results[29].pid == "2100292"
+    assert results[29].source == "/var/log/hostd.0.gz"
+    assert (
+        results[29].message
+        == "VmkVprobSource::Post event: (vim.event.EventEx) {\n    key = 90,\n    chainId = -1,\n    createdTime = "
+        '"1970-01-01T00:00:00Z",\n    userName = "",\n    host = (vim.event.HostEventArgument) {\n       name = '
+        '"SRVESX12",\n       host = \'vim.HostSystem:ha-host\'\n    },\n    eventTypeId = "esx.audit.ssh.'
+        'session.opened",\n    arguments = (vmodl.KeyAnyValue) [\n       (vmodl.KeyAnyValue) {\n          '
+        'key = "1",\n          value = "root"\n       },\n       (vmodl.KeyAnyValue) {\n          key = "2",\n   '
+        '       value = "192.168.101.96"\n       }\n    ],\n    objectId = "ha-host",\n    '
+        'objectType = "vim.HostSystem",\n }'
+    )
