3b2318b372bfb98c204f0c19abddc437b1a18b61
qmadev <190383216+qmadev@users.noreply.github.com>
2025-12-04 16:10:26 +0100

    Lint

--- dissect/target/plugins/os/windows/log/mssql.py
+++ dissect/target/plugins/os/windows/log/mssql.py
@@ -99,7 +99,9 @@ class MssqlPlugin(Plugin):
                     if previous_ts := RE_TIMESTAMP_PATTERN.match(buf):
                         if current_ts := RE_TIMESTAMP_PATTERN.match(line):
                             yield MssqlErrorlogRecord(
-                                ts=datetime.strptime(previous_ts.group(), "%Y-%m-%d %H:%M:%S.%f").replace(tzinfo=timezone.utc),
+                                ts=datetime.strptime(previous_ts.group(), "%Y-%m-%d %H:%M:%S.%f").replace(
+                                    tzinfo=timezone.utc
+                                ),
                                 instance=instance,
                                 # The process name is a fixed-width field and is always 12 characters long.
                                 process=buf[23:35].strip(),
@@ -116,7 +118,9 @@ class MssqlPlugin(Plugin):
                 if buf:
                     if current_ts := RE_TIMESTAMP_PATTERN.match(buf):
                         yield MssqlErrorlogRecord(
-                            ts=datetime.strptime(current_ts.group(), "%Y-%m-%d %H:%M:%S.%f").replace(tzinfo=timezone.utc),
+                            ts=datetime.strptime(current_ts.group(), "%Y-%m-%d %H:%M:%S.%f").replace(
+                                tzinfo=timezone.utc
+                            ),
                             instance=instance,
                             # The process name is a fixed-width field and is always 12 characters long.
                             process=buf[23:35].strip(),
@@ -124,7 +128,6 @@ class MssqlPlugin(Plugin):
                             path=errorlog,
                             _target=self.target,
                         )
-
 
     def _find_instances(self) -> set[str, TargetPath]:
         return {
