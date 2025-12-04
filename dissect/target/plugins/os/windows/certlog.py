0da840bbe4e28026680cfd3a371d47f99189143d
wbi <william.billaud@orange.com>
2025-12-04 17:24:06 +0100

    Add requests plugin

--- dissect/target/plugins/os/windows/certlog.py
+++ dissect/target/plugins/os/windows/certlog.py
@@ -6,20 +6,21 @@ from dissect.database.ese.tools import certlog
 from dissect.database.exception import Error
 
 from dissect.target.exceptions import UnsupportedPluginError
+from dissect.target.helpers.logging import get_logger
 from dissect.target.helpers.record import TargetRecordDescriptor
 from dissect.target.plugin import Plugin, export
 
 if TYPE_CHECKING:
     from collections.abc import Iterator
     from pathlib import Path
+
     from dissect.target.target import Target
+
+log = get_logger(__name__)
 
 CertificateExtensionsRecord = TargetRecordDescriptor(
     "filesystem/windows/certlog/certificate_extension",
-    [
-        ("datetime", "ts_submittedWhen"),
-        ("string", "database")
-    ],
+    [("datetime", "ts_submittedWhen"), ("path", "path")],
 )
 
 CertificatesRecord = TargetRecordDescriptor(
@@ -65,37 +66,107 @@ RequestAttributesRecord = TargetRecordDescriptor(
 )
 
 RequestsRecord = TargetRecordDescriptor(
-    "filesystem/windows/sru/energy_usage_lt",
+    "filesystem/windows/certlog/request",
     [
-        ("datetime", "ts"),
-        ("path", "app"),
-        ("string", "user"),
-        ("varint", "active_ac_time"),
-        ("varint", "cs_ac_time"),
-        ("varint", "active_dc_time"),
-        ("varint", "cs_dc_time"),
-        ("varint", "active_discharge_time"),
-        ("varint", "cs_discharge_time"),
-        ("varint", "active_energy"),
-        ("varint", "cs_energy"),
-        ("varint", "designed_capacity"),
-        ("varint", "full_charged_capacity"),
-        ("varint", "cycle_count"),
-        ("varint", "configuration_hash"),
+        ("string", "caller_name"),
+        ("string", "common_name"),
+        ("string", "country"),
+        ("string", "device_serial_number"),
+        ("string", "disposition_message"),
+        ("string", "distinguished_name"),
+        ("string", "domain_component"),
+        ("string", "e_mail"),
+        ("string", "endorsement_certificate_hash"),
+        ("string", "endorsement_key_hash"),
+        ("string", "given_name"),
+        ("string", "initials"),
+        ("string", "key_recovery_hashes"),
+        ("string", "locality"),
+        ("string", "organization"),
+        ("string", "organizational_unit"),
+        ("string", "request_attributes"),
+        ("string", "requester_name"),
+        ("string", "signer_application_policies"),
+        ("string", "signer_policies"),
+        ("string", "state_or_province"),
+        ("string", "street_address"),
+        ("string", "sur_name"),
+        ("string", "title"),
+        ("string", "unstructured_address"),
+        ("string", "unstructured_name"),
+        ("string", "attestation_challenge"),
+        ("string", "disposition"),
+        ("string", "raw_archived_key"),
+        ("string", "raw_name"),
+        ("string", "raw_old_certificate"),
+        ("string", "raw_precertificate"),
+        ("string", "raw_request"),
+        ("string", "request_flags"),
+        ("varint", "request_id"),
+        ("string", "request_type"),
+        ("datetime", "resolved_when"),
+        ("string", "revoked_effective_when"),
+        ("string", "revoked_reason"),
+        ("datetime", "revoked_when"),
+        ("string", "status_code"),
+        ("datetime", "submitted_when"),
+        ("string", "table_name"),
+        ("path", "source"),
     ],
 )
 
 CertLogRecord = Union[  # noqa: UP007
-    RequestsRecord,
-    RequestAttributesRecord,
-    CRLsRecord,
-    CertificatesRecord,
-    CertificateExtensionsRecord
+    RequestsRecord, RequestAttributesRecord, CRLsRecord, CertificatesRecord, CertificateExtensionsRecord
 ]
 
 TRANSFORMS = {}
+# {i: "".join("_" + c.lower() if c.isupper() else c for c in i.replace('$', ''))[1:] for i in a}
+FIELD_MAPPINGS = {
+    "$CallerName": "caller_name",
+    "$CommonName": "common_name",
+    "$Country": "country",
+    "$DeviceSerialNumber": "device_serial_number",
+    "$DispositionMessage": "disposition_message",
+    "$DistinguishedName": "distinguished_name",
+    "$DomainComponent": "domain_component",
+    "$EMail": "e_mail",
+    "$EndorsementCertificateHash": "endorsement_certificate_hash",
+    "$EndorsementKeyHash": "endorsement_key_hash",
+    "$GivenName": "given_name",
+    "$Initials": "initials",
+    "$KeyRecoveryHashes": "key_recovery_hashes",
+    "$Locality": "locality",
+    "$Organization": "organization",
+    "$OrganizationalUnit": "organizational_unit",
+    "$RequestAttributes": "request_attributes",
+    "$RequesterName": "requester_name",
+    "$SignerApplicationPolicies": "signer_application_policies",
+    "$SignerPolicies": "signer_policies",
+    "$StateOrProvince": "state_or_province",
+    "$StreetAddress": "street_address",
+    "$SurName": "sur_name",
+    "$Title": "title",
+    "$UnstructuredAddress": "unstructured_address",
+    "$UnstructuredName": "unstructured_name",
+    "AttestationChallenge": "attestation_challenge",
+    "Disposition": "disposition",
+    "RawArchivedKey": "raw_archived_key",
+    "RawName": "raw_name",
+    "RawOldCertificate": "raw_old_certificate",
+    "RawPrecertificate": "raw_precertificate",
+    "RawRequest": "raw_request",
+    "RequestFlags": "request_flags",
+    "RequestID": "request_id",
+    "RequestType": "request_type",
+    "ResolvedWhen": "resolved_when",
+    "RevokedEffectiveWhen": "revoked_effective_when",
+    "RevokedReason": "revoked_reason",
+    "RevokedWhen": "revoked_when",
+    "StatusCode": "status_code",
+    "SubmittedWhen": "submitted_when",
+    "TableName": "table_name",
+}
 
-FIELD_MAPPINGS = {}
 
 class CertLogPlugin(Plugin):
     """Return all available SRUM data stored in the SRUDB.dat.
@@ -112,15 +183,13 @@ class CertLogPlugin(Plugin):
 
     def __init__(self, target: Target):
         super().__init__(target)
-        print(f"Is direct {self.target.is_direct}")
-        self._certlog_db = []
+        self._certlog_dbs: list[certlog.CertLog, Path] = []
         for path in self.get_paths():
             try:
-                self._certlog_db.append(certlog.CertLog(path.open()))
+                self._certlog_dbs.append((certlog.CertLog(path.open()), path))
             except Error as e:
                 self.target.log.warning("Error opening Certlog database")
                 self.target.log.debug("", exc_info=e)
-        print("HERE")
 
     def _get_paths(self) -> Iterator[Path]:
         """Return all artifact files of interest to the plugin.
@@ -129,41 +198,34 @@ class CertLogPlugin(Plugin):
         """
         certlog_dir = self.target.resolve("%windir%/System32/CertLog")
         if certlog_dir.exists():
-            yield from certlog_dir.glob('*.edb')
+            yield from certlog_dir.glob("*.edb")
 
     def check_compatible(self) -> None:
-        if not self._certlog_db:
-            raise UnsupportedPluginError("No Certlog Database found")
+        if not self._certlog_dbs:
+            raise UnsupportedPluginError("No Certlog Databases found")
 
     def read_records(self, table_name: str, record_type: CertLogRecord) -> Iterator[CertLogRecord]:
-        for db in self.db:
-            table = self._sru.get_table(table_name=table_name)
+        for db, path in self._certlog_dbs:
+            table = [table for table in db.tables() if table.name == table_name]
             if not table:
                 self.target.log.warning("Table not found: %s", table_name)
                 return iter(())
-
-            columns = [c.name for c in table.columns]
-            if columns[:4] != ["AutoIncId", "TimeStamp", "AppId", "UserId"]:
-                raise ValueError(f"Unexpected table layout in SRU iteration: {table} ({columns[:4]})")
-            columns = columns[1:]
-
-            for entry in self._sru.get_table_entries(table=table):
+            columns = [c.name for c in table[0].columns]
+            for entry in db.records(table_name=table_name):
                 values = (entry[name] for name in columns)
                 column_values = zip(columns, values, strict=False)
 
                 record_values = {}
                 for column, value in column_values:
-                    new_value = value
-                    if new_value and (transform := TRANSFORMS.get(column)):
-                        if isinstance((transformed_value := transform(new_value)), str):
-                            new_value = self.target.fs.path(transformed_value)
-                        else:
-                            new_value = transformed_value
-                    new_column = FIELD_MAPPINGS.get(column, column)
-                    record_values[new_column] = new_value
+                    new_column = FIELD_MAPPINGS.get(column)
+                    if new_column:
+                        record_values[new_column] = value
+                    else:
+                        self.target.log.debug("Unexpected columns for table %s : %s", table_name, column)
 
                 yield record_type(
                     _target=self.target,
+                    source=path,
                     **record_values,
                 )
 
@@ -173,4 +235,4 @@ class CertLogPlugin(Plugin):
 
         Gives insight into the network usage of the system.
         """
-        yield from self.read_records("network_data", RequestsRecord)
+        yield from self.read_records("Requests", RequestsRecord)
