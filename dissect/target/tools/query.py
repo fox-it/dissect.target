c9723ab5fe4bb2d8c5883c3f18d82f4db7c777c1
Roel de Jong <12800443+twiggler@users.noreply.github.com>
2026-01-06 16:50:01 +0100

    Process review comments: - Do not instantiate full instance of loader when displaying help - Process all known arguments

--- dissect/target/tools/query.py
+++ dissect/target/tools/query.py
@@ -125,9 +125,7 @@ def main() -> int:
             return 1
 
         # The loader now knows how to print its own help.
-        # We just need to instantiate it. Note that the path is not important here.
-        loader_instance = loader_cls(pathlib.Path())
-        loader_instance.print_help()
+        loader_cls.print_help()
         return 0
 
     process_generic_arguments(parser, args)
@@ -167,8 +165,7 @@ def main() -> int:
     execution_report.set_event_callbacks(Target)
 
     try:
-        # Pass the loader-specific arguments (`rest`) to open_targets
-        for target in open_targets(args, loader_args=rest):
+        for target in open_targets(args):
             record_entries: list[tuple[FunctionDescriptor, Iterator[Record]]] = []
             basic_entries = []
             yield_entries = []
