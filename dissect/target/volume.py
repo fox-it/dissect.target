d3dd5308ff4e3f29639a9545ae5dcfad358f31c6
Schamper <1254028+Schamper@users.noreply.github.com>
2026-01-12 11:52:54 +0100

    Suggested change

--- dissect/target/volume.py
+++ dissect/target/volume.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import io
+from functools import cached_property
 from typing import TYPE_CHECKING, Any, BinaryIO
 
 from dissect.target.exceptions import VolumeSystemError
@@ -76,9 +77,8 @@ class VolumeSystem:
         self, fh: BinaryIO | list[BinaryIO], disk: BinaryIO | list[BinaryIO] | None = None, serial: str | None = None
     ):
         self.fh = fh
-        self._disk = disk
+        self.disk = disk or fh
         self.serial = serial
-        self._volumes_list: list[Volume] = None
 
         if self.__type__ is None:
             raise NotImplementedError(f"{self.__class__.__name__} must define __type__")
@@ -126,24 +126,10 @@ class VolumeSystem:
         """
         raise NotImplementedError
 
-    @property
+    @cached_property
     def volumes(self) -> list[Volume]:
         """A list of all the discovered volumes."""
-        if self._volumes_list is None:
-            self._volumes_list = list(self._volumes())
-
-        return self._volumes_list
-
-    @property
-    def disk(self) -> BinaryIO | list[BinaryIO]:
-        """The source disk or container on which the volume system is opened."""
-        if self._disk:
-            return self._disk
-
-        if isinstance(self.fh, list):
-            return [f.disk if isinstance(f, Volume) else f for f in self.fh]
-
-        return self.fh.disk if isinstance(self.fh, Volume) else self.fh
+        return list(self._volumes())
 
 
 class EncryptedVolumeSystem(VolumeSystem):
@@ -249,18 +235,6 @@ class LogicalVolumeSystem(VolumeSystem):
         """
         raise NotImplementedError
 
-    @property
-    def disk(self) -> BinaryIO | list[BinaryIO]:
-        """The source disk or container on which the volume system is opened."""
-        if self._disk:
-            return self._disk
-
-        if isinstance(self.fh, list):
-            return [dev.fh.disk if isinstance(dev.fh, Volume) else dev.fh for dev in self.fh]
-
-        dev = self.fh
-        return dev.fh.disk if isinstance(dev.fh, Volume) else dev.fh
-
 
 class Volume(io.IOBase):
     """A representation of a volume on disk.
@@ -365,7 +339,7 @@ def open(fh: BinaryIO, *args, **kwargs) -> DissectVolumeSystem:
     fh.seek(0)
 
     try:
-        return disk.DissectVolumeSystem(fh)
+        return disk.DissectVolumeSystem(fh, *args, **kwargs)
     except Exception as e:
         raise VolumeSystemError(f"Failed to load volume system for {fh}") from e
     finally:
@@ -409,7 +383,7 @@ def is_encrypted(volume: BinaryIO) -> bool:
     return False
 
 
-def open_encrypted(volume: BinaryIO) -> Iterator[Volume]:
+def open_encrypted(volume: BinaryIO, *args, **kwargs) -> Iterator[Volume]:
     """Open an encrypted ``volume``.
 
     An encrypted volume can only be opened if the encrypted volume system can successfully decrypt the volume,
@@ -426,7 +400,7 @@ def open_encrypted(volume: BinaryIO) -> Iterator[Volume]:
     for manager_cls in ENCRYPTED_VOLUME_MANAGERS:
         try:
             if manager_cls.detect(volume):
-                volume_manager = manager_cls(volume)
+                volume_manager = manager_cls(volume, *args, **kwargs)
                 yield from volume_manager.volumes
         except ImportError as e:  # noqa: PERF203
             log.info("Failed to import %s", manager_cls)
@@ -439,7 +413,7 @@ def open_encrypted(volume: BinaryIO) -> Iterator[Volume]:
     return None
 
 
-def open_lvm(volumes: list[BinaryIO], *args, **kwargs) -> Iterator[VolumeSystem]:
+def open_lvm(volumes: list[BinaryIO]) -> Iterator[VolumeSystem]:
     """Open a single logical volume system on a list of file-like objects.
 
     Args:
