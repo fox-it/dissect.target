4eefc5e885a173913ac25af42837e2985cbaab16
wbi <william.billaud@orange.com>
2026-01-14 13:51:29 +0100

    Add tests related to qcow2 container format + enhance test

--- tests/containers/test_ewf.py
+++ tests/containers/test_ewf.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+import os
+
 from dissect.target import container
 from dissect.target.containers.ewf import EwfContainer
 from tests._utils import absolute_path
@@ -13,3 +15,7 @@ def test_ewf_container() -> None:
     assert isinstance(fh, EwfContainer)
     a = fh.read(20)
     assert a == b"testdissecte01\n"
+    assert fh.tell() == 15
+    fh.seek(0, whence=os.SEEK_SET)
+    assert fh.read(20) == b"testdissecte01\n"
+    fh.close()
--- tests/containers/test_qcow2.py
+++ tests/containers/test_qcow2.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+import os
+
 from dissect.target import container
 from dissect.target.containers.qcow2 import QCow2Container
 from tests._utils import absolute_path
@@ -14,3 +16,7 @@ def test_qcow2_container() -> None:
     a = fh.read(20)
     # trailing \x00 are expected. This is the same when using qemu-nbd
     assert a == b"testdissecteqcow2\n\x00\x00"
+    assert fh.tell() == 20
+    fh.seek(0, whence=os.SEEK_SET)
+    assert fh.read(20) == b"testdissecteqcow2\n\x00\x00"
+    fh.close()
