e25d5e3cd7204f31054026a17fe0c144cd7d0ab6
wbi <william.billaud@orange.com>
2026-01-14 13:22:59 +0100

    Add tests related to ewf

---
+++ tests/containers/test_ewf.py
@@ -0,0 +1,12 @@
+from dissect.target import container
+from dissect.target.containers.ewf import EwfContainer
+from tests._utils import absolute_path
+
+
+def test_split_container() -> None:
+    path = absolute_path("_data/containers/ewf/small.E01")
+
+    fh = container.open(path)
+    assert isinstance(fh, EwfContainer)
+    a = fh.read(20)
+    assert a == b"testdissecte01\n"
