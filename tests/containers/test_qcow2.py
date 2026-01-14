ad8ee725e3e1c62cff132b56220a2447153294ed
wbi <william.billaud@orange.com>
2026-01-14 13:47:43 +0100

    Add tests related to qcow2 container format

---
+++ tests/containers/test_qcow2.py
@@ -0,0 +1,16 @@
+from __future__ import annotations
+
+from dissect.target import container
+from dissect.target.containers.qcow2 import QCow2Container
+from tests._utils import absolute_path
+
+
+def test_qcow2_container() -> None:
+    """Test that QCOW2 containers are properly opened."""
+    path = absolute_path("_data/containers/qcow2/small.qcow2")
+
+    fh = container.open(path)
+    assert isinstance(fh, QCow2Container)
+    a = fh.read(20)
+    # trailing \x00 are expected. This is the same when using qemu-nbd
+    assert a == b"testdissecteqcow2\n\x00\x00"
