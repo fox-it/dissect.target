c8a2a8e7b50c04296e12c77949d4d1e6329cf4b5
JSCU-CNI <121175071+JSCU-CNI@users.noreply.github.com>
2026-01-15 17:23:25 +0100

    add test

--- tests/plugins/os/unix/esxi/test__os.py
+++ tests/plugins/os/unix/esxi/test__os.py
@@ -1,13 +1,15 @@
 from __future__ import annotations
 
+from io import BytesIO
 from typing import TYPE_CHECKING
 from unittest.mock import patch
 
+from dissect.target.filesystem import VirtualFilesystem
 from dissect.target.plugin import OperatingSystem
 from dissect.target.plugins.os.unix.esxi._os import ESXiPlugin, _create_local_fs, _decrypt_crypto_util
+from tests._utils import absolute_path
 
 if TYPE_CHECKING:
-    from dissect.target.filesystem import VirtualFilesystem
     from dissect.target.target import Target
 
 
@@ -73,3 +75,23 @@ def test_esxi_os_detection(target_bare: Target, fs_esxi: VirtualFilesystem) -> N
     assert target_bare.hostname == "localhost"
     assert target_bare.version == "6.7.0"
     assert target_bare.ips == ["192.168.56.101"]
+
+
+def test_esxi_os_creation_version_7(target_bare: Target) -> None:
+    """Test handling of ``ESXiPlugin.create`` for ESXi 7 with separate partitions.
+    Indirectly tests the ESXi configstore plugin."""
+
+    fs1 = VirtualFilesystem()
+    fs1.map_file_fh("boot.cfg", BytesIO(b"build=7.13.37-1.2.3.4\nmodules=example.v00 --- example.tgz\n"))
+    fs1.map_file_fh("example.v00", BytesIO(b""))
+    target_bare.filesystems.add(fs1)
+
+    fs2 = VirtualFilesystem()
+    fs2.map_file_fh("/etc/vmware/esx.conf", BytesIO(b'/resourceGroups/version = "7.13.37"\n'))
+    fs2.map_file("/var/lib/vmware/configstore/backup/current-store-1", absolute_path("_data/plugins/os/unix/esxi/current-store-1"))
+    target_bare.filesystems.add(fs2)
+
+    target_bare.apply()
+
+    assert ESXiPlugin.detect(target_bare)
+    assert ESXiPlugin.create(target_bare, fs1)
