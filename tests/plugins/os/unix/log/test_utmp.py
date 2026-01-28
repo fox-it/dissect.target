432418f91bab0b938d1c4767d250be32dcd18e59
JSCU-CNI <121175071+JSCU-CNI@users.noreply.github.com>
2026-01-28 15:33:25 +0100

    Add support for wtmpdb

--- tests/plugins/os/unix/log/test_utmp.py
+++ tests/plugins/os/unix/log/test_utmp.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 from datetime import datetime, timezone
 from typing import TYPE_CHECKING
 
-from dissect.target.plugins.os.unix.log.utmp import UtmpPlugin
+from dissect.target.plugins.os.unix.log.utmp import UtmpPlugin, WtmpDbEntryType
 from tests._utils import absolute_path
 
 if TYPE_CHECKING:
@@ -79,3 +79,43 @@ def test_utmp_plugin(target_linux: Target, fs_linux: VirtualFilesystem) -> None:
     target_linux.add_plugin(UtmpPlugin)
     results = list(target_linux.utmp())
     assert len(results) == 70
+
+
+def test_wtmpdb(target_linux: Target, fs_linux: VirtualFilesystem) -> None:
+    """Test if we can parse a /var/log/wtmp.db SQLite3 file from libpam-wtmpdb."""
+
+    fs_linux.map_file("/var/log/wtmp.db", absolute_path("_data/plugins/os/unix/log/wtmp/wtmp.db"))
+    fs_linux.map_file("/var/lib/wtmpdb/wtmp.db", absolute_path("_data/plugins/os/unix/log/wtmp/wtmp.db"))
+    fs_linux.map_file("/var/lib/wtmpdb/wtmp_20261231.db", absolute_path("_data/plugins/os/unix/log/wtmp/wtmp.db"))
+
+    plugin = target_linux.add_plugin(UtmpPlugin)
+    results = list(target_linux.wtmp())
+
+    assert len(results) == 3
+    assert list(map(str, plugin.wtmp_paths)) == [
+        "/var/log/wtmp.db",
+        "/var/lib/wtmpdb/wtmp.db",
+        "/var/lib/wtmpdb/wtmp_20261231.db",
+    ]
+
+    assert results[0].ts == datetime(2026, 1, 28, 13, 13, 52, 776830, tzinfo=timezone.utc)
+    assert not results[0].ts_logout  # user is still logged in
+    assert results[0].ut_type == WtmpDbEntryType.USER_PROCESS.name
+    assert results[0].ut_user == "root"
+    assert results[0].ut_line == "pts/0"
+    assert not results[0].ut_host
+    assert not results[0].ut_addr
+    assert results[0].ut_service == "su"
+    assert results[0].source == "/var/log/wtmp.db"
+    assert results[0].hostname == "localhost"
+
+    assert results[1].ts == datetime(2026, 1, 28, 13, 13, 52, 776830, tzinfo=timezone.utc)
+    assert results[1].ts_logout == datetime(2026, 1, 28, 13, 13, 52, 876830, tzinfo=timezone.utc)
+    assert results[1].ut_type == WtmpDbEntryType.USER_PROCESS.name
+    assert results[1].ut_user == "root"
+    assert results[1].ut_line == "ssh"
+    assert results[1].ut_host == "127.0.0.1"
+    assert results[1].ut_addr == "127.0.0.1"
+    assert results[1].ut_service == "sshd"
+    assert results[1].source == "/var/log/wtmp.db"
+    assert results[1].hostname == "localhost"
