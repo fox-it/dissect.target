e83ae3b691006cae581e8ede0ed9b2ff82321348
JSCU-CNI <121175071+JSCU-CNI@users.noreply.github.com>
2026-02-12 17:30:09 +0100

    implement feedback

--- dissect/target/plugins/os/windows/credential/credential.py
+++ dissect/target/plugins/os/windows/credential/credential.py
@@ -1,15 +1,7 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
-
 from dissect.target.plugin import NamespacePlugin
-
-if TYPE_CHECKING:
-    from dissect.target.target import Target
 
 
 class WindowsCredentialPlugin(NamespacePlugin):
     __namespace__ = "credential"
-
-    def __init__(self, target: Target):
-        super().__init__(target)
