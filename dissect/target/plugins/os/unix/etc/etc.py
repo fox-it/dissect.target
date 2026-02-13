bcd6d6714683c8cc1a99838653f41340d842d4a6
Miauwkeru <Miauwkeru@users.noreply.github.com>
2026-02-13 11:17:14 +0100

    Rework etc.etc plugin and add some meaning behind the values
    Reduces the duplication inside the records

--- dissect/target/plugins/os/unix/etc/etc.py
+++ dissect/target/plugins/os/unix/etc/etc.py
@@ -2,7 +2,6 @@ from __future__ import annotations
 
 import fnmatch
 import re
-from pathlib import Path
 from typing import TYPE_CHECKING
 
 from dissect.target.helpers import configutil
@@ -12,11 +11,13 @@ from dissect.target.plugin import Plugin, arg, export
 if TYPE_CHECKING:
     from collections.abc import Iterator
 
+    from dissect.target.helpers.fsutil import TargetPath
+
 UnixConfigTreeRecord = TargetRecordDescriptor(
     "unix/config",
     [
         ("path", "source"),
-        ("path", "path"),
+        ("path", "config_path"),
         ("string", "key"),
         ("string[]", "value"),
     ],
@@ -32,13 +33,13 @@ class EtcTree(Plugin):
         return None
 
     def _sub(
-        self, items: configutil.ConfigurationParser, entry: Path, orig_path: Path, pattern: str
+        self, items: configutil.ConfigurationParser, entry: TargetPath, orig_path: TargetPath, pattern: str
     ) -> Iterator[UnixConfigTreeRecord]:
         index = 0
 
         for raw_key, value in items.items():
-            key = re.sub(r"[\n\r\t]", "", raw_key)
-            path = Path(entry) / Path(key)
+            key = re.sub(r"[\n\r\t/]", "", raw_key)
+            path = entry / key
 
             if isinstance(value, dict):
                 yield from self._sub(value, path, orig_path, pattern)
@@ -48,10 +49,11 @@ class EtcTree(Plugin):
                 value = [str(value)]
 
             if fnmatch.fnmatch(path, pattern):
+                config_path = entry.relative_to(orig_path)
                 data = {
                     "_target": self.target,
-                    "source": self.target.fs.path(orig_path),
-                    "path": path,
+                    "source": orig_path,
+                    "config_path": "/" / config_path,
                     "key": key,
                     "value": value,
                 }
@@ -65,19 +67,35 @@ class EtcTree(Plugin):
     @export(record=UnixConfigTreeRecord)
     @arg("--glob", dest="pattern", default="*", help="Glob-style pattern to search for")
     @arg("--root", dest="root", default="/etc", help="Path to use as root for search")
-    @arg("--unknowns", dest="unknowns", action="store_false", help="Return unknown / unparsable objects")
+    @arg("--unknowns", dest="unknowns", action="store_true", help="Return unknown / unparsable objects")
     def etc(self, pattern: str, root: str, unknowns: bool) -> Iterator[UnixConfigTreeRecord]:
-        """Yield etc configuration records."""
+        """This plugin yields configuration information from the etc directory in key value pairs.
+
+        Args:
+            pattern: What Glob-style pattern to search for
+            root: Path to use as root for searching
+            unknowns: Whether to also yield entries for unknown entries.
+
+        Yields UnixConfigTreeRecord with the following fields:
+
+        .. code-block:: text
+
+            source (path): The path on the target used for parsing.
+            config_path (path): The path inside the configuration file that is being used.
+            key (string): The configuration key returned by parsing.
+            value (string[]): The configuration value belonging to the key.
+        """
 
         for entry, _, items in self.target.fs.walk(root):
             for item in items:
+                path = self.target.fs.path(entry) / item
                 try:
-                    path = Path(entry) / item
                     config_object = configutil.parse(self.target.fs.path(path))
+
                     if not unknowns and isinstance(config_object, (configutil.Txt, configutil.Bin)):
                         # We don't have a specific config parser for this file,
                         # Ignore those config paths to get proper records
                         continue
                     yield from self._sub(config_object, path, orig_path=path, pattern=pattern)
                 except Exception:
-                    self.target.log.warning("Could not open configuration item: %s", item)
+                    self.target.log.warning("Could not parse file: %s", path)
