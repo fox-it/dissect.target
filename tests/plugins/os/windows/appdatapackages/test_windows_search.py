a39b8009d644d9a9b293fe12ce6a05b12fe7733a
loaflover <34983803+loaflover@users.noreply.github.com>
2026-03-10 13:27:54 +0200

    last test

--- tests/plugins/os/windows/appdatapackages/test_windows_search.py
+++ tests/plugins/os/windows/appdatapackages/test_windows_search.py
@@ -1,16 +1,13 @@
 from __future__ import annotations
 
-import re
 from typing import TYPE_CHECKING
 
 import pytest
 from flow.record.fieldtypes import datetime as dt
 
-from dissect.target.helpers import keychain
-from dissect.target.helpers.fsutil import TargetPath
 from tests._utils import absolute_path
 
-from dissect.target.plugins.os.windows.AppDataPackages.settings_cache import settings_cache
+from dissect.target.plugins.os.windows.AppDataPackages.windows_search import app_cache
 
 if TYPE_CHECKING:
     from dissect.target.filesystem import VirtualFilesystem
@@ -18,23 +15,23 @@ if TYPE_CHECKING:
 
 
 @pytest.fixture
-def target_settings_cache(target_win_users: Target, fs_win: VirtualFilesystem) -> Target:
+def target_app_cache(target_win_users: Target, fs_win: VirtualFilesystem) -> Target:
     fs_win.map_file(
-        "Users\\John\\AppData\\Local\\Packages\\Microsoft.Windows.Search_cw5n1h2txyewy\\LocalState\\DeviceSearchCache\\SettingsCache.txt",
-        absolute_path("_data/plugins/os/windows/AppDataPackages/settingscache.txt"),
+        "Users\\John\\AppData\\Local\\Packages\\Microsoft.Windows.Search_cw5n1h2txyewy\\LocalState\\DeviceSearchCache\\AppCache134176063414577965.txt",
+        absolute_path("_data/plugins/os/windows/AppDataPackages/AppCache123123123.txt"),
     )
 
-    target_win_users.add_plugin(settings_cache)
+    target_win_users.add_plugin(app_cache)
 
     return target_win_users
 
 
-def test_settings_cache(target_settings_cache: Target):
-    results = list(target_settings_cache.settingscache())
+def test_settings_cache(target_app_cache: Target):
+    results = list(target_app_cache.appcache())
 
     assert len(results) == 2
     print(results[0])
-    assert results[0].ParsingName == 'examplesetting'
-    assert results[0].ActivationContext == '%windir%\\system32\\rundll32.exe %windir%\\system32\\example\\setting.dll'
-    assert results[0].SettingID == '{11223344-1234-ABCD-EFGH-123456789123}'
-    assert results[0].Comment == 'idkwhat to write here tbh'
+    assert results[0].ParsingName == 'Z8X7C6V5B4N3M2L1'
+    assert results[0].FileExtension == '.qwe'
+    assert results[0].FileName == 'random_tool'
+    assert results[0].PackageFullName == 'com.random.tool'
