489e07006ed563758166f419537d4c3c95b06b6a
Miauwkeru <Miauwkeru@users.noreply.github.com>
2026-04-02 08:39:01 +0000

    Fix the side effects of module imports when running tests in a specific order
    This issue is best illustrated with the SMBLoader.
    
    When testing the SMBLoader, we monkeypatch the module out of
    `sys.modules`. So we can whether the import behaviour works as intended.
    The mocking of the loader module (`dissect.target.loaders.smb`) occurs
    at the start of that test. This only happens if that loader was inside
    `sys.modules`.
    
    However, when the loader module isn't in `sys.modules` the tests still
    import the `dissect.target.loaders.smb` module.
    This module contains all the references to the mocked libraries that
    were used to import that module. This "tainted" module will exist during
    the whole test cycle.
    
    The biggest effect observed from this behaviour was on
    test_target.py::test_exception_invalid_path() where it created a memory
    leak during test execution. Which is why the test sometimes failed.

--- tests/_utils.py
+++ tests/_utils.py
@@ -1,6 +1,15 @@
 from __future__ import annotations
 
+import sys
+from contextlib import contextmanager
 from pathlib import Path
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from collections.abc import Iterator
+    from types import ModuleType
+
+    import pytest
 
 
 def absolute_path(filename: str) -> Path:
@@ -10,3 +19,26 @@ def absolute_path(filename: str) -> Path:
 def mkdirs(root: Path, paths: list[str]) -> None:
     for path in paths:
         root.joinpath(path).mkdir(parents=True)
+
+
+@contextmanager
+def cleanup_modules(modules: list[str], monkeypatch: pytest.MonkeyPatch) -> Iterator[None]:
+    """Removes or reverts modules back into sys.modules.
+
+    There can be cases that a "tainted" module gets imported using monkeypatch.
+    This still has references to the mock objects and effects other tests.
+    """
+    prev_modules: dict[str, ModuleType | None] = {}
+    with monkeypatch.context() as m:
+        for mod in modules:
+            prev_modules.update({mod: sys.modules.get(mod)})
+            # Remove the module inside this monkeypatch context
+            if mod in sys.modules:
+                m.delitem(sys.modules, mod)
+
+        yield
+
+    for name, module in prev_modules.items():
+        if module is None and name in sys.modules:
+            # Delete the module if it was created during the test
+            del sys.modules[name]
