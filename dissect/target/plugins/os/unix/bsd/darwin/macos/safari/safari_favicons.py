7704d03e324c40667a289f97af4d282fcfb7df14
The Eternal Emperor <127401095+EmperorValkorion@users.noreply.github.com>
2026-06-09 16:34:16 +0200

    Add Safari favicons and per site preferences plugins

---
+++ dissect/target/plugins/os/unix/bsd/darwin/macos/safari/safari_favicons.py
@@ -0,0 +1,115 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from dissect.target.exceptions import UnsupportedPluginError
+from dissect.target.helpers.record import TargetRecordDescriptor
+from dissect.target.plugin import Plugin, export
+from dissect.target.plugins.os.unix.bsd.darwin.macos.helpers.build_paths import _build_userdirs
+from dissect.target.plugins.os.unix.bsd.darwin.macos.helpers.build_records import build_sqlite_records
+
+if TYPE_CHECKING:
+    from collections.abc import Iterator
+
+    from dissect.target import Target
+
+PageURLRecord = TargetRecordDescriptor(
+    "macos/safari/favicon/page_url",
+    [
+        ("string", "table"),
+        ("string", "url"),
+        ("string", "uuid"),
+        ("path", "source"),
+    ],
+)
+
+IconInfoRecord = TargetRecordDescriptor(
+    "macos/safari/favicon/icon_info",
+    [
+        ("string", "table"),
+        ("string", "uuid"),
+        ("string", "url"),
+        ("datetime", "timestamp"),
+        ("varint", "width"),
+        ("varint", "height"),
+        ("varint", "has_generated_representations"),
+        ("path", "source"),
+    ],
+)
+
+RejectedResourcesRecord = TargetRecordDescriptor(
+    "macos/safari/favicon/rejected_resources",
+    [
+        ("string", "table"),
+        ("string", "page_url"),
+        ("string", "icon_url"),
+        ("datetime", "timestamp"),
+        ("path", "source"),
+    ],
+)
+
+
+SafariFaviconRecords = (PageURLRecord, IconInfoRecord, RejectedResourcesRecord)
+
+CONVERT_TIMESTAMPS = {
+    "timestamp": "2001",
+}
+
+
+class SafariFaviconsPlugin(Plugin):
+    """macOS Safari favicons SQLite database plugin."""
+
+    USER_PATH = ("Library/Safari/Favicon Cache/favicons.db",)
+
+    def __init__(self, target: Target):
+        super().__init__(target)
+        self.files = self._find_files()
+
+    def check_compatible(self) -> None:
+        if not (self.files):
+            raise UnsupportedPluginError("No favicons.db files found")
+
+    def _find_files(self) -> set:
+        files = set()
+        for _, path in _build_userdirs(self, self.USER_PATH):
+            files.add(path)
+        return files
+
+    @export(record=SafariFaviconRecords)
+    def safari_favicons(
+        self,
+    ) -> Iterator[SafariFaviconRecords]:
+        """Return Safari favicon information.
+
+        Yields the following record types:
+
+        .. code-block:: text
+
+            PageURLRecord:
+                table (string): Name of the source table (page_url).
+                url (string): URL of the webpage.
+                uuid (string): Unique identifier.
+                source (path): Path to the favicons.db database file.
+
+            IconInfoRecord:
+                table (string): Name of the source table (icon_info).
+                uuid (string): Unique identifier.
+                url (string): URL of the favicon image.
+                timestamp (datetime): Timestamp.
+                width (varint): Width of the favicon.
+                height (varint): Height of the favicon.
+                has_generated_representations (varint): Indicates whether the favicon has generated representations:
+                    0 = False.
+                    1 = True.
+                source (path): Path to the favicons.db database file.
+
+            RejectedResourcesRecord:
+                table (string): Name of the source table (rejected_resources).
+                page_url (string): URL of the webpage associated with the rejected favicon.
+                icon_url (string): URL of the rejected favicon resource.
+                timestamp (datetime): Timestamp.
+                source (path): Path to the favicons.db database file.
+        """
+        yield from build_sqlite_records(self, self.files, SafariFaviconRecords, convert_timestamps=CONVERT_TIMESTAMPS)
+
+        # TODO: Add database_info table
---
+++ dissect/target/plugins/os/unix/bsd/darwin/macos/safari/safari_per_site_preferences.py
@@ -0,0 +1,111 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from dissect.target.exceptions import UnsupportedPluginError
+from dissect.target.helpers.record import TargetRecordDescriptor
+from dissect.target.plugin import Plugin, export
+from dissect.target.plugins.os.unix.bsd.darwin.macos.helpers.build_paths import _build_userdirs
+from dissect.target.plugins.os.unix.bsd.darwin.macos.helpers.build_records import build_sqlite_records
+
+if TYPE_CHECKING:
+    from collections.abc import Iterator
+
+    from dissect.target import Target
+
+
+SQLiteSequenceRecord = TargetRecordDescriptor(
+    "macos/per_site_preferences/sqlite_sequence",
+    [
+        ("string", "table"),
+        ("string", "name"),
+        ("varint", "seq"),
+        ("path", "source"),
+    ],
+)
+
+PreferencesValuesRecord = TargetRecordDescriptor(
+    "macos/safari/per_site_preferences/preferences_values",
+    [
+        ("string", "table"),
+        ("varint", "id"),
+        ("string", "preference_domain"),
+        ("string", "preference"),
+        ("varint", "preference_value"),
+        ("datetime", "timestamp"),
+        ("string", "sync_data"),
+        ("string", "record_name"),
+        ("path", "source"),
+    ],
+)
+
+
+SafariPerSitePreferencesRecords = (
+    SQLiteSequenceRecord,
+    PreferencesValuesRecord,
+)
+
+FIELD_MAPPINGS = {
+    "domain": "preference_domain",
+}
+
+CONVERT_TIMESTAMPS = {
+    "timestamp": "2001",
+}
+
+
+class SafariPerSitePreferencesPlugin(Plugin):
+    """macOS Safari per site preferences SQLite database plugin."""
+
+    USER_PATH = ("Library/Safari/PerSitePreferences.db",)
+
+    def __init__(self, target: Target):
+        super().__init__(target)
+        self.files = self._find_files()
+
+    def check_compatible(self) -> None:
+        if not (self.files):
+            raise UnsupportedPluginError("No PerSitePreferences.db files found")
+
+    def _find_files(self) -> set:
+        files = set()
+        for _, path in _build_userdirs(self, self.USER_PATH):
+            files.add(path)
+        return files
+
+    @export(record=SafariPerSitePreferencesRecords)
+    def safari_per_site_preferences(
+        self,
+    ) -> Iterator[SafariPerSitePreferencesRecords]:
+        """Return Safari per site preferences information.
+
+        Yields the following record types:
+
+        .. code-block:: text
+
+            PreferencesValuesRecord:
+                table (string): Name of the source table (preference_values).
+                id (varint): Primary key of the row.
+                preference_domain (string): Domain name the preference applies to.
+                preference (string): Name of the preference.
+                preference_value (varint): Value assigned to the preference.
+                timestamp (datetime): Timestamp indicating when the preference was set or updated.
+                sync_data (string): Synchronization metadata associated with the preference.
+                record_name (string): Record name.
+                source (path): Path to the PerSitePreferences.db database file.
+
+            SQLiteSequenceTableRecord:
+                table (string): Name of the source table (sqlite_sequence).
+                name (string): Name of the table for which the sequence applies.
+                seq (varint): Current autoincrement value for the table.
+                source (path): Path to the PerSitePreferences.db database file.
+        """
+        yield from build_sqlite_records(
+            self,
+            self.files,
+            SafariPerSitePreferencesRecords,
+            field_mappings=FIELD_MAPPINGS,
+            convert_timestamps=CONVERT_TIMESTAMPS,
+        )
+
+        # TODO: Add default_preferences table
