7470e4a1a80b80546edad184e7e1b1208e9c869d
wbi <william.billaud@orange.com>
2026-09-10 17:45:07 +0200

    Add version to target-build-winzones

--- dissect/target/tools/build_pluginlist.py
+++ dissect/target/tools/build_pluginlist.py
@@ -13,7 +13,7 @@ def main() -> int:
     parser = argparse.ArgumentParser(
         description=f"target-build-pluginlist {get_dissect_target_version()} : generate a static list of plugins. "
         f"This can be used to run dissect.target in some specific environments "
-        f"such as binaries built with PyOxidizer, where plugin auto discovery dynamic location does not work",
+        f"such as binaries built with PyOxidizer, where plugin auto discovery dynamic location does not work.",
     )
     parser.add_argument("-v", "--verbose", action="count", default=0, help="increase output verbosity")
     parser.add_argument("--version", action="version", version=get_dissect_target_version())
