4ca83fbdfbdd56b1f908c481b4218cd650e38d15
JSCU-CNI <121175071+JSCU-CNI@users.noreply.github.com>
2026-09-16 16:27:16 +0200

    fix pypy test

--- tests/helpers/test_typeurl.py
+++ tests/helpers/test_typeurl.py
@@ -31,7 +31,11 @@ def test_typeurl_unmarshal_json() -> None:
     [
         (None, ValueError, r"Expecting value\: line 1 column 1 \(char 0\)"),
         (b"", ValueError, r"Invalid typeurl structure\: Read 0 bytes\, but expected 1"),
-        (b"\x00\x03foo\x00\x03bar", JSONDecodeError, r"Expecting value: line 1 column 1 \(char 0\)"),
+        (
+            b"\x00\x03foo\x00\x03bar",
+            JSONDecodeError,
+            r"(Expecting value|Unexpected \'\x00\'): line 1 column 1 \(char 0\)",
+        ),
     ],
 )
 def test_typeurl_unmarshal_json_invalid(input: bytes, exception: type[Exception], match: str) -> None:
